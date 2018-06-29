b8e2aee7f3263f8289d4738b086d9bad254d8e66
robertm <robert.marskar@sintef.no>
2018-06-29 12:59:13 +0200

    Last commit had a bug...

--- src/poisson_solver/neumann_func.cpp
+++ src/poisson_solver/neumann_func.cpp
@@ -7,12 +7,10 @@
 
 #include "neumann_func.H"
 
-neumann_func::neumann_func(Real (*a_value)(const Real a_time),
-			   Real (*a_func)(const RealVect a_pos),
+neumann_func::neumann_func(const Real a_curveReal (*a_func)(const RealVect a_pos),
 			   const RealVect a_origin){
-  m_value  = a_value;
-  m_func   = a_func;
-  m_origin = a_origin;
+  m_func      = a_func;
+  m_origin    = a_origin;
 }
 
 neumann_func::~neumann_func(){
@@ -25,9 +23,9 @@ void neumann_func::set_time(const Real a_time){
 
 Real neumann_func::value(const RealVect& a_point, const int& a_comp) const {
   // m_func wants physical coordinates but a_point is the computational coordinate
-  return m_func(a_point + m_origin)*m_value(m_time);
+  return m_func(a_point + m_origin)*m_potential(m_time);
 }
 
 Real neumann_func::derivative(const RealVect& a_point, const int& a_comp, const int& a_dir) const {
-  MayDay::Abort("neumann_func::derivative - this should not be called. How did you get here?");
+  MayDay::Abort("poisson_multifluid_gmg::neumann_func::derivative - this should not be called. How did you get here?");
 }
