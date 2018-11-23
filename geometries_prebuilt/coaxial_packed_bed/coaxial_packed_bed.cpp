8db14bfa38142f335de0b5c3788c29bf0fd3e2d6
robertm <robert.marskar@sintef.no>
2018-11-23 13:31:13 +0100

    Added the coaxial packed sphere geometry. It needs a better sphere packing algorithm - the current one does not work very well

--- geometries_prebuilt/coaxial_packed_bed/coaxial_packed_bed.cpp
+++ geometries_prebuilt/coaxial_packed_bed/coaxial_packed_bed.cpp
@@ -14,6 +14,7 @@
 #include <ParmParse.H>
 #include <BaseIF.H>
 #include <SphereIF.H>
+#include <MultiSphereIF.H>
 
 #include "cylinder_if.H"
 #include "perlin_sphere_if.H"
@@ -21,153 +22,162 @@
 #include "new_sphere_if.H"
 #include "cylinder_if.H"
 
+#define hard_spheres 1
+
 coaxial_packed_bed::coaxial_packed_bed(){
+  this->build_conductors();
+  this->build_packed_bed();
+}
 
-  Real eps0 = 1.0;
-  Real eps1 = 4.0;
-  Real R0   = 0.9;
-  Real R2   = 0.3;
+coaxial_packed_bed::~coaxial_packed_bed(){
+  
+}
 
-  RealVect c0  = RealVect(D_DECL(0.0, 0.0, -10.0));
-  RealVect c00 = RealVect(D_DECL(0.0, 0.0,  10.0));
-  RealVect c1  = RealVect(D_DECL(0.0, 0.0, -10.0));
-  RealVect c11 = RealVect(D_DECL(0.0, 0.0,  10.0));
+void coaxial_packed_bed::reseed(){
+  int seed = time(NULL);   // Reseed the RNG
 
+  // Haven't had trouble with this, but I'm putting this here for safety. 
+#ifdef CH_MPI
+  int result = MPI_Bcast(&seed, 1, MPI_INT, 0, Chombo_MPI::comm);
+  if(result != MPI_SUCCESS){
+    MayDay::Error("coaxial_packed_bed::reseed() - broadcast failed");
+  }
+#endif
+  std::srand(seed);
+}
+
+Real coaxial_packed_bed::random(){
+  return  static_cast <Real> (rand()) / static_cast <Real> (RAND_MAX);
+}
+
+void coaxial_packed_bed::build_conductors(){
+  m_rad_inner = 0.1;
+  m_rad_outer = 0.9;
+  
+  const RealVect c0 = RealVect(D_DECL(0.0, 0.0, -2.0));
+  const RealVect c1 = RealVect(D_DECL(0.0, 0.0,  2.0));
+
+  std::string str          = "";
   bool turn_off_outer      = false;
   bool turn_off_inner      = false;
   bool inner_live          = true;
   bool outer_live          = false;
   
+  ParmParse pp("coaxial_packed_bed");
+
+  pp.get("outer_radius", m_rad_outer);
+  pp.get("inner_radius", m_rad_inner);
   
-  { // Get parameters from input script
-    ParmParse pp("coaxial_packed_bed");
-
-    std::string str;
-    Vector<Real> v0(SpaceDim), v00(SpaceDim);
-    Vector<Real> v1(SpaceDim), v11(SpaceDim);
-    Vector<Real> v2(SpaceDim), v22(SpaceDim);
-    
-    pp.query("eps0",                 eps0);
-    pp.query("eps1",                 eps1);
-    pp.query("outer_radius",         R0);
-    pp.query("inner_radius",         R2);
-
-    pp.queryarr("outer_center1",      v0,  0, SpaceDim);
-    pp.queryarr("outer_center2",      v00, 0, SpaceDim);
-    pp.queryarr("inner_center1",      v2, 0, SpaceDim);
-    pp.queryarr("inner_center2",      v22, 0, SpaceDim);
-
-    c0  = RealVect(D_DECL(v0[0],  v0[1],  v0[2]));
-    c00 = RealVect(D_DECL(v00[0], v00[1], v00[2]));
-    c1  = RealVect(D_DECL(v2[0],  v2[1],  v2[2]));
-    c11 = RealVect(D_DECL(v22[0], v22[1], v22[2]));
-
-    if(pp.contains("turn_off_outer")){
-      pp.get("turn_off_outer", str);
-      turn_off_outer = (str == "true") ? true : false;
-    }
-    if(pp.contains("turn_off_inner")){
-      pp.get("turn_off_inner", str);
-      turn_off_inner = (str == "true") ? true : false;
-    }
-    if(pp.contains("outer_live")){
-      pp.get("outer_live", str);
-      outer_live = (str == "true") ? true : false;
-    }
-    if(pp.contains("inner_live")){
-      pp.get("inner_live", str);
-      inner_live = (str == "true") ? true : false;
-    }
+  if(pp.contains("turn_off_outer")){
+    pp.get("turn_off_outer", str);
+    turn_off_outer = (str == "true") ? true : false;
+  }
+  if(pp.contains("turn_off_inner")){
+    pp.get("turn_off_inner", str);
+    turn_off_inner = (str == "true") ? true : false;
+  }
+  if(pp.contains("outer_live")){
+    pp.get("outer_live", str);
+    outer_live = (str == "true") ? true : false;
+  }
+  if(pp.contains("inner_live")){
+    pp.get("inner_live", str);
+    inner_live = (str == "true") ? true : false;
   }
 
   // Create geometry
+  const Real eps0 = 1.0;
   this->set_eps0(eps0);
-  m_dielectrics.resize(0);
-  m_electrodes.resize(0);
 
+  m_electrodes.resize(0);
   if(!turn_off_outer){
-    RefCountedPtr<BaseIF> outer = RefCountedPtr<BaseIF> (new cylinder_if(c0, c00, R0, true));
+    RefCountedPtr<BaseIF> outer = RefCountedPtr<BaseIF> (new cylinder_if(c0, c1, m_rad_outer, true));
     m_electrodes.push_back(electrode(outer, outer_live, 1.0));
   }
   if(!turn_off_inner){
-    RefCountedPtr<BaseIF> inner = RefCountedPtr<BaseIF> (new cylinder_if(c1, c11, R2, false));
+    RefCountedPtr<BaseIF> inner = RefCountedPtr<BaseIF> (new cylinder_if(c0, c1, m_rad_inner, false));
     m_electrodes.push_back(electrode(inner, inner_live, 1.0));
   }
 
-#if 1 // Debug
-  {
-  const RealVect center = RealVect(0.5, 0.0, 0.0);
-  RefCountedPtr<BaseIF> middle = RefCountedPtr<BaseIF> (new new_sphere_if(center, 0.2, false));
-  m_dielectrics.push_back(dielectric(middle, eps1));
-  }
-
-  {
-  const RealVect center = RealVect(0.0, 0.5, 0.0);
-  RefCountedPtr<BaseIF> middle = RefCountedPtr<BaseIF> (new new_sphere_if(center, 0.2, false));
-  m_dielectrics.push_back(dielectric(middle, eps1));
-  }
-  {
-  const RealVect center = RealVect(0.0, -0.5, 0.0);
-  RefCountedPtr<BaseIF> middle = RefCountedPtr<BaseIF> (new new_sphere_if(center, 0.2, false));
-  m_dielectrics.push_back(dielectric(middle, eps1));
-  }
-  {
-    const RealVect center = RealVect(-0.5, 0.0, 0.0);
-    RefCountedPtr<BaseIF> middle = RefCountedPtr<BaseIF> (new new_sphere_if(center, 0.2, false));
-    m_dielectrics.push_back(dielectric(middle, eps1));
-  }
+#if 0 // debug
+  m_electrodes.resize(0);
+  RefCountedPtr<BaseIF> sph = RefCountedPtr<BaseIF> (new new_sphere_if(RealVect::Zero, 0.2, false));
+  m_electrodes.push_back(electrode(sph, true, 1.0));
 #endif
-
-#if 1 // Debug
-  {
-    const RealVect center = RealVect(0.5/sqrt(2), 0.5/sqrt(2), 0.5);
-    RefCountedPtr<BaseIF> middle = RefCountedPtr<BaseIF> (new new_sphere_if(center, 0.2, false));
-    m_dielectrics.push_back(dielectric(middle, eps1));
-  }
-
-  {
-    const RealVect center = RealVect(-0.5/sqrt(2), 0.5/sqrt(2), 0.5);
-    RefCountedPtr<BaseIF> middle = RefCountedPtr<BaseIF> (new new_sphere_if(center, 0.2, false));
-    m_dielectrics.push_back(dielectric(middle, eps1));
-  }
-  {
-    const RealVect center = RealVect(0.5/sqrt(2), -0.5/sqrt(2), 0.5);
-    RefCountedPtr<BaseIF> middle = RefCountedPtr<BaseIF> (new new_sphere_if(center, 0.2, false));
-    m_dielectrics.push_back(dielectric(middle, eps1));
-  }
-  {
-    const RealVect center = RealVect(-0.5/sqrt(2), -0.5/sqrt(2), 0.5);
-    RefCountedPtr<BaseIF> middle = RefCountedPtr<BaseIF> (new new_sphere_if(center, 0.2, false));
-    m_dielectrics.push_back(dielectric(middle, eps1));
-  }
-#endif
-
-#if 1 // Debug
-  {
-    const RealVect center = RealVect(0.5/sqrt(2), 0.5/sqrt(2), -0.5);
-    RefCountedPtr<BaseIF> middle = RefCountedPtr<BaseIF> (new new_sphere_if(center, 0.2, false));
-    m_dielectrics.push_back(dielectric(middle, eps1));
-  }
-
-  {
-    const RealVect center = RealVect(-0.5/sqrt(2), 0.5/sqrt(2), -0.5);
-    RefCountedPtr<BaseIF> middle = RefCountedPtr<BaseIF> (new new_sphere_if(center, 0.2, false));
-    m_dielectrics.push_back(dielectric(middle, eps1));
-  }
-  {
-    const RealVect center = RealVect(0.5/sqrt(2), -0.5/sqrt(2), -0.5);
-    RefCountedPtr<BaseIF> middle = RefCountedPtr<BaseIF> (new new_sphere_if(center, 0.2, false));
-    m_dielectrics.push_back(dielectric(middle, eps1));
-  }
-  {
-    const RealVect center = RealVect(-0.5/sqrt(2), -0.5/sqrt(2), -0.5);
-    RefCountedPtr<BaseIF> middle = RefCountedPtr<BaseIF> (new new_sphere_if(center, 0.2, false));
-    m_dielectrics.push_back(dielectric(middle, eps1));
-  }
-#endif
-  
 }
 
-coaxial_packed_bed::~coaxial_packed_bed(){
+void coaxial_packed_bed::build_packed_bed(){
+  ParmParse pp("coaxial_packed_bed");
   
+  // Init RNG
+  std::string str;
+  pp.get("reseed", str);
+  if(str == "true"){
+    this->reseed();
+  }
+  else{
+    std::srand(0);
+  }
+
+  bool turn_off_spheres = false;
+  if(pp.contains("turn_off_spheres")){
+    pp.get("turn_off_spheres", str);
+    turn_off_spheres = (str == "true") ? true : false;
+  }
+
+  if(turn_off_spheres){
+    m_dielectrics.resize(0);
+  }
+  else{
+    Real eps1 = 1.0;
+    Real zlo, zhi;
+    pp.get("num_spheres",         m_num_spheres);
+    pp.get("sphere_radius",       m_rad_sphere);
+    pp.get("sphere_min_dist",     m_min_dist);
+    pp.get("sphere_permittivity", eps1);
+    pp.get("z_low",               zlo);
+    pp.get("z_high",              zhi);
+
+    // Draw sphere centers
+    int ndrawn = 0;
+    Vector<RealVect> centers;
+    Vector<Real> radii;
+    while(ndrawn < m_num_spheres){
+      RealVect c;
+      for (int dir = 0; dir < SpaceDim; dir++){
+	c[dir] = 2.0*m_rad_outer*(random()-0.5);
+      }
+#if CH_SPACEDIM==3
+      c[2] = zlo + random()*(zhi-zlo);
+#endif
+      const RealVect rdist = RealVect(D_DECL(c[0], c[1], 0.0));
+      const bool test_dist_inner = (rdist.vectorLength() - (m_rad_sphere+m_rad_inner) >= m_min_dist);     // True = OK!
+      const bool test_dist_outer = (rdist.vectorLength() + m_rad_sphere <=  (m_rad_outer - m_min_dist));  // True = OK!
+#if CH_SPACEDIM==3
+      const bool test_dist_low   = (c[2] - zlo) >= m_min_dist + m_rad_sphere;
+      const bool test_dist_high  = (zhi - c[2]) >= m_min_dist + m_rad_sphere;
+#else
+      const bool test_dist_low  = true;
+      const bool test_dist_high = true;
+#endif
+      if(test_dist_inner && test_dist_outer && test_dist_low && test_dist_high){ // Does not intersect coaxial geometry
+
+	bool isect_others = false;
+	for (int i = 0; i < centers.size(); i++){
+	  const Real dist = (c-centers[i]).vectorLength() - 2*m_rad_sphere;
+	  if(dist < m_min_dist) isect_others = true;
+
+	}
+
+	if(!isect_others){
+	  centers.push_back(c);
+	  ndrawn++;
+	}
+      }
+    }
+    radii.resize(centers.size(), m_rad_sphere);
+
+    RefCountedPtr<BaseIF> spheres = RefCountedPtr<BaseIF> (new MultiSphereIF(radii, centers, false));
+    m_dielectrics.push_back(dielectric(spheres, eps1));
+  }
 }
