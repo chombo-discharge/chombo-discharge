0f34b148b4191bfe7dedb7aebe0b51a14c615f10
Robert Marskar <robert.marskar@sintef.no>
2019-06-09 22:12:35 +0200

    Contributions to pic_solver

--- src/pic_solver/pic_solver.cpp
+++ src/pic_solver/pic_solver.cpp
@@ -16,14 +16,17 @@
 #include <EBArith.H>
 #include <ParmParse.H>
 #include <ParticleIO.H>
+#include <EBAMRIO.H>
 
   
 pic_solver::pic_solver(){
   m_name = "pic_solver";
-  set_verbosity(-1);
+  set_verbosity(10);
   set_time(0, 0.0, 0.0);
   set_deposition_type();
   set_phase(phase::gas);
+  set_charge(0);
+  set_mass(1.0);
 }
 pic_solver::~pic_solver(){
 
@@ -59,6 +62,14 @@ void pic_solver::set_verbosity(const int a_verbosity){
   m_verbosity = a_verbosity;
 }
 
+void pic_solver::set_charge(const int a_charge){
+  m_charge = a_charge;
+}
+
+void pic_solver::set_mass(const Real a_mass){
+  m_mass = a_mass;
+}
+
 void pic_solver::advance(const Real a_dt){
 
 }
@@ -92,7 +103,7 @@ void pic_solver::deposit_charge(EBAMRCellData& a_rho) {
   }
 
   // 2. Scale with particle charge
-  data_ops::scale(m_scratch_sca, m_charge*units::s_Qe);
+  //  data_ops::scale(m_scratch_sca, m_charge*units::s_Qe);
 
   // 3. Increment space charge density
   data_ops::incr(a_rho, m_scratch_sca, 1.0);
@@ -162,6 +173,11 @@ void pic_solver::initial_data(List<Particle>& a_particles){
     m_particles[lvl]->outcast().clear();      
 #endif
   }
+
+
+#if 0  // Debug
+  std::cout << m_particles[0]->numParticles() << std::endl;
+#endif
 }
 
 void pic_solver::set_deposition_type(){
@@ -205,7 +221,7 @@ void pic_solver::allocate_internals(){
   }
 
   const int buffer = 0;
-  const int ncomp  = 0;
+  const int ncomp  = 1;
 
   m_amr->allocate(m_scratch_sca, m_phase, ncomp);
   m_amr->allocate(m_scratch_vec, m_phase, SpaceDim);
@@ -232,8 +248,44 @@ void pic_solver::regrid(const int a_old_finest_level, const int a_new_finest_lev
 
 }
 
-void pic_solver::write_plot_file() const {
+void pic_solver::write_plot_file() {
+  CH_TIME("pic_solver::write_plot_file");
+  if(m_verbosity > 5){
+    pout() << m_name + "::write_plot_file" << endl;
+  }
 
+  // Filename
+  char file_char[1000];
+  sprintf(file_char, "%s.step%07d.%dd.hdf5", m_name.c_str(), m_step, SpaceDim);
+
+  // Deposit charge to some data holder
+  const int ncomps = 1;
+  EBAMRCellData output;
+  Vector<string> names(ncomps);
+  m_amr->allocate(output, m_phase, ncomps);
+  data_ops::set_value(output, 0.0);
+  deposit_charge(output);
+  names[0] = "density";
+
+  Vector<Real> covered_values(ncomps, 0.0);
+  string fname(file_char);
+
+  Vector<LevelData<EBCellFAB>* > output_ptr;
+  m_amr->alias(output_ptr, output);
+
+  writeEBHDF5(fname,
+	      m_amr->get_grids(),
+	      output_ptr,
+	      names,
+	      m_amr->get_domains()[0].domainBox(),
+	      m_amr->get_dx()[0],
+	      m_dt,
+	      m_time,
+	      m_amr->get_ref_rat(),
+	      m_amr->get_finest_level() + 1,
+	      true,
+	      covered_values,
+	      IntVect::Unit);
 }
 
 void pic_solver::set_phase(const phase::which_phase a_phase){
@@ -243,6 +295,58 @@ void pic_solver::set_phase(const phase::which_phase a_phase){
   }
 
   m_phase = a_phase;
+}
+
+void pic_solver::leapfrog_rewind(const Real a_dt){
+  CH_TIME("pic_solver::leapfrog_rewind");
+  if(m_verbosity > 5){
+    pout() << m_name + "leapfrog_rewind" << endl;
+  }
+
+  for (int lvl = 0; lvl <= m_amr->get_finest_level(); lvl++){
+    const Real dx                = m_amr->get_dx()[lvl];
+    const DisjointBoxLayout& dbl = m_amr->get_grids()[lvl];
+    const RealVect origin  = m_physdom->get_prob_lo();
+    
+    for (DataIterator dit = dbl.dataIterator(); dit.ok(); ++dit){
+      List<Particle>& particles = (*m_particles[lvl])[dit()].listItems();
+
+      for (ListIterator<Particle> lit(particles); lit.ok(); ++lit){
+	Particle& particle = lit();
+
+	particle.velocity() -= 0.5*particle.acceleration()*a_dt;
+      }
+    }
+  }
+}
+
+void pic_solver::advance_leapfrog(const Real a_dt){
+  CH_TIME("pic_solver::leapfrog_rewind");
+  if(m_verbosity > 5){
+    pout() << m_name + "leapfrog_rewind" << endl;
+  }
+
+  // 1. Move and accelerate
+  for (int lvl = 0; lvl <= m_amr->get_finest_level(); lvl++){
+    const Real dx                = m_amr->get_dx()[lvl];
+    const DisjointBoxLayout& dbl = m_amr->get_grids()[lvl];
+    const RealVect origin  = m_physdom->get_prob_lo();
+    
+    for (DataIterator dit = dbl.dataIterator(); dit.ok(); ++dit){
+      List<Particle>& particles = (*m_particles[lvl])[dit()].listItems();
+
+      for (ListIterator<Particle> lit(particles); lit.ok(); ++lit){
+	Particle& particle = lit();
+
+	
+	particle.velocity() += particle.acceleration()*a_dt;
+	particle.position() += particle.velocity()*a_dt;
+      }
+    }
+  }
+
+  // 2. Rebin photons
+  MayDay::Abort("pic_solver::advance_leapfrog - routine is not finished");
 }
 
 Real pic_solver::get_time() const{
