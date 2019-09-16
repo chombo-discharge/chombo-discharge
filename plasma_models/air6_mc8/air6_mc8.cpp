6c08a6007afede3808856d9fc2adca16b3cf1a4d
Robert Marskar <robert.marskar@sintef.no>
2019-09-16 22:03:40 +0200

    Adding more stuff to air6_mc8

--- plasma_models/air6_mc8/air6_mc8.cpp
+++ plasma_models/air6_mc8/air6_mc8.cpp
@@ -6,6 +6,7 @@
 */
 
 #include "air6_mc8.H"
+#include "air6_mc8_species.H"
 #include "units.H" 
 
 #include <iostream>
@@ -15,14 +16,21 @@
 
 #include <ParmParse.H>
 
+std::string air6_mc8::s_bolsig_mobility = "Energy (eV)	Mobility *N (1/m/V/s)";
+std::string air6_mc8::s_bolsig_diffco   = "E/N (Td)	Diffusion coefficient *N (1/m/s)";
+std::string air6_mc8::s_bolsig_alpha    = "E/N (Td)	Total ionization freq. /N (m3/s)";
+std::string air6_mc8::s_bolsig_eta      = "E/N (Td)	Total attachment freq. /N (m3/s)";
+
+
 air6_mc8::air6_mc8() {
 
   parse_transport_file();
   parse_transport();
-  //  parse_gas_params();
-  //  parse_electron_mobility();
-  //  parse_alpha();
-  //  parse_eta();
+  parse_gas_params();
+  parse_electron_mobility();
+  parse_electron_diffco();
+  parse_alpha();
+  parse_eta();
   
   parse_photoi();
   parse_chemistry();
@@ -37,6 +45,34 @@ air6_mc8::air6_mc8() {
 
 air6_mc8::~air6_mc8() {
 
+}
+
+void air6_mc8::read_file_entries(lookup_table& a_table, const std::string a_string){
+  Real x, y;
+  bool read_line = false;
+  std::ifstream infile(m_transport_file);
+  std::string line;
+  while (std::getline(infile, line)){
+
+    // Right trim string
+    line.erase(line.find_last_not_of(" \n\r\t")+1);
+
+    if(line == a_string){ // Begin reading
+      read_line = true;
+    }
+    else if(line == "" & read_line){ // Stop reading
+      read_line = false;
+    }
+
+    if(read_line){
+      std::istringstream iss(line);
+      if (!(iss >> x >> y)) {
+    	continue;
+      }
+      a_table.add_entry(x, y);
+    }
+  }
+  infile.close();
 }
 
 void air6_mc8::parse_transport_file(){
@@ -68,32 +104,46 @@ void air6_mc8::parse_transport(){
   m_ion_diffusion = m_ion_mobility*(units::s_kb*m_T)/units::s_Qe;
 }
 
-void air6_mc8::read_file_entries(lookup_table& a_table, const std::string a_string){
-  Real x, y;
-  bool read_line = false;
-  std::ifstream infile(m_transport_file);
-  std::string line;
-  while (std::getline(infile, line)){
+void air6_mc8::parse_gas_params(){
+  ParmParse pp("air6_mc8");
 
-    // Right trim string
-    line.erase(line.find_last_not_of(" \n\r\t")+1);
+  // Pressure form input script
+  pp.get("pressure",    m_p);
+  pp.get("temperature", m_T);
+  pp.get("frac_N2",     m_N2frac);
+  pp.get("frac_O2",     m_O2frac);
 
-    if(line == a_string){ // Begin reading
-      read_line = true;
-    }
-    else if(line == "" & read_line){ // Stop reading
-      read_line = false;
-    }
+  m_N = m_p*units::s_Na/(m_T*units::s_R);
 
-    if(read_line){
-      std::istringstream iss(line);
-      if (!(iss >> x >> y)) {
-    	continue;
-      }
-      a_table.add_entry(x, y);
-    }
-  }
-  infile.close();
+}
+
+void air6_mc8::parse_electron_mobility(){
+  ParmParse pp("air6_mc8");
+  read_file_entries(m_e_mobility, air6_mc8::s_bolsig_mobility);
+  m_e_mobility.scale_y(1./m_N); 
+  m_e_mobility.make_uniform(m_uniform_entries);
+}
+
+void air6_mc8::parse_electron_diffco(){
+  ParmParse pp("air6_mc8");
+  
+  read_file_entries(m_e_diffco, air6_mc8::s_bolsig_mobility);
+  m_e_diffco.scale_y(1./m_N); 
+  m_e_diffco.make_uniform(m_uniform_entries);
+}
+
+void air6_mc8::parse_alpha(){
+  ParmParse pp("air6_mc8");
+  read_file_entries(m_e_alpha, air6_mc8::s_bolsig_alpha);
+  m_e_alpha.scale_y(m_N); 
+  m_e_alpha.make_uniform(m_uniform_entries);
+}
+
+void air6_mc8::parse_eta(){
+  ParmParse pp("air6_mc8");
+  read_file_entries(m_e_eta, air6_mc8::s_bolsig_eta);
+  m_e_eta.scale_y(m_N); 
+  m_e_eta.make_uniform(m_uniform_entries);
 }
 
 void air6_mc8::parse_photoi(){
@@ -183,4 +233,42 @@ void air6_mc8::init_rng(){
   m_udist01 = new std::uniform_real_distribution<Real>(0.0, 1.0);
   m_udist11 = new std::uniform_real_distribution<Real>(-1.0, 1.0);
   m_rng     = new std::mt19937_64(m_rng_seed);
+}
+
+void air6_mc8::instantiate_species(){
+
+  m_num_species = 6;
+  m_num_photons = 8;
+
+  m_elec_idx = 0;
+  m_plus_idx = 1;
+  m_minu_idx = 2;
+  m_c4v0_idx = 3;
+  m_c4v1_idx = 4;
+  m_b1v1_idx = 5;
+
+  m_c4v0_X1v0_idx = 0;
+  m_c4v0_X1v1_idx = 1;
+  m_c4v1_X1v0_idx = 2;
+  m_c4v1_X1v1_idx = 3;
+  m_c4v1_X1v2_idx = 4;
+  m_c4v1_X1v3_idx = 5;
+  m_b1v1_X1v0_idx = 6;
+  m_b1v1_X1v1_idx = 7;
+
+  m_species[m_elec_idx]  = RefCountedPtr<species>      (new air6_mc8::electron());
+  m_species[m_plus_idx]  = RefCountedPtr<species>      (new air6_mc8::M_plus());
+  m_species[m_minu_idx]  = RefCountedPtr<species>      (new air6_mc8::M_minus());
+  m_species[m_c4v0_idx]  = RefCountedPtr<species>      (new air6_mc8::N2_c4v0());
+  m_species[m_c4v1_idx]  = RefCountedPtr<species>      (new air6_mc8::N2_c4v1());
+  m_species[m_b1v1_idx]  = RefCountedPtr<species>      (new air6_mc8::N2_b1v1());
+
+  m_photons[m_c4v0_X1v0_idx] = RefCountedPtr<photon_group> (new air6_mc8::phot_c4v0_X1v0());
+  m_photons[m_c4v0_X1v1_idx] = RefCountedPtr<photon_group> (new air6_mc8::phot_c4v0_X1v0());
+  m_photons[m_c4v1_X1v0_idx] = RefCountedPtr<photon_group> (new air6_mc8::phot_c4v1_X1v0());
+  m_photons[m_c4v1_X1v1_idx] = RefCountedPtr<photon_group> (new air6_mc8::phot_c4v1_X1v1());
+  m_photons[m_c4v1_X1v2_idx] = RefCountedPtr<photon_group> (new air6_mc8::phot_c4v1_X1v2());
+  m_photons[m_c4v1_X1v3_idx] = RefCountedPtr<photon_group> (new air6_mc8::phot_c4v1_X1v3());
+  m_photons[m_b1v1_X1v0_idx] = RefCountedPtr<photon_group> (new air6_mc8::phot_b1v1_X1v0());
+  m_photons[m_b1v1_X1v1_idx] = RefCountedPtr<photon_group> (new air6_mc8::phot_b1v1_X1v1());
 }
---
+++ plasma_models/air6_mc8/air6_mc8_species.cpp
@@ -0,0 +1,18 @@
+#include "air6_mc8.H"
+#include "air6_mc8_species.H"
+
+air6_mc8::electron::electron(){}
+air6_mc8::M_plus::M_plus(){}
+air6_mc8::M_minus::M_minus(){}
+air6_mc8::N2_c4v0::N2_c4v0(){}
+air6_mc8::N2_c4v1::N2_c4v1(){}
+air6_mc8::N2_b1v1::N2_b1v1(){}
+
+air6_mc8::phot_c4v0_X1v0::phot_c4v0_X1v0(){}
+air6_mc8::phot_c4v0_X1v1::phot_c4v0_X1v1(){}
+air6_mc8::phot_c4v1_X1v0::phot_c4v1_X1v0(){}
+air6_mc8::phot_c4v1_X1v1::phot_c4v1_X1v1(){}
+air6_mc8::phot_c4v1_X1v2::phot_c4v1_X1v2(){}
+air6_mc8::phot_c4v1_X1v3::phot_c4v1_X1v3(){}
+air6_mc8::phot_b1v1_X1v0::phot_b1v1_X1v0(){}
+air6_mc8::phot_b1v1_X1v1::phot_b1v1_X1v1(){}
