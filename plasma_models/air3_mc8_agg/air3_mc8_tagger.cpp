8c7cfeafa1319e26279b901ece1ea9b5df4ed06e
robertm <robert.marskar@sintef.no>
2019-11-15 15:24:09 +0100

    Restricted max coarsening level for air3_mc8_agg tagger

--- plasma_models/air3_mc8_agg/air3_mc8_tagger.cpp
+++ plasma_models/air3_mc8_agg/air3_mc8_tagger.cpp
@@ -29,18 +29,19 @@ void air3_mc8_tagger::parse_options(){
   pp.get("refine_curvature",  m_refi_curv);
   pp.get("refine_alpha",      m_refi_alpha);
   pp.get("coarsen_alpha",     m_coar_alpha);
+  pp.get("max_coarsen_lvl",   m_max_coarsen_level);
 }
 
 
 Vector<Real> air3_mc8_tagger::tracer(const RealVect         a_pos,
-			       const Real             a_time,
-			       const Real             a_dx,
-			       const RealVect         a_E,
-			       const Real             a_min_E,
-			       const Real             a_max_E,
-			       const RealVect         a_grad_E,
-			       const Real             a_min_grad_E,
-			       const Real             a_max_grad_E){
+				     const Real             a_time,
+				     const Real             a_dx,
+				     const RealVect         a_E,
+				     const Real             a_min_E,
+				     const Real             a_max_E,
+				     const RealVect         a_grad_E,
+				     const Real             a_min_grad_E,
+				     const Real             a_max_grad_E){
 
   const air3_mc8_agg* plaskin = static_cast<air3_mc8_agg*> (&(*m_plaskin));
   
@@ -52,24 +53,32 @@ Vector<Real> air3_mc8_tagger::tracer(const RealVect         a_pos,
   return tracers;
 }
 bool air3_mc8_tagger::coarsen_cell(const RealVect         a_pos,
-			     const Real             a_time,
-			     const Real             a_dx,
-			     const int              a_lvl,
-			     const Vector<Real>     a_tracer,
-			     const Vector<RealVect> a_grad_tracer){
+				   const Real             a_time,
+				   const Real             a_dx,
+				   const int              a_lvl,
+				   const Vector<Real>     a_tracer,
+				   const Vector<RealVect> a_grad_tracer){
 
-  const bool coarsen = a_grad_tracer[0].vectorLength()*a_dx/a_tracer[0] < m_coar_curv && a_tracer[1]*a_dx < m_coar_alpha;
-  
+  bool coarsen = false;
+
+  if(a_lvl >= m_max_coarsen_level){
+    coarsen = a_grad_tracer[0].vectorLength()*a_dx/a_tracer[0] < m_coar_curv && a_tracer[1]*a_dx < m_coar_alpha;
+  }
+  else{
+    coarsen = false;
+  }
+
+
   return coarsen;
 }
 
 
 bool air3_mc8_tagger::refine_cell(const RealVect         a_pos,
-			    const Real             a_time,
-			    const Real             a_dx,
-			    const int              a_lvl,
-			    const Vector<Real>     a_tracer,
-			    const Vector<RealVect> a_grad_tracer){
+				  const Real             a_time,
+				  const Real             a_dx,
+				  const int              a_lvl,
+				  const Vector<Real>     a_tracer,
+				  const Vector<RealVect> a_grad_tracer){
   const bool refine1  = a_grad_tracer[0].vectorLength()*a_dx/a_tracer[0] > m_refi_curv;
   const bool refine2 = a_tracer[1]*a_dx > m_refi_alpha;
     
