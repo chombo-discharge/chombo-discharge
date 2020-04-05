bfb30bf1e1b06b64f805dd0ad7e385acb740e58f
Robert Marskar <robert.marskar@sintef.no>
2020-04-05 11:23:06 +0200

    Starting to add regression mini-app for brownian_walker which will be used for development (and later regression testing)

---
+++ physics/brownian_walker/brownian_walker_tagger.cpp
@@ -0,0 +1,40 @@
+/*!
+  @file   brownian_walker_tagger.cpp
+  @brief  Implementation of brownian_walker_tagger.H
+  @author Robert Marskar
+  @date   Nov. 2017
+*/
+
+#include "brownian_walker_tagger.H"
+#include "data_ops.H"
+
+#include <ParmParse.H> 
+
+using namespace physics::brownian_walker;
+
+brownian_walker_tagger::brownian_walker_tagger(RefCountedPtr<ito_solver>& a_solver,
+					       RefCountedPtr<amr_mesh>&   a_amr){
+  m_solver    = a_solver;
+  m_amr       = a_amr;
+  m_name      = "brownian_walker";
+  m_verbosity = -1;
+}
+
+brownian_walker_tagger::~brownian_walker_tagger(){
+
+}
+
+void brownian_walker_tagger::regrid(){
+
+}
+
+void brownian_walker_tagger::parse_options(){
+  ParmParse pp(m_name.c_str());
+  pp.get("refine_magn", m_refi_magn);
+  
+  parse_buffer(); // Derived from cell_tagger
+}
+
+bool brownian_walker_tagger::tag_cells(EBAMRTags& a_tags){
+  return false;
+}
---
+++ physics/brownian_walker/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+import argparse
+import os
+import sys
+sys.path.append('./python')
+import app_main
+import app_make
+import app_options
+
+# Get arguments from input script
+parser = argparse.ArgumentParser();
+parser.add_argument('-dim',             type=int,  help='Dimension', default=2)
+parser.add_argument('-procs',           type=int,  help='Processors to use when building executable', default=1)
+parser.add_argument('-use_mpi',         type=bool, help='MPI enabled (default true)', default=True)
+parser.add_argument('-build',           type=bool, help='Build executable at end', default=False)
+parser.add_argument('-silent',          type=bool, help='Silent build of executable', default=False)
+parser.add_argument('-plasmac_home',    type=str,  help="Source code base directory", default=os.environ.get('PLASMAC_HOME'))
+parser.add_argument('-base_dir',        type=str,  help="Base directory of mini-app", default="./mini_apps")
+parser.add_argument('-app_name',        type=str,  help="Mini app name. An error message is issued if the name already exists")
+parser.add_argument('-filename',        type=str,  help="File name of main file", default="main")
+parser.add_argument('-ito_solver',      type=str,  help="Ito solver", default="ito_solver")
+parser.add_argument('-geometry',        type=str,  help="Geometry class", default="regular_geometry")
+parser.add_argument('-stepper',         type=str,  help="Time stepper class", default="brownian_walker_stepper")
+
+args = parser.parse_args()
+
+# Check if PLASMAC_HOME and CHOMBO_HOME has been set
+plasmac_home = args.plasmac_home
+if not args.plasmac_home:
+    print "Error: Cannot set application because the PLASMAC_HOME environment variable has not been set."
+    print "       Please set PLASMAC_HOME, for example:"
+    print "       >export  PLASMAC_HOME=<directory>"
+else:
+    print "PLASMAC_HOME is " + args.plasmac_home
+    print 'Setting up problem in directory ' + args.plasmac_home + "/" + args.base_dir + "/" + args.app_name
+
+    app_main.write_template(args)    # Write main file
+    app_make.write_template(args)    # Write makefile
+    app_options.write_template(args) # Write options file
+
+    # Build executable if called for it
+    if args.build:
+        os.chdir(args.plasmac_home + "/" + args.base_dir + "/" + args.app_name)
+        os.system('pwd')
+        if args.silent:
+            os.system('make -s -j ' + str(args.procs) + ' ' + args.filename)
+        else:
+            os.system('make -j ' + str(args.procs) + ' ' + args.filename)
+            print 'Created and built your mini app - it resides in ' + args.plasmac_home + "/" + args.base_dir + "/" + args.app_name
+    else:
+        print 'Problem setup successful'
