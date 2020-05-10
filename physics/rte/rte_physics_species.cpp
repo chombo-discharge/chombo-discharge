b99bfc6c7a58a62bdc8096c982ac9b545fd15018
Robert Marskar <robert.marskar@sintef.no>
2020-05-10 11:05:20 +0200

    Working on setting up rte physics

---
+++ physics/rte/rte_physics_species.cpp
@@ -0,0 +1,26 @@
+/*!
+  @file   rte_physics_species.cpp
+  @brief  Implementation of rte_physics_species.H
+  @author Robert Marskar
+  @date   May 2020
+*/
+
+#include "rte_physics_species.H"
+
+#include <ParmParse.H>
+
+using namespace physics::rte;
+
+rte_physics_species::rte_physics_species(){
+
+  // This stuff sets the name and a constnat kappa taken from the input script
+  m_name = "rte_physics_species";
+  m_constant = true;
+
+  ParmParse pp("rte_stepper");
+  pp.get("kappa", m_kappa);
+}
+
+rte_physics_species::~rte_physics_species(){
+
+}
