0b373028cd700ae9dd3de400a323498d48bdddcb
Robert Marskar <robert.marskar@sintef.no>
2021-02-25 22:37:29 +0100

    Rounded cylinder rotation issue in 3D

--- src/geometry/rounded_cylinder_if.cpp
+++ src/geometry/rounded_cylinder_if.cpp
@@ -16,7 +16,7 @@
 rounded_cylinder_if::rounded_cylinder_if(const RealVect a_center1, const RealVect a_center2, const Real a_radius, const Real a_curv, const bool a_fluidInside){
   m_center1      = a_center1;
   m_center2      = a_center2;
-  m_length       = sqrt(PolyGeom::dot(m_center2-m_center1,m_center2-m_center1));
+  m_length       = (m_center2-m_center1).vectorLength();
   m_radius       = a_radius;
   m_curv         = a_curv;
   m_fluidInside  = a_fluidInside;
@@ -78,9 +78,6 @@ void rounded_cylinder_if::makeBaseIF3D(){
   BaseIF* torusBottom = (BaseIF*) (new TorusIF(majorRadius, minorRadius, y0, false));
   BaseIF* torusTop    = (BaseIF*) (new TorusIF(majorRadius, minorRadius, y1, false));
 
-  //  std::cout << mainCylinder->value(BASISREALV(1)) << std::endl;
-  //  std::cout << torusBottom->value(BASISREALV(1)) << std::endl;
-
   // Make the intersection of these
   Vector<BaseIF*> parts;
   parts.push_back(mainCylinder);
@@ -92,8 +89,15 @@ void rounded_cylinder_if::makeBaseIF3D(){
   // Do a transform
   TransformIF* transif = new TransformIF(*isect);
 
-  transif->rotate(BASISREALV(2), m_center2-m_center1);
-  transif->translate(m_center1);
+  if(m_center2[2] > m_center1[2]){
+    transif->rotate(BASISREALV(2), m_center2-m_center1);
+    transif->translate(m_center1);
+  }
+  else{
+    transif->rotate(BASISREALV(2), m_center1-m_center2);
+    transif->translate(m_center2);
+  }
+
 
   // Ok, we're done. 
   m_baseif = RefCountedPtr<BaseIF> (transif);
