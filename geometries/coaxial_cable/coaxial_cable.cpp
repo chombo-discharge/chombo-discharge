cc395de180a5933337b4e43ce4118744dc189fb6
Robert Marskar <robert.marskar@sintef.no>
2021-03-02 13:14:26 +0100

    Reworking coaxial cable geometry and setting it up as regtest

--- geometries/coaxial_cable/coaxial_cable.cpp
+++ geometries/coaxial_cable/coaxial_cable.cpp
@@ -12,98 +12,78 @@
 #include <fstream>
 
 #include <ParmParse.H>
-#include <BaseIF.H>
-#include <SphereIF.H>
 
 #include "cylinder_if.H"
-#include "perlin_sphere_if.H"
-#include "box_if.H"
 #include "new_sphere_if.H"
-#include "cylinder_if.H"
 
 coaxial_cable::coaxial_cable(){
-  if(SpaceDim==3){
-  MayDay::Abort("coaxial_cable::coaxial_cable - Only 2D currently supported. If you want 3D, you need to modify this class");
-  }
 
-  Real eps0 = 1.0;
-  Real eps1 = 4.0;
-  Real R0   = 0.9;
-  Real R1   = 0.6;
-  Real R2   = 0.3;
+  ParmParse pp      ("coaxial_cable");
+  ParmParse ppOuter ("coaxial_cable.outer");
+  ParmParse ppInner ("coaxial_cable.inner");
+  ParmParse ppMiddle("coaxial_cable.dielectric");
 
-  RealVect c0 = RealVect(D_DECL(0.0, 0.0, -1.0));
-  RealVect c1 = RealVect(D_DECL(0.0, 0.0, -1.0));
-  RealVect c2 = RealVect(D_DECL(0.0, 0.0, -1.0));
-
-  bool turn_off_outer      = false;
-  bool turn_off_dielectric = false;
-  bool turn_off_inner      = false;
-  bool inner_live          = true;
-  bool outer_live          = false;
+  // Get centers
+  Vector<Real> v(SpaceDim);
+  RealVect e1, e2;
+  pp.getarr("endpoint1", v, 0, SpaceDim); e1 = RealVect(D_DECL(v[0], v[1], v[2]));
+  pp.getarr("endpoint2", v, 0, SpaceDim); e2 = RealVect(D_DECL(v[0], v[1], v[2]));
   
-  
-  { // Get parameters from input script
-    ParmParse pp("coaxial_cable");
+  // Check which ones to use. 
+  bool outer, inner, middle;
+  ppOuter.get ("on", outer);
+  ppInner.get ("on", inner);
+  ppMiddle.get("on", middle);
 
-    std::string str;
-    Vector<Real> v0(SpaceDim);
-    Vector<Real> v1(SpaceDim);
-    Vector<Real> v2(SpaceDim);
-    
-    pp.query("eps0",                 eps0);
-    pp.query("eps1",                 eps1);
-    pp.query("outer_radius",         R0);
-    pp.query("dielectric_radius",    R1);
-    pp.query("inner_radius",         R2);
-
-    pp.queryarr("outer_center",      v0, 0, SpaceDim);
-    pp.queryarr("dielectric_center", v1, 0, SpaceDim);
-    pp.queryarr("inner_center",      v2, 0, SpaceDim);
-    c0 = RealVect(D_DECL(v0[0], v0[1], v0[2]));
-    c1 = RealVect(D_DECL(v1[0], v1[1], v1[2]));
-    c2 = RealVect(D_DECL(v2[0], v2[1], v2[2]));
-
-    if(pp.contains("turn_off_outer")){
-      pp.get("turn_off_outer", str);
-      turn_off_outer = (str == "true") ? true : false;
-    }
-    if(pp.contains("turn_off_dielectric")){
-      pp.get("turn_off_dielectric", str);
-      turn_off_dielectric = (str == "true") ? true : false;
-    }
-    if(pp.contains("turn_off_inner")){
-      pp.get("turn_off_inner", str);
-      turn_off_inner = (str == "true") ? true : false;
-    }
-    if(pp.contains("outer_live")){
-      pp.get("outer_live", str);
-      outer_live = (str == "true") ? true : false;
-    }
-    if(pp.contains("inner_live")){
-      pp.get("inner_live", str);
-      inner_live = (str == "true") ? true : false;
-    }
-  }
-
-  // Create geometry
-  this->set_eps0(eps0);
-  m_dielectrics.resize(0);
+  // Reset
   m_electrodes.resize(0);
+  m_dielectrics.resize(0);
+  this->set_eps0(1.0);
 
-  if(!turn_off_outer){
-    RefCountedPtr<BaseIF> outer = RefCountedPtr<BaseIF> (new new_sphere_if(c0, R0, true));
-    m_electrodes.push_back(electrode(outer, outer_live, 1.0));
+  if(outer){ // Add outer electrode
+    Real rad;
+    bool live;
+
+    ppOuter.get("radius", rad);
+    ppOuter.get("live",   live);
+
+#if CH_SPACEDIM==2
+    RefCountedPtr<BaseIF> baseif = RefCountedPtr<BaseIF> (new new_sphere_if(e1, rad, true));
+#elif CH_SPACEDIM==3
+    RefCountedPtr<BaseIF> baseif = RefCountedPtr<BaseIF> (new cylinder_if(e1, e2, rad, true));
+#endif
+    m_electrodes.push_back(electrode(baseif, live));
   }
-  if(!turn_off_dielectric){
-    RefCountedPtr<BaseIF> middle = RefCountedPtr<BaseIF> (new new_sphere_if(c1, R1, false));
-    m_dielectrics.push_back(dielectric(middle, eps1));
+
+  if(inner){ // Add inner electrode
+    Real rad;
+    bool live;
+
+    ppInner.get("radius", rad);
+    ppInner.get("live",   live);
+
+#if CH_SPACEDIM==2
+    RefCountedPtr<BaseIF> baseif = RefCountedPtr<BaseIF> (new new_sphere_if(e1, rad, true));
+#elif CH_SPACEDIM==3
+    RefCountedPtr<BaseIF> baseif = RefCountedPtr<BaseIF> (new cylinder_if(e1, e2, rad, true));
+#endif
+    m_electrodes.push_back(electrode(baseif, live));
   }
-  if(!turn_off_inner){
-    RefCountedPtr<BaseIF> inner = RefCountedPtr<BaseIF> (new new_sphere_if(c2, R2, false));
-    m_electrodes.push_back(electrode(inner, inner_live, 1.0));
+
+  if(middle){ // Add dielectric
+    Real rad, eps;
+
+    ppMiddle.get("radius", rad);
+    ppMiddle.get("eps",    eps);
+
+
+#if CH_SPACEDIM==2
+    RefCountedPtr<BaseIF> baseif = RefCountedPtr<BaseIF> (new new_sphere_if(e1, rad, false));
+#elif CH_SPACEDIM==3
+    RefCountedPtr<BaseIF> baseif = RefCountedPtr<BaseIF> (new cylinder_if(e1, e2, rad, false));
+#endif
+    m_dielectrics.push_back(dielectric(baseif, eps));
   }
-  
 }
 
 coaxial_cable::~coaxial_cable(){
