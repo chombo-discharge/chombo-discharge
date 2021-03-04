6fe749166d78ca1ca536a8d50fb3baf340644e35
Robert Marskar <robert.marskar@sintef.no>
2021-03-04 08:44:42 +0100

    Added rough_rod as regression test. Levelset looks fine.

--- geometries/rough_rod/rough_rod.cpp
+++ geometries/rough_rod/rough_rod.cpp
@@ -1,58 +1,45 @@
 /*!
-  @file rough_rod.cpp
-  @brief Implementation of rough_rod.H
+  @file   rough_rod.cpp
+  @brief  Implementation of rough_rod.H
   @author Robert Marskar
-  @date Nov. 2017
+  @date   Nov. 2017
 */
 
 #include "rough_rod.H"
-
-#include <string>
-#include <iostream>
-#include <fstream>
-
-#include <ParmParse.H>
-#include <BaseIF.H>
-
 #include "perlin_rod_if.H"
 
+#include <ParmParse.H>
+
 rough_rod::rough_rod(){
+  this->set_eps0(1.0);
 
-  std::string str;
-  Vector<Real> v(SpaceDim);
-  Real eps0, R0, noise_pers, noise_amp;
-  RealVect f, c0, c1;
-  int noise_octav   = 1;
-  bool reseed, turn_off_rod, live;
-  
   ParmParse pp("rough_rod");
-  pp.get   ("eps0",             eps0);
-  pp.get   ("radius",            R0);
-  pp.get   ("noise_octaves",     noise_octav);
-  pp.get   ("noise_amplitude",   noise_amp);
-  pp.get   ("noise_persistence", noise_pers);
-  pp.get   ("turn_off_rod",      str);               turn_off_rod = (str == "true") ? true : false;
-  pp.get   ("live",              str);                       live = (str == "true") ? true : false;
-  pp.get   ("noise_reseed",      str);               reseed = (str == "true") ? true : false;
-  pp.getarr("center1",           v, 0, SpaceDim); c0 = RealVect(D_DECL(v0[0], v0[1], v0[2]));
-  pp.getarr("center2",           v, 0, SpaceDim); c1 = RealVect(D_DECL(v1[0], v1[1], v1[2]));
-  pp.getarr("noise_frequency",   v, 0, SpaceDim); f  = RealVect(D_DECL(v2[0], v2[1], v2[2]));
 
-  // Create geometry
-  this->set_eps0(eps0);
-  m_dielectrics.resize(0);
-  m_electrodes.resize(0);
-  if(!turn_off_rod){
-    RefCountedPtr<BaseIF> rod = RefCountedPtr<BaseIF> (new perlin_rod_if(R0,
-									 c0,
-									 c1,
-									 false,
-									 noise_amp,
-									 f,
-									 noise_pers,
-									 noise_octav,
-									 reseed));
-    m_electrodes.push_back(electrode(rod, true, 1.0));
+  bool useRod;
+  
+  pp.get("on", useRod);
+
+  if(useRod){
+    bool live, reseed;
+    RealVect f, e1, e2;
+    Real r, persist, amp;
+    int octaves;
+    Vector<Real> v;
+
+    pp.get("radius", r);
+    pp.get("live", live);
+    pp.get("noise_reseed", reseed);
+    pp.get("noise_amplitude", amp);
+    pp.get("noise_octaves", octaves);
+    pp.get("noise_persistence", persist);
+
+    pp.getarr("noise_frequency", v, 0 ,SpaceDim); f  = RealVect(D_DECL(v[0], v[1], v[2]));
+    pp.getarr("endpoint1",       v, 0 ,SpaceDim); e1 = RealVect(D_DECL(v[0], v[1], v[2]));
+    pp.getarr("endpoint2",       v, 0 ,SpaceDim); e2 = RealVect(D_DECL(v[0], v[1], v[2]));
+
+    RefCountedPtr<BaseIF> rod = RefCountedPtr<BaseIF> (new perlin_rod_if(r, e1, e2, false, amp, f, persist, octaves, reseed));
+
+    m_electrodes.push_back(electrode(rod, live));
   }
 }
 
