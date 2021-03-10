643b561f8c5cb279ec13219314ba436562907c4c
Robert Marskar <robert.marskar@sintef.no>
2021-03-10 22:28:47 +0100

    Vec2 is now also template

--- src/geometry/dcel_poly.cpp
+++ src/geometry/dcel_poly.cpp
@@ -43,7 +43,7 @@ bool Polygon2D::isPointInside(const Vec3<double>& a_point, const InsideOutsideAl
 }
 
 bool Polygon2D::isPointInsidePolygonWindingNumber(const Vec3<double>& a_point) const noexcept {
-  const Vec2 p = this->projectPoint(a_point);
+  const Vec2<double> p = this->projectPoint(a_point);
   
   const int wn = this->computeWindingNumber(p);
 
@@ -51,7 +51,7 @@ bool Polygon2D::isPointInsidePolygonWindingNumber(const Vec3<double>& a_point) c
 }
 
 bool Polygon2D::isPointInsidePolygonCrossingNumber(const Vec3<double>& a_point) const noexcept {
-  const Vec2 p = this->projectPoint(a_point);
+  const Vec2<double> p = this->projectPoint(a_point);
   
   const int cn  = this->computeCrossingNumber(p);
     
@@ -61,7 +61,7 @@ bool Polygon2D::isPointInsidePolygonCrossingNumber(const Vec3<double>& a_point) 
 }
 
 bool Polygon2D::isPointInsidePolygonSubtend(const Vec3<double>& a_point) const noexcept {
-  const Vec2 p = this->projectPoint(a_point);
+  const Vec2<double> p = this->projectPoint(a_point);
 
   double sumTheta = this->computeSubtendedAngle(p); // Should be = 2pi if point is inside. 
 
@@ -72,7 +72,7 @@ bool Polygon2D::isPointInsidePolygonSubtend(const Vec3<double>& a_point) const n
   return ret;
 }
 
-bool Polygon2D::isPointOnEdge(const Vec2& a_point, const Vec2& a_endPoint1, const Vec2& a_endPoint2, const double a_thresh) const noexcept{
+bool Polygon2D::isPointOnEdge(const Vec2<double>& a_point, const Vec2<double>& a_endPoint1, const Vec2<double>& a_endPoint2, const double a_thresh) const noexcept{
   const double AB2 = (a_endPoint2 - a_endPoint1).length2();
   const double CA2 = (a_point     - a_endPoint1).length2();
   const double CB2 = (a_point     - a_endPoint2).length2();
@@ -84,13 +84,13 @@ bool Polygon2D::isPointOnEdge(const Vec2& a_point, const Vec2& a_endPoint1, cons
   return ret;
 }
 
-bool Polygon2D::isPointOnBoundary(const Vec2& a_point, const double a_thresh) const noexcept {
+bool Polygon2D::isPointOnBoundary(const Vec2<double>& a_point, const double a_thresh) const noexcept {
   const int N = m_points.size();
 
   bool ret = false;
   for (int i = 0; i < N; i++){
-    const Vec2& p1 = m_points[i];
-    const Vec2& p2 = m_points[(i+1)%N];
+    const Vec2<double>& p1 = m_points[i];
+    const Vec2<double>& p2 = m_points[(i+1)%N];
 
     const bool pointOnLine = this->isPointOnEdge(a_point, p1, p2, a_thresh);
 
@@ -103,8 +103,8 @@ bool Polygon2D::isPointOnBoundary(const Vec2& a_point, const double a_thresh) co
   return ret;
 }
 
-Vec2 Polygon2D::projectPoint(const Vec3<double>& a_point) const noexcept {
-  return Vec2(a_point[m_xDir], a_point[m_yDir]);
+Vec2<double> Polygon2D::projectPoint(const Vec3<double>& a_point) const noexcept {
+  return Vec2<double>(a_point[m_xDir], a_point[m_yDir]);
 }
 
 void Polygon2D::define(const Vec3<double>& a_normal, const std::vector<Vec3<double> >& a_points) {
@@ -137,20 +137,20 @@ void Polygon2D::define(const Vec3<double>& a_normal, const std::vector<Vec3<doub
 
 
 
-int Polygon2D::computeWindingNumber(const Vec2& P) const noexcept {
+int Polygon2D::computeWindingNumber(const Vec2<double>& P) const noexcept {
   int wn = 0;    // the  winding number counter
 
   const int N = m_points.size();
 
-  auto isLeft = [](const Vec2& P0, const Vec2& P1, const Vec2& P2){
+  auto isLeft = [](const Vec2<double>& P0, const Vec2<double>& P1, const Vec2<double>& P2){
     return (P1.x - P0.x)*(P2.y - P0.y) - (P2.x -  P0.x)*(P1.y - P0.y);
   };
 
   // loop through all edges of the polygon
   for (int i = 0; i < N; i++) {   // edge from V[i] to  V[i+1]
 
-    const Vec2& P1 = m_points[i];
-    const Vec2& P2 = m_points[(i+1)%N];
+    const Vec2<double>& P1 = m_points[i];
+    const Vec2<double>& P2 = m_points[(i+1)%N];
 
     const int res = int(isLeft(P1, P2, P));
     
@@ -169,7 +169,7 @@ int Polygon2D::computeWindingNumber(const Vec2& P) const noexcept {
   return wn;
 }
 
-int Polygon2D::computeCrossingNumber(const Vec2& P) const noexcept {
+int Polygon2D::computeCrossingNumber(const Vec2<double>& P) const noexcept {
   int cn = 0; 
 
   const int N = m_points.size();
@@ -177,8 +177,8 @@ int Polygon2D::computeCrossingNumber(const Vec2& P) const noexcept {
   constexpr double thresh = 1.E-6;
   
   for (int i = 0; i < N; i++) {    // edge from V[i]  to V[i+1]
-    const Vec2& P1 = m_points[i];
-    const Vec2& P2 = m_points[(i+1)%N];
+    const Vec2<double>& P1 = m_points[i];
+    const Vec2<double>& P2 = m_points[(i+1)%N];
 
     const bool upwardCrossing   = (P1.y <= P.y) && (P2.y >  P.y);
     const bool downwardCrossing = (P1.y >  P.y) && (P2.y <= P.y);
@@ -195,7 +195,7 @@ int Polygon2D::computeCrossingNumber(const Vec2& P) const noexcept {
   return cn;
 }
 
-double Polygon2D::computeSubtendedAngle(const Vec2& p) const noexcept {
+double Polygon2D::computeSubtendedAngle(const Vec2<double>& p) const noexcept {
   double sumTheta = 0.0;
   
   const int N = m_points.size();
@@ -203,8 +203,8 @@ double Polygon2D::computeSubtendedAngle(const Vec2& p) const noexcept {
   constexpr double thresh = 1.E-6;
   
   for (int i = 0; i < N; i++){
-    const Vec2 p1 = m_points[i]       - p;
-    const Vec2 p2 = m_points[(i+1)%N] - p;
+    const Vec2<double> p1 = m_points[i]       - p;
+    const Vec2<double> p2 = m_points[(i+1)%N] - p;
     
     const double theta1 = atan2(p1.y, p1.x);
     const double theta2 = atan2(p2.y, p2.x);
