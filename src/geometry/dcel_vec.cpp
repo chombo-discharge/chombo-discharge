2ce4d4be2f1d43cc6dc9deab191e5b14b32f47e4
Robert Marskar <robert.marskar@sintef.no>
2021-03-10 22:07:13 +0100

    Added zero and units vectors for Vec2 and Vec3

---
+++ src/geometry/dcel_vec.cpp
@@ -0,0 +1,16 @@
+/*!
+  @file   dcel_vec.cpp
+  @brief  Implementation of dcel_vec.H
+  @author Robert Marskar
+  @date   March 2021
+*/
+
+#include "dcel_vec.H"
+
+using namespace dcel;
+
+const Vec2 Vec2::Zero(0., 0.);
+const Vec2 Vec2::Unit(1., 1.);
+
+const Vec3 Vec3::Zero(0., 0., 0.);
+const Vec3 Vec3::Unit(1., 1., 1.);
