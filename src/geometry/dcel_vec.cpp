cded2be5a5b4282820a649faf7ecafc2287977dd
Robert Marskar <robert.marskar@sintef.no>
2021-03-11 09:10:28 +0100

    Vec2 and Vec3 zero and unit vectors added

--- src/geometry/dcel_vec.cpp
+++ src/geometry/dcel_vec.cpp
@@ -9,8 +9,33 @@
 
 using namespace dcel;
 
-// const Vec2 Vec2::Zero(0., 0.);
-// const Vec2 Vec2::Unit(1., 1.);
+template class Vec2<float>;
+template class Vec2<double>;
 
-// const Vec3 Vec3::Zero(0., 0., 0.);
-// const Vec3 Vec3::Unit(1., 1., 1.);
+template <>
+const Vec2<float> Vec2<float>::Zero(0., 0.);
+
+template <>
+const Vec2<float> Vec2<float>::Unit(1., 1.);
+
+template <>
+const Vec2<double> Vec2<double>::Zero(0., 0.);
+
+template <>
+const Vec2<double> Vec2<double>::Unit(1., 1.);
+
+
+template class Vec3<float>;
+template class Vec3<double>;
+
+template <>
+const Vec3<float> Vec3<float>::Zero(0., 0., 0.);
+
+template <>
+const Vec3<float> Vec3<float>::Unit(1., 1., 1.);
+
+template <>
+const Vec3<double> Vec3<double>::Zero(0., 0., 0.);
+
+template <>
+const Vec3<double> Vec3<double>::Unit(1., 1., 1.);
