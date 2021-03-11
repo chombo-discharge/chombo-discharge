5db2c2a37d7f426f10fc8d9eeec655bc8726efc4
Robert Marskar <robert.marskar@sintef.no>
2021-03-11 21:32:36 +0100

    Using numeric_limits instead of hardcoded big number

--- src/geometry/dcel_vec.cpp
+++ src/geometry/dcel_vec.cpp
@@ -13,29 +13,29 @@ template class Vec2T<float>;
 template class Vec2T<double>;
 
 template <>
-const Vec2T<float> Vec2T<float>::Zero(0., 0.);
+const Vec2T<float> Vec2T<float>::Zero(0.f, 0.f);
 
 template <>
-const Vec2T<float> Vec2T<float>::Unit(1., 1.);
+const Vec2T<float> Vec2T<float>::Unit(1.f, 1.f);
 
 template <>
-const Vec2T<double> Vec2T<double>::Zero(0., 0.);
+const Vec2T<double> Vec2T<double>::Zero(0.d, 0.d);
 
 template <>
-const Vec2T<double> Vec2T<double>::Unit(1., 1.);
+const Vec2T<double> Vec2T<double>::Unit(1.d, 1.d);
 
 
 template class Vec3T<float>;
 template class Vec3T<double>;
 
 template <>
-const Vec3T<float> Vec3T<float>::Zero(0., 0., 0.);
+const Vec3T<float> Vec3T<float>::Zero(0.f, 0.f, 0.f);
 
 template <>
-const Vec3T<float> Vec3T<float>::Unit(1., 1., 1.);
+const Vec3T<float> Vec3T<float>::Unit(1.f, 1.f, 1.f);
 
 template <>
-const Vec3T<double> Vec3T<double>::Zero(0., 0., 0.);
+const Vec3T<double> Vec3T<double>::Zero(0.d, 0.d, 0.d);
 
 template <>
-const Vec3T<double> Vec3T<double>::Unit(1., 1., 1.);
+const Vec3T<double> Vec3T<double>::Unit(1.d, 1.d, 1.d);
