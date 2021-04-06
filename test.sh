c10711b491ce0f637e68efcf7ea953ae66c96a69
Robert Marskar <robert.marskar@sintef.no>
2021-04-06 09:57:29 +0200

    Removed file

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for f in $(find ./regression/ -type f -name '*.inputs')
-do
-    sed -i '21 i amr_mesh.lsf_ghost       = 3           # Number of ghost cells when writing level-set to grid' $f
-done
