a7d4448ed2bc826295d8f95503689db59372eae4
Robert Marskar <robert.marskar@sintef.no>
2021-04-19 11:10:33 +0200

    Made PLASMAC_HOME into DISCHARGE_HOME

--- physics/poisson/setup.py
+++ physics/poisson/setup.py
@@ -15,7 +15,7 @@ parser.add_argument('-procs',           type=int,  help='Processors to use when 
 parser.add_argument('-use_mpi',         type=bool, help='MPI enabled (default true)', default=True)
 parser.add_argument('-build',           type=bool, help='Build executable at end', default=False)
 parser.add_argument('-silent',          type=bool, help='Silent build of executable', default=False)
-parser.add_argument('-plasmac_home',    type=str,  help="Source code base directory", default=os.environ.get('PLASMAC_HOME', os.getcwd()))
+parser.add_argument('-plasmac_home',    type=str,  help="Source code base directory", default=os.environ.get('DISCHARGE_HOME', os.getcwd()))
 parser.add_argument('-base_dir',        type=str,  help="Base directory of mini-app", default="./mini_apps")
 parser.add_argument('-app_name',        type=str,  help="Mini app name. An error message is issued if the name already exists")
 parser.add_argument('-filename',        type=str,  help="File name of main file", default="main")
@@ -28,14 +28,14 @@ app_main.write_template(args)    # Write main file
 app_make.write_template(args)    # Write makefile
 app_options.write_template(args) # Write options file
 
-# Check if PLASMAC_HOME and CHOMBO_HOME has been set
+# Check if DISCHARGE_HOME and CHOMBO_HOME has been set
 plasmac_home = args.plasmac_home
 if not args.plasmac_home:
-    print "Error: Cannot set application because the PLASMAC_HOME environment variable has not been set."
-    print "       Please set PLASMAC_HOME, for example:"
-    print "       >export  PLASMAC_HOME=<directory>"
+    print "Error: Cannot set application because the DISCHARGE_HOME environment variable has not been set."
+    print "       Please set DISCHARGE_HOME, for example:"
+    print "       >export  DISCHARGE_HOME=<directory>"
 else:
-    print "PLASMAC_HOME is " + args.plasmac_home
+    print "DISCHARGE_HOME is " + args.plasmac_home
     print 'Setting up problem in directory ' + args.plasmac_home + "/" + args.base_dir + "/" + args.app_name
 
     app_main.write_template(args)    # Write main file
