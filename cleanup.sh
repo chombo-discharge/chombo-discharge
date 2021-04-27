173c1d81ec8048f4d1ed4354c9bd5c77466ee705
Robert Marskar <robert.marskar@sintef.no>
2021-04-27 08:56:29 +0200

    Changes to regtest and cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -26,37 +26,53 @@ mv physics/poisson/poisson_stepperI.H physics/poisson/field_stepperI.H
 mv physics/poisson/poisson_stepper.options physics/poisson/field_stepper.options
 mv physics/poisson physics/field
 
+# Move regression tests
+mv regression/poisson regression/field
+mv regression/poisson.ini regression/field.ini
+
 
 # Find and replaces field_solver with field_solver stuff in all .H files
 for i in `find . -name "*.H" -type f`;
 do
     sed -i 's/poisson_solver/field_solver/g' $i
-    sed -i 's/POISSON_SOLVER/FIELD_SOLVER/g' $i    
+    sed -i 's/POISSON_SOLVER/FIELD_SOLVER/g' $i
+    sed -i 's/poisson_stepper/field_stepper/g' $i
+    sed -i 's/POISSON_STEPPER/FIELD_STEPPER/g' $i
 done
 for i in `find . -name "*.cpp" -type f`;
 do
     sed -i 's/poisson_solver/field_solver/g' $i
-    sed -i 's/POISSON_SOLVER/FIELD_SOLVER/g' $i    
+    sed -i 's/POISSON_SOLVER/FIELD_SOLVER/g' $i
+    sed -i 's/poisson_stepper/field_stepper/g' $i
+    sed -i 's/POISSON_STEPPER/FIELD_STEPPER/g' $i    
 done
 for i in `find . -name "*.options" -type f`;
 do
     sed -i 's/poisson_solver/field_solver/g' $i
-    sed -i 's/POISSON_SOLVER/FIELD_SOLVER/g' $i        
+    sed -i 's/POISSON_SOLVER/FIELD_SOLVER/g' $i
+    sed -i 's/poisson_stepper/field_stepper/g' $i
+    sed -i 's/POISSON_STEPPER/FIELD_STEPPER/g' $i    
 done
 for i in `find . -name "*.inputs" -type f`;
 do
     sed -i 's/poisson_solver/field_solver/g' $i
-    sed -i 's/POISSON_SOLVER/FIELD_SOLVER/g' $i        
+    sed -i 's/POISSON_SOLVER/FIELD_SOLVER/g' $i
+    sed -i 's/poisson_stepper/field_stepper/g' $i
+    sed -i 's/POISSON_STEPPER/FIELD_STEPPER/g' $i    
 done
 for i in `find . -name "*.py" -type f`;
 do
     sed -i 's/poisson_solver/field_solver/g' $i
-    sed -i 's/POISSON_SOLVER/FIELD_SOLVER/g' $i        
+    sed -i 's/POISSON_SOLVER/FIELD_SOLVER/g' $i
+    sed -i 's/poisson_stepper/field_stepper/g' $i
+    sed -i 's/POISSON_STEPPER/FIELD_STEPPER/g' $i    
 done
 for i in `find . -name "GNUmakefile" -type f`;
 do
     sed -i 's/poisson_solver/field_solver/g' $i
-    sed -i 's/POISSON_SOLVER/FIELD_SOLVER/g' $i        
+    sed -i 's/POISSON_SOLVER/FIELD_SOLVER/g' $i
+    sed -i 's/poisson_stepper/field_stepper/g' $i
+    sed -i 's/POISSON_STEPPER/FIELD_STEPPER/g' $i    
 done
 
 # Find and replaces poisson_multifluid_gmg with field_solver stuff in all .H files
