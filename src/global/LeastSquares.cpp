aedcae9afa00a16ec824a02903c5bc3479cc8754
Robert Marskar <robert.marskar@sintef.no>
2021-05-03 10:08:03 +0200

    Cleanup prep

--- src/global/LeastSquares.cpp
+++ src/global/LeastSquares.cpp
@@ -11,14 +11,16 @@
 #include "EBArith.H"
 
 Vector<Real> LeastSquares::makeDiagWeights(const Vector<RealVect>& a_displacements, const int a_pow){
-  Vector<Real> ret;
+  Vector<Real> ret(a_displacements.size(), 1.0);
 
-  for (int i = 0; i < a_displacements.size(); i++){
-    const RealVect& d = a_displacements[i];
-    const Real len    = d.vectorLength();
-    const Real w      = 1./std::pow(len, a_pow);
+  if(a_pow > 0){
+    for (int i = 0; i < a_displacements.size(); i++){
+      const RealVect& d = a_displacements[i];
+      const Real len    = d.vectorLength();
+      const Real w      = 1./std::pow(len, a_pow);
 
-    ret.push_back(w);
+      ret[i] = w;
+    }
   }
 
   return ret;
@@ -40,9 +42,9 @@ Vector<RealVect> LeastSquares::getCenterToCentroidDisplacements(const VolIndex& 
 }
 
 Vector<RealVect> LeastSquares::getCenterToEBDisplacements(const VolIndex&         a_vof,
-								  const Vector<VolIndex>& a_monoVoFs,
-								  const EBISBox&          a_ebisbox,
-								  const Real&             a_dx){
+							  const Vector<VolIndex>& a_monoVoFs,
+							  const EBISBox&          a_ebisbox,
+							  const Real&             a_dx){
   Vector<RealVect> displacements(a_monoVoFs.size());
   const RealVect ebCentroid = RealVect(a_vof.gridIndex()) + a_ebisbox.bndryCentroid(a_vof);
   for (int i = 0; i < a_monoVoFs.size(); i++){
@@ -68,6 +70,138 @@ Vector<RealVect> LeastSquares::getCentroidToCenterDisplacements(const VolIndex& 
   }
 
   return displacements;
+}
+
+bool LeastSquares::getLSqGradientStencil(VoFStencil&             a_stencil,
+					 const Vector<VolIndex>& a_monoVoFs,
+					 const Vector<RealVect>& a_displ,
+					 const int               a_Q,
+					 const int               a_pow){
+  bool foundStencil;
+
+  // Build a vector of weights for the least squares method. The weights are given by the inverse distance
+  Vector<Real> wi = makeDiagWeights(a_displ, a_pow);
+
+  // Size of the linear system
+  Vector<MultiIndex> indices = LeastSquares::getMultiIndicesLexiOrder(a_Q);
+  int k;
+  int M = LeastSquares::getTaylorExpansionSize(a_Q);
+  int N = LeastSquares::getTaylorExpansionSize(a_Q);
+  int K = a_displ.size();
+  CH_assert(K >= M);
+
+  // Build the A-matrix so we can use LaPackUtils::computePseudoInverse
+  k = 0;
+  Vector<Real> linA(M*N);
+  for (MultiIndex q(IntVect::Zero); q <= a_Q; q.next(a_Q)){
+    for (MultiIndex p(IntVect::Zero); p <= a_Q; p.next(a_Q)){
+      
+      linA[k] = 0.;
+      for (int i = 0; i < K; i++){
+	linA[k] += wi[i]*wi[i]*MultiIndex::pow(a_displ[i], q)*MultiIndex::pow(a_displ[i], p)/(q.factorial()*p.factorial());
+      }
+
+      k++;
+    }
+  }
+
+  // Build the B-matrix
+  k = 0;
+  double B[N*K];
+  for (int i = 0; i < K; i++){
+    for (MultiIndex p(IntVect::Zero); p <= a_Q; p.next(a_Q)){
+      B[k] = wi[i]*wi[i]*MultiIndex::pow(a_displ[i], p)/p.factorial();
+
+      k++;
+    }
+  }
+
+  // Let LaPack compute the Moore-Penrose pseudoinverse
+  Vector<Real> linAPlus(M*N, 0.0);
+  const bool foundSVD = LaPackUtils::computePseudoInverse(linAPlus.stdVector(), linA.stdVector(), M, N);
+
+  if(foundSVD){
+
+    // Must have the pseudoinverse in something useable by Fortran
+    double Aplus[M*N];
+    for (int i = 0; i < M*N; i++){
+      Aplus[i] = linAPlus[i];
+    }
+
+    // Now compute Aplus*B which will be an M*K matrix
+    double C[M*K];
+    char TRANSA = 'N';
+    char TRANSB = 'N';
+    double ALPHA = 1.0;
+    double BETA  = 0.0;
+    dgemm_(&TRANSA,
+	   &TRANSB,
+	   &M,
+	   &K,
+	   &M,
+	   &ALPHA,
+	   Aplus,
+	   &M,
+	   B,
+	   &M,
+	   &BETA,
+	   C,
+	   &M);
+
+    if(a_Q == 1){
+      // The stencil is now found in the 2nd, 3rd, and 4th rows of C, which is an (M x K) matrix
+      a_stencil.clear();
+      for (int dir = 0; dir < SpaceDim; dir++){
+	for (int j = 0; j < K; j++){
+	  const VolIndex& jvof = a_monoVoFs[j]; 
+	  const int k = M*j + (dir + 1);
+	  a_stencil.add(jvof, C[k], dir);
+	}
+	foundStencil = true;
+      }
+    }
+    else {
+      MayDay::Abort("LeastSquares::getLSqGradStencil only works for a_Q = 1. This is a work in progress...");
+    }
+  }
+  else{
+    foundStencil = false;
+  }
+
+  return foundStencil;
+}
+
+bool LeastSquares::getEbCentroidGradientStencil(VoFStencil&     a_stencil,
+						const VolIndex& a_vof,
+						const EBISBox&  a_ebisbox,
+						const Real&     a_dx,
+						const int       a_order,
+						const int       a_radius,
+						const int       a_weight){
+  int radius = a_radius;
+  Vector<VolIndex> monoVoFs;
+  const int numTaylorTerms = LeastSquares::getTaylorExpansionSize(a_order);
+
+  // Can or must we drop order?
+  const bool dropOrder = LeastSquares::getVoFsRadius(monoVoFs, a_vof, a_ebisbox, numTaylorTerms, a_order, a_radius);
+
+  bool foundStencil = false;
+  
+  if(!dropOrder){
+    // Get the displacement vectors
+    Vector<RealVect> displacements = LeastSquares::getCenterToEBDisplacements(a_vof, monoVoFs, a_ebisbox, a_dx);
+
+    // Build least squarse system and solve it
+    foundStencil = LeastSquares::getLSqGradientStencil(a_stencil, monoVoFs, displacements, a_order, a_weight);
+
+    // Drop order if we could not find stencil
+    if(!foundStencil){
+      pout() << "LeastSquares::getEbCentroidGradientStencil - could not find stencil" << endl;
+      a_stencil.clear();
+    }
+  }
+
+  return foundStencil;
 }
 
 bool LeastSquares::getCenterToCentroidInterpStencil(VoFStencil&     a_stencil,
@@ -118,13 +252,11 @@ bool LeastSquares::getCenterToCentroidInterpStencil(VoFStencil&     a_stencil,
 }
 
 bool LeastSquares::getCenterToEBInterpStencil(VoFStencil&     a_stencil,
-						      const VolIndex& a_vof,
-						      const EBISBox&  a_ebisbox,
-						      const Real&     a_dx,
-						      const int       a_order,
-						      const int       a_radius){
-  CH_TIME("LeastSquares::getCenterToEBInterpStencil");
-
+					      const VolIndex& a_vof,
+					      const EBISBox&  a_ebisbox,
+					      const Real&     a_dx,
+					      const int       a_order,
+					      const int       a_radius){
   bool foundStencil;
   int radius = a_radius;
   Vector<VolIndex> monoVoFs;
@@ -165,45 +297,7 @@ bool LeastSquares::getCenterToEBInterpStencil(VoFStencil&     a_stencil,
   return foundStencil;
 }
 
-bool LeastSquares::getEbCentroidGradientStencil(VoFStencil&     a_stencil,
-						const VolIndex& a_vof,
-						const EBISBox&  a_ebisbox,
-						const Real&     a_dx,
-						const int       a_order,
-						const int       a_radius){
-  CH_TIME("LeastSquares::getEbCentroidGradientStencil");
 
-  int radius = a_radius;
-  Vector<VolIndex> monoVoFs;
-  const int numTaylorTerms = LeastSquares::getTaylorExpansionSize(a_order);
-
-  // Can or must we drop order?
-  bool dropOrder = false;
-  const RealVect vofCentroid = a_ebisbox.bndryCentroid(a_vof);
-  if(vofCentroid.vectorLength() < LeastSquares::s_tol){ 
-    dropOrder = true;
-  }
-  else{
-    dropOrder = LeastSquares::getVoFsRadius(monoVoFs, a_vof, a_ebisbox, numTaylorTerms, a_order, a_radius);
-  }
-
-  bool foundStencil = false;
-  if(!dropOrder){
-    // Get the displacement vectors
-    Vector<RealVect> displacements = LeastSquares::getCenterToEBDisplacements(a_vof, monoVoFs, a_ebisbox, a_dx);
-
-    // Build least squarse system and solve it
-    foundStencil = LeastSquares::getLSqGradientStencil(a_stencil, monoVoFs, displacements, a_order);
-
-    // Drop order if we could not find stencil
-    if(!foundStencil){
-      pout() << "LeastSquares::getEbCentroidGradientStencil - could not find stencil" << endl;
-      a_stencil.clear();
-    }
-  }
-
-  return foundStencil;
-}
 
 bool LeastSquares::getCentroidToCenterInterpStencil(VoFStencil&     a_stencil,
 						    const VolIndex& a_vof,
@@ -211,8 +305,6 @@ bool LeastSquares::getCentroidToCenterInterpStencil(VoFStencil&     a_stencil,
 						    const Real&     a_dx,
 						    const int       a_order,
 						    const int       a_radius){
-  CH_TIME("LeastSquares::getCentroidToCenterInterpStencil");
-
   bool foundStencil;
   int radius = a_radius;
   Vector<VolIndex> monoVoFs;
@@ -255,9 +347,6 @@ bool LeastSquares::getCentroidToCenterInterpStencil(VoFStencil&     a_stencil,
 }
 
 Vector<MultiIndex> LeastSquares::getMultiIndicesLexiOrder(const int a_Q){
-  CH_TIME("LeastSquares::getMultiIndicesLexiOrder");
-
-
   Vector<MultiIndex> ret;
   for (MultiIndex cur = IntVect::Zero; cur <= a_Q; cur.next(a_Q)){
     ret.push_back(cur);
@@ -280,8 +369,6 @@ bool LeastSquares::getLSqInterpStencil(VoFStencil&             a_stencil,
 				       const Vector<VolIndex>& a_monoVoFs,
 				       const Vector<RealVect>& a_displ,
 				       const int               a_Q){
-  CH_TIME("LeastSquares::getLSqInterpStencil");
-
   bool foundStencil;
 
   // Build a vector of weights for the least squares method. The weights are given by the inverse distance
@@ -374,111 +461,7 @@ bool LeastSquares::getLSqInterpStencil(VoFStencil&             a_stencil,
   return foundStencil;
 }
 
-bool LeastSquares::getLSqGradientStencil(VoFStencil&             a_stencil,
-					 const Vector<VolIndex>& a_monoVoFs,
-					 const Vector<RealVect>& a_displ,
-					 const int               a_Q){
-  CH_TIME("LeastSquares::getLSqGradientStencil");
 
-  bool foundStencil;
-
-  // Build a vector of weights for the least squares method. The weights are given by the inverse distance
-  Vector<Real> wi;
-  for (int i = 0; i < a_displ.size(); i++){
-    wi.push_back(1.0/sqrt(a_displ[i].vectorLength()));
-  }
-
-  // Size of the linear system
-  Vector<MultiIndex> indices = LeastSquares::getMultiIndicesLexiOrder(a_Q);
-  int k;
-  int M = LeastSquares::getTaylorExpansionSize(a_Q);
-  int N = LeastSquares::getTaylorExpansionSize(a_Q);
-  int K = a_displ.size();
-  CH_assert(K >= M);
-
-  // Build the A-matrix so we can use LaPackUtils::computePseudoInverse
-  k = 0;
-  Vector<Real> linA(M*N);
-  for (MultiIndex q(IntVect::Zero); q <= a_Q; q.next(a_Q)){
-    for (MultiIndex p(IntVect::Zero); p <= a_Q; p.next(a_Q)){
-      
-      //
-      linA[k] = 0.;
-      for (int i = 0; i < K; i++){
-	linA[k] += wi[i]*wi[i]*MultiIndex::pow(a_displ[i], q)*MultiIndex::pow(a_displ[i], p)/(q.factorial()*p.factorial());
-      }
-
-      k++;
-    }
-  }
-
-  // Build the B-matrix
-  k = 0;
-  double B[N*K];
-  for (int i = 0; i < K; i++){
-    for (MultiIndex p(IntVect::Zero); p <= a_Q; p.next(a_Q)){
-      B[k] = wi[i]*wi[i]*MultiIndex::pow(a_displ[i], p)/p.factorial();
-
-      k++;
-    }
-
-    
-  }
-
-  // Let LaPack compute the Moore-Penrose pseudoinverse
-  Vector<Real> linAPlus(M*N, 0.0);
-  const bool foundSVD = LaPackUtils::computePseudoInverse(linAPlus.stdVector(), linA.stdVector(), M, N);
-
-  if(foundSVD){
-
-    // Must have the pseudoinverse in something useable by Fortran
-    double Aplus[M*N];
-    for (int i = 0; i < M*N; i++){
-      Aplus[i] = linAPlus[i];
-    }
-
-    // Now compute Aplus*B which will be an M*K matrix
-    double C[M*K];
-    char TRANSA = 'N';
-    char TRANSB = 'N';
-    double ALPHA = 1.0;
-    double BETA  = 0.0;
-    dgemm_(&TRANSA,
-	   &TRANSB,
-	   &M,
-	   &K,
-	   &M,
-	   &ALPHA,
-	   Aplus,
-	   &M,
-	   B,
-	   &M,
-	   &BETA,
-	   C,
-	   &M);
-
-    if(a_Q == 1){
-      // The stencil is now found in the 2nd, 3rd, and 4th rows of C, which is an (M x K) matrix
-      a_stencil.clear();
-      for (int dir = 0; dir < SpaceDim; dir++){
-	for (int j = 0; j < K; j++){
-	  const VolIndex& jvof = a_monoVoFs[j]; 
-	  const int k = M*j + (dir + 1);
-	  a_stencil.add(jvof, C[k], dir);
-	}
-	foundStencil = true;
-      }
-    }
-    else {
-      MayDay::Abort("LeastSquares::getLSqGradStencil only works for a_Q = 1. This is a work in progress...");
-    }
-  }
-  else{
-    foundStencil = false;
-  }
-
-  return foundStencil;
-}
 
 bool LeastSquares::getVoFsRadius(Vector<VolIndex>& a_allVoFsRad,
 				 const VolIndex&   a_vof,
@@ -486,8 +469,6 @@ bool LeastSquares::getVoFsRadius(Vector<VolIndex>& a_allVoFsRad,
 				 const int         a_minVoFs,
 				 const int         a_order, 
 				 const int         a_radius){
-  CH_TIME("LeastSquares::getVoFsRadius");
-
   // Get VoFs
   int radius     = a_radius;
   bool dropOrder = false;
