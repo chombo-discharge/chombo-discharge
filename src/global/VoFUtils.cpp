a330f8af5e983b8a7428c719bffc3e61e9de3d2d
Robert Marskar <robert.marskar@sintef.no>
2021-05-06 10:19:15 +0200

    WIP on VoFUtils

--- src/global/VoFUtils.cpp
+++ src/global/VoFUtils.cpp
@@ -16,7 +16,21 @@ bool VoFUtils::isQuadrantWellDefined(const RealVect a_normal){
   const RealVect v = a_normal/a_normal.vectorLength(); // Idiot guard. 
   
   for (int dir = 0; dir < SpaceDim; dir++){
-    if (v[dir] == 1.0 || v[dir] == 0.0) ret = false;
+    if (std::abs(v[dir]) == 1.0) ret = false;
+  }
+
+  return ret;
+}
+
+int VoFUtils::getCardinalDirection(const RealVect a_normal){
+  int ret = -1;
+  
+  const RealVect v = a_normal/a_normal.vectorLength(); // Idiot guard.
+  
+  for (int dir = 0; dir < SpaceDim; dir++){
+    if (std::abs(v[dir]) == 1.0) {
+      ret = int(v[dir]);
+    }
   }
 
   return ret;
@@ -73,41 +87,75 @@ Vector<VolIndex> VoFUtils::getAllVoFsInQuadrant(const VolIndex& a_startVoF, cons
     if(a_addStartVoF){
       ret.push_back(a_startVoF);
     }
-
-    return ret;
-  }
-
-  
-
-  IntVect quadrant;
-  for (int dir = 0; dir < SpaceDim; dir++){
-    if(a_normal[dir] <= 0.){
-      quadrant[dir] = -1;
+  } // Should be able to remove this after we've done the "symmetric quadrant"
+  else{
+    IntVect quadrant;
+    for (int dir = 0; dir < SpaceDim; dir++){
+      if(a_normal[dir] <= 0.){
+	quadrant[dir] = -1;
+      }
+      else{
+	quadrant[dir] = 1;
+      }
     }
-    else{
-      quadrant[dir] = 1;
-    }
-  }
 
 
   
-  IntVect iv0 = a_startVoF.gridIndex();
-  IntVect iv1 = iv0 + BASISV(0)*quadrant[0]                         ;
-  IntVect iv2 = iv0                         + BASISV(1)*quadrant[1] ;
-  IntVect iv3 = iv0 + BASISV(0)*quadrant[0] + BASISV(1)*quadrant[1] ;
+    IntVect iv0 = a_startVoF.gridIndex();
+    IntVect iv1 = iv0 + BASISV(0)*quadrant[0]                         ;
+    IntVect iv2 = iv0                         + BASISV(1)*quadrant[1] ;
+    IntVect iv3 = iv0 + BASISV(0)*quadrant[0] + BASISV(1)*quadrant[1] ;
 
-  VolIndex vof1, vof2, vof3;
+    VolIndex vof1, vof2, vof3;
 
-  if(a_ebisbox.getVoFs(iv1).size() > 0) ret.push_back(VolIndex(iv1, 0));
-  if(a_ebisbox.getVoFs(iv2).size() > 0) ret.push_back(VolIndex(iv2, 0));
-  if(a_ebisbox.getVoFs(iv3).size() > 0) ret.push_back(VolIndex(iv3, 0));
+    if(a_ebisbox.getVoFs(iv1).size() > 0) ret.push_back(VolIndex(iv1, 0));
+    if(a_ebisbox.getVoFs(iv2).size() > 0) ret.push_back(VolIndex(iv2, 0));
+    if(a_ebisbox.getVoFs(iv3).size() > 0) ret.push_back(VolIndex(iv3, 0));
 
-  if(a_addStartVoF){
-    ret.push_back(a_startVoF);
+    if(a_addStartVoF){
+      ret.push_back(a_startVoF);
+    }
   }
   
   return ret;
 }
+
+Vector<VolIndex> getAllVoFsSymmetric(const VolIndex& a_startVoF, const EBISBox& a_ebisbox, const int a_normal, const int a_radius, const bool a_addStartVoF){
+  Vector<VolIndex> ret;
+
+  Box bx(a_startVoF.gridIndex(), a_startVoF.gridIndex());
+  for (int dir = 0; dir < SpaceDim; dir++){
+    if(dir == a_normal){
+      bx.growHi(a_radius);
+    }
+    else if(dir == -a_normal){
+      bx.growLo(a_radius);
+    }
+    else{
+      bx.growLo(dir, a_radius);
+      bx.growHi(dir, a_radius);
+    }
+  }
+  bx &= a_ebisbox.getDomain().domainBox();
+
+  // Get all VoFs that can be reached with a monotone path and add them if they fall within the above box.
+  Vector<VolIndex> monoVoFs;
+  EBArith::getAllVoFsInMonotonePath(monoVoFs, a_startVoF, a_ebisbox, a_radius);
+  for (auto& ivof : monoVoFs.stdVector()){
+    if(bx.contains(ivof.gridIndex()) && ivof != a_startVoF){
+      ret.push_back(ivof);
+    }
+  }
+    
+  if(a_addStartVoF){
+    ret.push_back(a_startVoF);
+  }
+
+  return ret;
+  
+}
+
+																				  
 
 Vector<VolIndex> VoFUtils::getAllVoFsInRadius(const VolIndex& a_startVoF, const EBISBox& a_ebisbox, const int a_radius, const bool a_addStartVoF){
 
