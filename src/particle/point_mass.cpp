182f5d8f1b9e6c4b1d191340bc2aca7f770eef09
Robert Marskar <robert.marskar@sintef.no>
2021-05-07 19:49:12 +0200

    Namespace added to src/particle

--- src/particle/point_mass.cpp
+++ src/particle/point_mass.cpp
@@ -7,35 +7,38 @@
 
 #include "point_mass.H"
 
-point_mass::point_mass(){
+namespace ChomboDischarge {
+  
+  point_mass::point_mass(){
 
-}
-
-point_mass::point_mass(const RealVect a_pos, const Real a_mass, const Real a_energy){
-  m_pos    = a_pos;
-  m_mass   = a_mass;
-  m_energy = a_energy;
-}
-
-point_mass::point_mass(const std::vector<point_mass>& a_point_masses){
-  m_pos    = RealVect::Zero;
-  m_mass   = 0.0;
-  m_energy = 0.0;
-
-  for (int i = 0; i < a_point_masses.size(); i++){
-    const RealVect& p = a_point_masses[i].m_pos;
-    const Real& m     = a_point_masses[i].m_mass;
-    const Real& e     = a_point_masses[i].m_energy;
-    
-    m_mass   += m;
-    m_pos    += m*p;
-    m_energy += m*e;
   }
 
-  m_pos    = m_pos/m_mass;
-  m_energy = m_energy/m_mass;
-}
+  point_mass::point_mass(const RealVect a_pos, const Real a_mass, const Real a_energy){
+    m_pos    = a_pos;
+    m_mass   = a_mass;
+    m_energy = a_energy;
+  }
 
-point_mass::~point_mass(){
+  point_mass::point_mass(const std::vector<point_mass>& a_point_masses){
+    m_pos    = RealVect::Zero;
+    m_mass   = 0.0;
+    m_energy = 0.0;
 
+    for (int i = 0; i < a_point_masses.size(); i++){
+      const RealVect& p = a_point_masses[i].m_pos;
+      const Real& m     = a_point_masses[i].m_mass;
+      const Real& e     = a_point_masses[i].m_energy;
+    
+      m_mass   += m;
+      m_pos    += m*p;
+      m_energy += m*e;
+    }
+
+    m_pos    = m_pos/m_mass;
+    m_energy = m_energy/m_mass;
+  }
+
+  point_mass::~point_mass(){
+
+  }
 }
