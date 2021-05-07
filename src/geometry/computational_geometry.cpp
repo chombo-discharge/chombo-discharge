8aadd0676d7ea94d841d90694c8d09372b5c1bb0
Robert Marskar <robert.marskar@sintef.no>
2021-05-07 20:00:39 +0200

    Some geo-stuff to namespace

--- src/geometry/computational_geometry.cpp
+++ src/geometry/computational_geometry.cpp
@@ -18,126 +18,128 @@
 #include "ScanShop.H"
 #include "memrep.H"
 
+namespace ChomboDischarge {
+
 #if CH_USE_DOUBLE
-Real computational_geometry::s_thresh = 1.E-15;
+  Real computational_geometry::s_thresh = 1.E-15;
 #elif CH_USE_FLOAT
-Real computational_geometry::s_thresh = 1.E-6;
+  Real computational_geometry::s_thresh = 1.E-6;
 #endif
 
-bool          computational_geometry::s_use_new_gshop = false;
-ProblemDomain computational_geometry::s_ScanDomain = ProblemDomain(); // Needs to be set
+  bool          computational_geometry::s_use_new_gshop = false;
+  ProblemDomain computational_geometry::s_ScanDomain = ProblemDomain(); // Needs to be set
 
-computational_geometry::computational_geometry(){
-  m_eps0 = 1.0;
-  m_electrodes.resize(0);
-  m_dielectrics.resize(0);
+  computational_geometry::computational_geometry(){
+    m_eps0 = 1.0;
+    m_electrodes.resize(0);
+    m_dielectrics.resize(0);
   
-  m_mfis = RefCountedPtr<mfis> (new mfis());
-}
+    m_mfis = RefCountedPtr<mfis> (new mfis());
+  }
 
-computational_geometry::~computational_geometry(){}
+  computational_geometry::~computational_geometry(){}
 
-const Vector<dielectric>& computational_geometry::get_dielectrics() const  {
-  return m_dielectrics;
-}
+  const Vector<dielectric>& computational_geometry::get_dielectrics() const  {
+    return m_dielectrics;
+  }
 
-const Vector<electrode>& computational_geometry::get_electrodes() const  {
-  return m_electrodes;
-}
+  const Vector<electrode>& computational_geometry::get_electrodes() const  {
+    return m_electrodes;
+  }
 
-const RefCountedPtr<BaseIF>& computational_geometry::get_gas_if() const{
-  return m_gas_if;
-}
+  const RefCountedPtr<BaseIF>& computational_geometry::get_gas_if() const{
+    return m_gas_if;
+  }
 
-const RefCountedPtr<BaseIF>& computational_geometry::get_sol_if() const{
-  return m_sol_if;
-}
+  const RefCountedPtr<BaseIF>& computational_geometry::get_sol_if() const{
+    return m_sol_if;
+  }
 
-const Real& computational_geometry::get_eps0() const {
-  return m_eps0;
-}
+  const Real& computational_geometry::get_eps0() const {
+    return m_eps0;
+  }
 
-const RefCountedPtr<mfis>& computational_geometry::get_mfis() const {
-  return m_mfis;
-}
+  const RefCountedPtr<mfis>& computational_geometry::get_mfis() const {
+    return m_mfis;
+  }
 
-void computational_geometry::set_dielectrics(Vector<dielectric>& a_dielectrics){
-  m_dielectrics = a_dielectrics;
-}
+  void computational_geometry::set_dielectrics(Vector<dielectric>& a_dielectrics){
+    m_dielectrics = a_dielectrics;
+  }
 
-void computational_geometry::set_electrodes(Vector<electrode>& a_electrodes){
-  m_electrodes = a_electrodes;
-}
+  void computational_geometry::set_electrodes(Vector<electrode>& a_electrodes){
+    m_electrodes = a_electrodes;
+  }
 
-void computational_geometry::set_eps0(const Real a_eps0){
-  m_eps0 = a_eps0;
-}
+  void computational_geometry::set_eps0(const Real a_eps0){
+    m_eps0 = a_eps0;
+  }
 
-void computational_geometry::build_geometries(const ProblemDomain   a_finestDomain,
-					      const RealVect        a_origin,
-					      const Real            a_finestDx,
-					      const int             a_nCellMax,
-					      const int             a_maxCoarsen){
+  void computational_geometry::build_geometries(const ProblemDomain   a_finestDomain,
+						const RealVect        a_origin,
+						const Real            a_finestDx,
+						const int             a_nCellMax,
+						const int             a_maxCoarsen){
 
-  // Build geoservers
-  Vector<GeometryService*> geoservers(2, NULL);
-  this->build_gas_geoserv(geoservers[phase::gas],     a_finestDomain, a_origin, a_finestDx);
-  this->build_solid_geoserv(geoservers[phase::solid], a_finestDomain, a_origin, a_finestDx);
+    // Build geoservers
+    Vector<GeometryService*> geoservers(2, NULL);
+    this->build_gas_geoserv(geoservers[phase::gas],     a_finestDomain, a_origin, a_finestDx);
+    this->build_solid_geoserv(geoservers[phase::solid], a_finestDomain, a_origin, a_finestDx);
 
-  m_mfis->define(a_finestDomain.domainBox(), // Define MF
-		 a_origin,
-		 a_finestDx,
-		 geoservers,
-		 a_nCellMax,
-		 a_maxCoarsen);
+    m_mfis->define(a_finestDomain.domainBox(), // Define MF
+		   a_origin,
+		   a_finestDx,
+		   geoservers,
+		   a_nCellMax,
+		   a_maxCoarsen);
 
-  for (int i = 0; i < 2; i++){
-    if(geoservers[i] != NULL){
-      delete geoservers[i];
+    for (int i = 0; i < 2; i++){
+      if(geoservers[i] != NULL){
+	delete geoservers[i];
+      }
     }
   }
-}
 
-void computational_geometry::build_geo_from_files(const std::string&   a_gas_file,
-						  const std::string&   a_sol_file){
+  void computational_geometry::build_geo_from_files(const std::string&   a_gas_file,
+						    const std::string&   a_sol_file){
 
-  RefCountedPtr<EBIndexSpace>& ebis_gas = m_mfis->get_ebis(phase::gas);
-  RefCountedPtr<EBIndexSpace>& ebis_sol = m_mfis->get_ebis(phase::solid);
+    RefCountedPtr<EBIndexSpace>& ebis_gas = m_mfis->get_ebis(phase::gas);
+    RefCountedPtr<EBIndexSpace>& ebis_sol = m_mfis->get_ebis(phase::solid);
 
-  // Define gas phase
-  HDF5Handle gas_handle(a_gas_file.c_str(), HDF5Handle::OPEN_RDONLY);
-  ebis_gas->define(gas_handle);
-  gas_handle.close();
+    // Define gas phase
+    HDF5Handle gas_handle(a_gas_file.c_str(), HDF5Handle::OPEN_RDONLY);
+    ebis_gas->define(gas_handle);
+    gas_handle.close();
 
-  if(m_dielectrics.size() > 0){
-    HDF5Handle sol_handle(a_sol_file.c_str(), HDF5Handle::OPEN_RDONLY);
-    ebis_sol->define(sol_handle);
-    sol_handle.close();
+    if(m_dielectrics.size() > 0){
+      HDF5Handle sol_handle(a_sol_file.c_str(), HDF5Handle::OPEN_RDONLY);
+      ebis_sol->define(sol_handle);
+      sol_handle.close();
+    }
+    else {
+      ebis_sol = RefCountedPtr<EBIndexSpace> (NULL);
+    }
   }
-  else {
-    ebis_sol = RefCountedPtr<EBIndexSpace> (NULL);
-  }
-}
 
-void computational_geometry::build_gas_geoserv(GeometryService*&   a_geoserver,
-					       const ProblemDomain a_finestDomain,
-					       const RealVect      a_origin,
-					       const Real          a_dx){
+  void computational_geometry::build_gas_geoserv(GeometryService*&   a_geoserver,
+						 const ProblemDomain a_finestDomain,
+						 const RealVect      a_origin,
+						 const Real          a_dx){
   
-  // The gas ebis is the intersection of electrodes and dielectrics
-  Vector<BaseIF*> parts;
-  for (int i = 0; i < m_dielectrics.size(); i++){
-    parts.push_back(&(*(m_dielectrics[i].get_function())));
-  }
-  for (int i = 0; i < m_electrodes.size(); i++){
-    parts.push_back(&(*(m_electrodes[i].get_function())));
-  }
+    // The gas ebis is the intersection of electrodes and dielectrics
+    Vector<BaseIF*> parts;
+    for (int i = 0; i < m_dielectrics.size(); i++){
+      parts.push_back(&(*(m_dielectrics[i].get_function())));
+    }
+    for (int i = 0; i < m_electrodes.size(); i++){
+      parts.push_back(&(*(m_electrodes[i].get_function())));
+    }
 
-  // Create geoshop; either all regular or an intersection
-  // if(parts.size() == 0){ 
-  //   a_geoserver = new AllRegularService();
-  // }
-  // else {
+    // Create geoshop; either all regular or an intersection
+    // if(parts.size() == 0){ 
+    //   a_geoserver = new AllRegularService();
+    // }
+    // else {
     m_gas_if = RefCountedPtr<BaseIF> (new IntersectionIF(parts));
     if(s_use_new_gshop){
       a_geoserver = static_cast<GeometryService*> (new ScanShop(*m_gas_if,
@@ -152,49 +154,50 @@ void computational_geometry::build_gas_geoserv(GeometryService*&   a_geoserver,
       a_geoserver = static_cast<GeometryService*> (new GeometryShop(*m_gas_if, 0, a_dx*RealVect::Unit, s_thresh));
     }
     //  }
-}
-
-void computational_geometry::build_solid_geoserv(GeometryService*&   a_geoserver,
-						 const ProblemDomain a_finestDomain,
-						 const RealVect      a_origin,
-						 const Real          a_dx){
-
-  // The gas ebis is the intersection of dielectrics
-  Vector<BaseIF*> parts_dielectrics, parts_electrodes;
-  for (int i = 0; i < m_dielectrics.size(); i++){
-    parts_dielectrics.push_back(&(*m_dielectrics[i].get_function()));
-  }
-  for (int i = 0; i < m_electrodes.size(); i++){
-    parts_electrodes.push_back(&(*m_electrodes[i].get_function()));
   }
 
-  // Create EBIndexSpace. If there are no solid phases, return null
-  if(parts_dielectrics.size() == 0){
-    a_geoserver = NULL;
-  }
-  else {
-    Vector<BaseIF*> parts;
+  void computational_geometry::build_solid_geoserv(GeometryService*&   a_geoserver,
+						   const ProblemDomain a_finestDomain,
+						   const RealVect      a_origin,
+						   const Real          a_dx){
 
-    RefCountedPtr<BaseIF> diel_baseif = RefCountedPtr<BaseIF> (new IntersectionIF(parts_dielectrics)); // Gas outside dielectric
-    RefCountedPtr<BaseIF> elec_baseif = RefCountedPtr<BaseIF> (new IntersectionIF(parts_electrodes));  // Gas outside electrodes
-    RefCountedPtr<BaseIF> diel_compif = RefCountedPtr<BaseIF> (new ComplementIF(*diel_baseif));        // Inside of dielectrics
-
-    parts.push_back(&(*diel_compif)); // Parts for intersection
-    parts.push_back(&(*elec_baseif)); // Parts for intersection
-    
-    m_sol_if = RefCountedPtr<BaseIF> (new IntersectionIF(parts)); 
-
-    if(s_use_new_gshop){
-      a_geoserver = static_cast<GeometryService*> (new ScanShop(*m_sol_if,
-								0,
-								a_dx,
-								a_origin,
-								a_finestDomain,
-								s_ScanDomain,
-								s_thresh));
+    // The gas ebis is the intersection of dielectrics
+    Vector<BaseIF*> parts_dielectrics, parts_electrodes;
+    for (int i = 0; i < m_dielectrics.size(); i++){
+      parts_dielectrics.push_back(&(*m_dielectrics[i].get_function()));
     }
-    else{ // Chombo geometry generation
-      a_geoserver = static_cast<GeometryService*> (new GeometryShop(*m_sol_if, 0, a_dx*RealVect::Unit, s_thresh));
+    for (int i = 0; i < m_electrodes.size(); i++){
+      parts_electrodes.push_back(&(*m_electrodes[i].get_function()));
+    }
+
+    // Create EBIndexSpace. If there are no solid phases, return null
+    if(parts_dielectrics.size() == 0){
+      a_geoserver = NULL;
+    }
+    else {
+      Vector<BaseIF*> parts;
+
+      RefCountedPtr<BaseIF> diel_baseif = RefCountedPtr<BaseIF> (new IntersectionIF(parts_dielectrics)); // Gas outside dielectric
+      RefCountedPtr<BaseIF> elec_baseif = RefCountedPtr<BaseIF> (new IntersectionIF(parts_electrodes));  // Gas outside electrodes
+      RefCountedPtr<BaseIF> diel_compif = RefCountedPtr<BaseIF> (new ComplementIF(*diel_baseif));        // Inside of dielectrics
+
+      parts.push_back(&(*diel_compif)); // Parts for intersection
+      parts.push_back(&(*elec_baseif)); // Parts for intersection
+    
+      m_sol_if = RefCountedPtr<BaseIF> (new IntersectionIF(parts)); 
+
+      if(s_use_new_gshop){
+	a_geoserver = static_cast<GeometryService*> (new ScanShop(*m_sol_if,
+								  0,
+								  a_dx,
+								  a_origin,
+								  a_finestDomain,
+								  s_ScanDomain,
+								  s_thresh));
+      }
+      else{ // Chombo geometry generation
+	a_geoserver = static_cast<GeometryService*> (new GeometryShop(*m_sol_if, 0, a_dx*RealVect::Unit, s_thresh));
+      }
     }
   }
 }
--- src/geometry/geo_coarsener.cpp
+++ src/geometry/geo_coarsener.cpp
@@ -10,127 +10,130 @@
 #include <ParmParse.H>
 #include <sstream>
 
-geo_coarsener::geo_coarsener(){
-  m_coarsen_boxes.resize(0);
-  m_coarsen_levels.resize(0);
+namespace ChomboDischarge {
+
+  geo_coarsener::geo_coarsener(){
+    m_coarsen_boxes.resize(0);
+    m_coarsen_levels.resize(0);
 
   
-  { // Info from input script
-    ParmParse pp("geo_coarsener");
+    { // Info from input script
+      ParmParse pp("geo_coarsener");
 
-    int num_boxes = 0;
-    pp.query("num_boxes", num_boxes);
+      int num_boxes = 0;
+      pp.query("num_boxes", num_boxes);
 
-    if(num_boxes > 0){
-      m_coarsen_boxes.resize(num_boxes);
-      m_coarsen_levels.resize(num_boxes);
-      m_inverse.resize(num_boxes, 0);
+      if(num_boxes > 0){
+	m_coarsen_boxes.resize(num_boxes);
+	m_coarsen_levels.resize(num_boxes);
+	m_inverse.resize(num_boxes, 0);
       
-      const int ndigits = (int) log10((double) num_boxes) + 1;
+	const int ndigits = (int) log10((double) num_boxes) + 1;
       
-      for (int ibox = 0; ibox < num_boxes; ibox++){
-	char* cstr = new char[ndigits];
-	sprintf(cstr, "%d", 1+ibox);
+	for (int ibox = 0; ibox < num_boxes; ibox++){
+	  char* cstr = new char[ndigits];
+	  sprintf(cstr, "%d", 1+ibox);
 
-	std::string str = "false";
-	std::string str1 = "box" + std::string(cstr) + "_lo";
-	std::string str2 = "box" + std::string(cstr) + "_hi";
-	std::string str3 = "box" + std::string(cstr) + "_lvl";
-	std::string str4 = "box" + std::string(cstr) + "_inv";
+	  std::string str = "false";
+	  std::string str1 = "box" + std::string(cstr) + "_lo";
+	  std::string str2 = "box" + std::string(cstr) + "_hi";
+	  std::string str3 = "box" + std::string(cstr) + "_lvl";
+	  std::string str4 = "box" + std::string(cstr) + "_inv";
 
-	Vector<Real> corner_lo(SpaceDim);
-	Vector<Real> corner_hi(SpaceDim);
-	int finest_box_lvl;
+	  Vector<Real> corner_lo(SpaceDim);
+	  Vector<Real> corner_hi(SpaceDim);
+	  int finest_box_lvl;
 
-	pp.getarr(str1.c_str(), corner_lo, 0, SpaceDim);
-	pp.getarr(str2.c_str(), corner_hi, 0, SpaceDim);
-	pp.get(str3.c_str(),    finest_box_lvl);
-	pp.query(str4.c_str(),    str);
+	  pp.getarr(str1.c_str(), corner_lo, 0, SpaceDim);
+	  pp.getarr(str2.c_str(), corner_hi, 0, SpaceDim);
+	  pp.get(str3.c_str(),    finest_box_lvl);
+	  pp.query(str4.c_str(),    str);
 
-	const RealVect c1 = RealVect(D_DECL(corner_lo[0], corner_lo[1], corner_lo[2]));
-	const RealVect c2 = RealVect(D_DECL(corner_hi[0], corner_hi[1], corner_hi[2]));
+	  const RealVect c1 = RealVect(D_DECL(corner_lo[0], corner_lo[1], corner_lo[2]));
+	  const RealVect c2 = RealVect(D_DECL(corner_hi[0], corner_hi[1], corner_hi[2]));
 
-	m_coarsen_boxes[ibox]  = real_box(c1,c2);
-	m_coarsen_levels[ibox] = finest_box_lvl;
-	m_inverse[ibox]        = (str == "true") ? 1 : 0;
+	  m_coarsen_boxes[ibox]  = real_box(c1,c2);
+	  m_coarsen_levels[ibox] = finest_box_lvl;
+	  m_inverse[ibox]        = (str == "true") ? 1 : 0;
 	
-	delete cstr;
+	  delete cstr;
+	}
       }
     }
   }
-}
 
-geo_coarsener::~geo_coarsener(){
-}
-
-void geo_coarsener::coarsen_tags(Vector<IntVectSet>& a_tags, const Vector<Real>& a_dx, const RealVect& a_origin) const {
-  CH_TIME("geo_coarsener::coarsen_tags");
-  if(!(m_coarsen_boxes.size() == m_coarsen_levels.size())){
-    pout() << "geo_coarsener::coarsen_tags - m_geocoarsen is not well defined. Skipping the coarsening step" << endl;
+  geo_coarsener::~geo_coarsener(){
   }
-  else{
-    if(m_coarsen_boxes.size() > 0){
-      for (int lvl = 0; lvl < a_tags.size(); lvl++){
-	const int num_coarsen = m_coarsen_boxes.size();
-	const IntVectSet tmp = a_tags[lvl];
-	for (IVSIterator it(tmp); it.ok(); ++it){
-	  const IntVect iv   = it();
-	  const RealVect pos = a_origin + RealVect(iv)*a_dx[lvl];
 
-	  bool remove_tag = false;
+  void geo_coarsener::coarsen_tags(Vector<IntVectSet>& a_tags, const Vector<Real>& a_dx, const RealVect& a_origin) const {
+    CH_TIME("geo_coarsener::coarsen_tags");
+    if(!(m_coarsen_boxes.size() == m_coarsen_levels.size())){
+      pout() << "geo_coarsener::coarsen_tags - m_geocoarsen is not well defined. Skipping the coarsening step" << endl;
+    }
+    else{
+      if(m_coarsen_boxes.size() > 0){
+	for (int lvl = 0; lvl < a_tags.size(); lvl++){
+	  const int num_coarsen = m_coarsen_boxes.size();
+	  const IntVectSet tmp = a_tags[lvl];
+	  for (IVSIterator it(tmp); it.ok(); ++it){
+	    const IntVect iv   = it();
+	    const RealVect pos = a_origin + RealVect(iv)*a_dx[lvl];
 
-	  bool inside_coarsen_box  = false;
-	  bool inside_inverse_box  = false;
-	  bool outside_inverse_box = false;
+	    bool remove_tag = false;
 
-	  // Check the regular box
-	  for (int ibox = 0; ibox < num_coarsen; ibox++){
-	    const RealVect lo = m_coarsen_boxes[ibox].get_lo();
-	    const RealVect hi = m_coarsen_boxes[ibox].get_hi();
+	    bool inside_coarsen_box  = false;
+	    bool inside_inverse_box  = false;
+	    bool outside_inverse_box = false;
 
-	    const bool inverse     = (m_inverse[ibox] != 0) ? true : false;
-	    const bool inside_box  = pos > lo && pos < hi;
-	    const bool coarsen_lvl = lvl >= m_coarsen_levels[ibox];
+	    // Check the regular box
+	    for (int ibox = 0; ibox < num_coarsen; ibox++){
+	      const RealVect lo = m_coarsen_boxes[ibox].get_lo();
+	      const RealVect hi = m_coarsen_boxes[ibox].get_hi();
 
-	    if(inside_box && !inverse && coarsen_lvl){
-	      inside_coarsen_box = true;
+	      const bool inverse     = (m_inverse[ibox] != 0) ? true : false;
+	      const bool inside_box  = pos > lo && pos < hi;
+	      const bool coarsen_lvl = lvl >= m_coarsen_levels[ibox];
+
+	      if(inside_box && !inverse && coarsen_lvl){
+		inside_coarsen_box = true;
+	      }
 	    }
-	  }
 
-	  // Check the inverse boxes
-	  for (int ibox = 0; ibox < num_coarsen; ibox++){
-	    const RealVect lo = m_coarsen_boxes[ibox].get_lo();
-	    const RealVect hi = m_coarsen_boxes[ibox].get_hi();
+	    // Check the inverse boxes
+	    for (int ibox = 0; ibox < num_coarsen; ibox++){
+	      const RealVect lo = m_coarsen_boxes[ibox].get_lo();
+	      const RealVect hi = m_coarsen_boxes[ibox].get_hi();
 
-	    const bool inverse     = (m_inverse[ibox] != 0) ? true : false;
-	    const bool inside_box  = pos > lo && pos < hi;
-	    const bool coarsen_lvl = lvl >= m_coarsen_levels[ibox];
+	      const bool inverse     = (m_inverse[ibox] != 0) ? true : false;
+	      const bool inside_box  = pos > lo && pos < hi;
+	      const bool coarsen_lvl = lvl >= m_coarsen_levels[ibox];
 
-	    if(inside_box && inverse && coarsen_lvl){ // Protect tag
-	      inside_inverse_box = true;
+	      if(inside_box && inverse && coarsen_lvl){ // Protect tag
+		inside_inverse_box = true;
+	      }
+	      else if(!inside_box && inverse && coarsen_lvl){ // Remove tag
+		outside_inverse_box = true;
+	      }
 	    }
-	    else if(!inside_box && inverse && coarsen_lvl){ // Remove tag
-	      outside_inverse_box = true;
-	    }
-	  }
 
-	  if(inside_coarsen_box && !inside_inverse_box){
-	    a_tags[lvl] -= iv;
-	  }
-	  if(!inside_inverse_box && outside_inverse_box){
-	    a_tags[lvl] -= iv;
+	    if(inside_coarsen_box && !inside_inverse_box){
+	      a_tags[lvl] -= iv;
+	    }
+	    if(!inside_inverse_box && outside_inverse_box){
+	      a_tags[lvl] -= iv;
+	    }
 	  }
 	}
       }
     }
   }
-}
 
-Vector<real_box> geo_coarsener::get_coarsen_boxes(){
-  return m_coarsen_boxes;
-}
+  Vector<real_box> geo_coarsener::get_coarsen_boxes(){
+    return m_coarsen_boxes;
+  }
 
 
-Vector<int> geo_coarsener::get_coarsen_levels(){
-  return m_coarsen_levels;
+  Vector<int> geo_coarsener::get_coarsen_levels(){
+    return m_coarsen_levels;
+  }
 }
