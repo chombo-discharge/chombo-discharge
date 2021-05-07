017b7204605b6a51702c897d9dd026a27835463c
Robert Marskar <robert.marskar@sintef.no>
2021-05-07 22:10:33 +0200

    physics/advection_diffusion now in namespace

--- physics/advection_diffusion/advection_diffusion_species.cpp
+++ physics/advection_diffusion/advection_diffusion_species.cpp
@@ -10,31 +10,33 @@
 #include <ParmParse.H>
 #include <PolyGeom.H>
 
-using namespace physics::advection_diffusion;
+namespace ChomboDischarge {
+  using namespace physics::advection_diffusion;
 
-advection_diffusion_species::advection_diffusion_species(){
+  advection_diffusion_species::advection_diffusion_species(){
 
-  ParmParse pp("advection_diffusion");
+    ParmParse pp("advection_diffusion");
 
-  m_charge = 0;
-  m_name   = "scalar species";
+    m_charge = 0;
+    m_name   = "scalar species";
 
-  Vector<Real> v;
-  pp.get   ("diffusion",      m_diffusive);
-  pp.get   ("advection",      m_mobile);
-  pp.get   ("blob_amplitude", m_blob_amplitude);
-  pp.get   ("blob_radius",    m_blob_radius);
-  pp.getarr("blob_center",    v, 0, SpaceDim); m_blob_center = RealVect(D_DECL(v[0], v[1], v[2]));
-}
+    Vector<Real> v;
+    pp.get   ("diffusion",      m_diffusive);
+    pp.get   ("advection",      m_mobile);
+    pp.get   ("blob_amplitude", m_blob_amplitude);
+    pp.get   ("blob_radius",    m_blob_radius);
+    pp.getarr("blob_center",    v, 0, SpaceDim); m_blob_center = RealVect(D_DECL(v[0], v[1], v[2]));
+  }
 
-advection_diffusion_species::~advection_diffusion_species(){
+  advection_diffusion_species::~advection_diffusion_species(){
 
-}
+  }
 
-Real advection_diffusion_species::initial_data(const RealVect a_pos, const Real a_time) const{
-  const RealVect d = a_pos - m_blob_center;
-  const Real d2 = PolyGeom::dot(d,d);
-  const Real r2 = m_blob_radius*m_blob_radius;
+  Real advection_diffusion_species::initial_data(const RealVect a_pos, const Real a_time) const{
+    const RealVect d = a_pos - m_blob_center;
+    const Real d2 = PolyGeom::dot(d,d);
+    const Real r2 = m_blob_radius*m_blob_radius;
 
-  return m_blob_amplitude*exp(-0.5*d2*d2/(r2*r2));
+    return m_blob_amplitude*exp(-0.5*d2*d2/(r2*r2));
+  }
 }
--- physics/advection_diffusion/advection_diffusion_tagger.cpp
+++ physics/advection_diffusion/advection_diffusion_tagger.cpp
@@ -11,105 +11,107 @@
 
 #include <ParmParse.H> 
 
-using namespace physics::advection_diffusion;
+namespace ChomboDischarge {
+  using namespace physics::advection_diffusion;
 
-advection_diffusion_tagger::advection_diffusion_tagger(RefCountedPtr<cdr_solver>& a_solver,
-						       RefCountedPtr<amr_mesh>&   a_amr){
-  m_solver    = a_solver;
-  m_amr       = a_amr;
-  m_name      = "advection_diffusion";
-  m_verbosity = -1;
-  m_realm     = a_solver->get_realm();
-}
-
-
-advection_diffusion_tagger::~advection_diffusion_tagger(){
-
-}
-
-void advection_diffusion_tagger::regrid(){
-  pout() << "regridding cell tagger" << endl;
-}
-
-void advection_diffusion_tagger::parse_options(){
-  ParmParse pp(m_name.c_str());
-  pp.get("refine_curv", m_refi_curv);
-  pp.get("refine_magn", m_refi_magn);
-  
-  parse_buffer(); // Derived from cell
-}
-
-bool advection_diffusion_tagger::tag_cells(EBAMRTags& a_tags){
-  EBAMRCellData sca;
-  EBAMRCellData vec;
-
-  m_amr->allocate(sca, m_realm, phase::gas, 1);
-  m_amr->allocate(vec, m_realm, phase::gas, SpaceDim);
-
-  const EBAMRCellData& state = m_solver->get_state();
-
-  // Compute the gradient, vec = grad(phi)
-  m_amr->compute_gradient(vec, state, m_realm, phase::gas); // vec = grad(phi)
-  data_ops::vector_length(sca, vec);                        // sca = |grad(phi)|
-  data_ops::set_covered_value(sca, 0, 0.0);                 // covered cell values are set to 0.0
-
-  bool found_tags = false;
-
-  // Never tag on max_amr_depth
-  const int finest_level     = m_amr->get_finest_level();
-  const int max_depth        = m_amr->get_max_amr_depth();
-  const int finest_tag_level = (finest_level == max_depth) ? max_depth - 1 : finest_level; // Never tag on max_amr_depth
-
-  for (int lvl = 0; lvl <= finest_tag_level; lvl++){
-    data_ops::scale(*sca[lvl], m_amr->get_dx()[lvl]); // sca = |grad(phi)|*dx
-
-    const Real SAFETY = 1.E-6;
-    
-    const DisjointBoxLayout& dbl = m_amr->get_grids(m_realm)[lvl];
-    for (DataIterator dit = dbl.dataIterator(); dit.ok(); ++dit){
-      const EBCellFAB& c   = (*sca[lvl])[dit()];
-      const EBCellFAB& phi = (*state[lvl])[dit()];
-
-      const BaseFab<Real>& cReg   = c.getSingleValuedFAB();
-      const BaseFab<Real>& phiReg = phi.getSingleValuedFAB();
-
-      const Box box = dbl.get(dit());
-
-      // These are the tags
-      DenseIntVectSet& tags = (*a_tags[lvl])[dit()];
-      tags.makeEmptyBits(); // Clear all previous tags
-
-      // Do regular cells
-      for (BoxIterator bit(box); bit.ok(); ++bit){
-	const IntVect iv = bit();
-
-	const Real crit = Abs(cReg(iv,0))/(SAFETY + Abs(phiReg(iv,0)));
-	if(crit > m_refi_curv && Abs(phiReg(iv,0)) > m_refi_magn){
-	  tags |= iv;
-	  found_tags = true;
-	}
-      }
-
-      tags &= box;
-
-      // Do irregular cells
-      const EBISBox& ebisbox = c.getEBISBox();
-      const EBGraph& ebgraph = ebisbox.getEBGraph();
-      for (VoFIterator vofit(ebisbox.getIrregIVS(box), ebgraph); vofit.ok(); ++vofit){
-	const VolIndex& vof = vofit();
-      }
-    }
+  advection_diffusion_tagger::advection_diffusion_tagger(RefCountedPtr<cdr_solver>& a_solver,
+							 RefCountedPtr<amr_mesh>&   a_amr){
+    m_solver    = a_solver;
+    m_amr       = a_amr;
+    m_name      = "advection_diffusion";
+    m_verbosity = -1;
+    m_realm     = a_solver->get_realm();
   }
 
-  // Some ranks may have gotten new tags while others have not. This little code snippet
-  // sets got_new_tags = true for all ranks if any rank originally had got_new_tags = true
+
+  advection_diffusion_tagger::~advection_diffusion_tagger(){
+
+  }
+
+  void advection_diffusion_tagger::regrid(){
+    pout() << "regridding cell tagger" << endl;
+  }
+
+  void advection_diffusion_tagger::parse_options(){
+    ParmParse pp(m_name.c_str());
+    pp.get("refine_curv", m_refi_curv);
+    pp.get("refine_magn", m_refi_magn);
+  
+    parse_buffer(); // Derived from cell
+  }
+
+  bool advection_diffusion_tagger::tag_cells(EBAMRTags& a_tags){
+    EBAMRCellData sca;
+    EBAMRCellData vec;
+
+    m_amr->allocate(sca, m_realm, phase::gas, 1);
+    m_amr->allocate(vec, m_realm, phase::gas, SpaceDim);
+
+    const EBAMRCellData& state = m_solver->get_state();
+
+    // Compute the gradient, vec = grad(phi)
+    m_amr->compute_gradient(vec, state, m_realm, phase::gas); // vec = grad(phi)
+    data_ops::vector_length(sca, vec);                        // sca = |grad(phi)|
+    data_ops::set_covered_value(sca, 0, 0.0);                 // covered cell values are set to 0.0
+
+    bool found_tags = false;
+
+    // Never tag on max_amr_depth
+    const int finest_level     = m_amr->get_finest_level();
+    const int max_depth        = m_amr->get_max_amr_depth();
+    const int finest_tag_level = (finest_level == max_depth) ? max_depth - 1 : finest_level; // Never tag on max_amr_depth
+
+    for (int lvl = 0; lvl <= finest_tag_level; lvl++){
+      data_ops::scale(*sca[lvl], m_amr->get_dx()[lvl]); // sca = |grad(phi)|*dx
+
+      const Real SAFETY = 1.E-6;
+    
+      const DisjointBoxLayout& dbl = m_amr->get_grids(m_realm)[lvl];
+      for (DataIterator dit = dbl.dataIterator(); dit.ok(); ++dit){
+	const EBCellFAB& c   = (*sca[lvl])[dit()];
+	const EBCellFAB& phi = (*state[lvl])[dit()];
+
+	const BaseFab<Real>& cReg   = c.getSingleValuedFAB();
+	const BaseFab<Real>& phiReg = phi.getSingleValuedFAB();
+
+	const Box box = dbl.get(dit());
+
+	// These are the tags
+	DenseIntVectSet& tags = (*a_tags[lvl])[dit()];
+	tags.makeEmptyBits(); // Clear all previous tags
+
+	// Do regular cells
+	for (BoxIterator bit(box); bit.ok(); ++bit){
+	  const IntVect iv = bit();
+
+	  const Real crit = Abs(cReg(iv,0))/(SAFETY + Abs(phiReg(iv,0)));
+	  if(crit > m_refi_curv && Abs(phiReg(iv,0)) > m_refi_magn){
+	    tags |= iv;
+	    found_tags = true;
+	  }
+	}
+
+	tags &= box;
+
+	// Do irregular cells
+	const EBISBox& ebisbox = c.getEBISBox();
+	const EBGraph& ebgraph = ebisbox.getEBGraph();
+	for (VoFIterator vofit(ebisbox.getIrregIVS(box), ebgraph); vofit.ok(); ++vofit){
+	  const VolIndex& vof = vofit();
+	}
+      }
+    }
+
+    // Some ranks may have gotten new tags while others have not. This little code snippet
+    // sets got_new_tags = true for all ranks if any rank originally had got_new_tags = true
 #ifdef CH_MPI
-  int glo = 1;
-  int loc = found_tags ? 1 : 0;
+    int glo = 1;
+    int loc = found_tags ? 1 : 0;
 
-  const int result = MPI_Allreduce(&loc, &glo, 1, MPI_INT, MPI_MAX, Chombo_MPI::comm);
+    const int result = MPI_Allreduce(&loc, &glo, 1, MPI_INT, MPI_MAX, Chombo_MPI::comm);
 
-  found_tags = (glo == 1) ? true : false;
+    found_tags = (glo == 1) ? true : false;
 #endif
-  return found_tags;
+    return found_tags;
+  }
 }
