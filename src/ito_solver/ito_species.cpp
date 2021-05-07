3530792caca925a3e91950b4632a02a673ee3d97
Robert Marskar <robert.marskar@sintef.no>
2021-05-07 19:45:11 +0200

    src/ito_solver added to namespace

--- src/ito_solver/ito_species.cpp
+++ src/ito_solver/ito_species.cpp
@@ -7,46 +7,49 @@
 
 #include "ito_species.H"
 
-ito_species::ito_species(){
-  m_name = "ito_species.H";
-}
+namespace ChomboDischarge {
 
-ito_species::ito_species(const std::string a_name, const int a_charge, const bool a_mobile, const bool a_diffusive){
-  m_name      = a_name;
-  m_charge    = a_charge;
-  m_mobile    = a_mobile;
-  m_diffusive = a_diffusive;
-}
+  ito_species::ito_species(){
+    m_name = "ito_species.H";
+  }
 
-ito_species::~ito_species(){
+  ito_species::ito_species(const std::string a_name, const int a_charge, const bool a_mobile, const bool a_diffusive){
+    m_name      = a_name;
+    m_charge    = a_charge;
+    m_mobile    = a_mobile;
+    m_diffusive = a_diffusive;
+  }
 
-}
+  ito_species::~ito_species(){
 
-std::string ito_species::get_name() const{
-  return m_name;
-}
+  }
 
-int ito_species::get_charge() const{
-  return m_charge;
-}
+  std::string ito_species::get_name() const{
+    return m_name;
+  }
+
+  int ito_species::get_charge() const{
+    return m_charge;
+  }
     
-bool ito_species::is_diffusive() const{
-  return m_diffusive;
-}
+  bool ito_species::is_diffusive() const{
+    return m_diffusive;
+  }
 
 
-bool ito_species::is_mobile() const {
-  return m_mobile;
-}
+  bool ito_species::is_mobile() const {
+    return m_mobile;
+  }
 
-List<ito_particle>& ito_species::get_initial_particles() {
-  return m_initial_particles;
-}
+  List<ito_particle>& ito_species::get_initial_particles() {
+    return m_initial_particles;
+  }
 
-Real ito_species::mobility(const Real a_energy) const {
-  return 0.0;
-}
+  Real ito_species::mobility(const Real a_energy) const {
+    return 0.0;
+  }
 
-Real ito_species::diffusion(const Real a_energy) const {
-  return 0.0;
+  Real ito_species::diffusion(const Real a_energy) const {
+    return 0.0;
+  }
 }
