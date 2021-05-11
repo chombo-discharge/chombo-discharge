9c4820bd5fb0b9887d9017402197ec53bc3a706f
Robert Marskar <robert.marskar@sintef.no>
2021-05-11 19:19:06 +0200

    Updated makefile

--- physics/field/python/app_make.py
+++ physics/field/python/app_make.py
@@ -45,7 +45,7 @@ def write_template(args):
     makef.write('\t$(DISCHARGE_HOME)/src/field_solver \\\n')
     makef.write('\t$(DISCHARGE_HOME)/src/particle \\\n')
     makef.write('\t$(DISCHARGE_HOME)/geometries/' + args.geometry + '\\\n')
-    makef.write('\t$(DISCHARGE_HOME)/physics/poisson \\\n')
+    makef.write('\t$(DISCHARGE_HOME)/physics/field \\\n')
     makef.write('\n')
 
     makef.write('\n')
