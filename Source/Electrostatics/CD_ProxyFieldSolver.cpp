fb03301d8ec49869456dc876183d7743134130d4
Robert Marskar <robert.marskar@sintef.no>
2021-05-27 12:08:13 +0200

    Registered operators

--- Source/Electrostatics/CD_ProxyFieldSolver.cpp
+++ Source/Electrostatics/CD_ProxyFieldSolver.cpp
@@ -37,7 +37,23 @@ bool ProxyFieldSolver::solve(MFAMRCellData&       a_potential,
 }
 
 void ProxyFieldSolver::registerOperators()  {
-
+  if(m_amr.isNull()){
+    MayDay::Abort("FieldSolverMultigrid::registerOperators - need to set AmrMesh!");
+  }
+  else{
+    m_amr->registerOperator(s_eb_coar_ave,     m_realm, phase::gas);
+    m_amr->registerOperator(s_eb_coar_ave,     m_realm, phase::solid);
+    m_amr->registerOperator(s_eb_fill_patch,   m_realm, phase::gas);
+    m_amr->registerOperator(s_eb_fill_patch,   m_realm, phase::solid);
+    m_amr->registerOperator(s_eb_pwl_interp,   m_realm, phase::gas);
+    m_amr->registerOperator(s_eb_pwl_interp,   m_realm, phase::solid);
+    m_amr->registerOperator(s_eb_quad_cfi,     m_realm, phase::gas);
+    m_amr->registerOperator(s_eb_quad_cfi,     m_realm, phase::solid);
+    m_amr->registerOperator(s_eb_irreg_interp, m_realm, phase::gas);
+    m_amr->registerOperator(s_eb_irreg_interp, m_realm, phase::solid);
+    m_amr->registerOperator(s_eb_flux_reg,     m_realm, phase::gas);
+    m_amr->registerOperator(s_eb_flux_reg,     m_realm, phase::solid);
+  }
 }
 
 void ProxyFieldSolver::solveOnePhase(EBAMRCellData& a_phi){
