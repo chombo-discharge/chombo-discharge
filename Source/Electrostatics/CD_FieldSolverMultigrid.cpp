e23bcc844c1fa02e150db4078622461663479239
Robert Marskar <robert.marskar@sintef.no>
2021-08-05 15:29:57 +0200

    Removed olde code from FieldSolverMultigrid

--- Source/Electrostatics/CD_FieldSolverMultigrid.cpp
+++ Source/Electrostatics/CD_FieldSolverMultigrid.cpp
@@ -7,34 +7,21 @@
   @file   FieldSolverMultigrid.cpp
   @brief  Implementation of FieldSolverMultigrid.H
   @author Robert Marskar
-  @todo   When specifying the bottom solver, use syntax "simple 400" to indicate the number of smoothings. 
-  @todo   gmg parameters should be specified as "FieldSolverMultigrid.gmg.<setting>"
-  @todo   Purge the pre-coarsening stuff, e.g. defineDeeperMultigridLevels
-  @todo   Register the new multigrid interpolators
   @todo   Once the new operator is in, check the computeLoads routine. 
 */
 
 // Chombo includes
 #include <ParmParse.H>
-#include <BRMeshRefine.H>
 
 // Our includes
 #include <CD_FieldSolverMultigrid.H>
 #include <CD_DataOps.H>
-#include <CD_MFQuadCFInterp.H>
-#include <CD_MFInterfaceFAB.H>
 #include <CD_MFMultigridInterpolator.H>
-#include <CD_MFHelmholtzDirichletEBBCFactory.H>
-#include <CD_MFHelmholtzDirichletDomainBCFactory.H>
 #include <CD_MFHelmholtzElectrostaticDomainBCFactory.H>
 #include <CD_MFHelmholtzElectrostaticEBBCFactory.H>
-#include <CD_JumpBc.H>
-#include <CD_AmrMesh.H>
-#include <CD_ConductivityElectrostaticDomainBcFactory.H>
 #include <CD_Units.H>
 #include <CD_NamespaceHeader.H>
 
-#define USE_NEW_HELMHOLTZ_FACTORY 1
 
 constexpr Real FieldSolverMultigrid::m_alpha;
 constexpr Real FieldSolverMultigrid::m_beta;
@@ -43,7 +30,6 @@ FieldSolverMultigrid::FieldSolverMultigrid() : FieldSolver() {
 
   // Default settings
   m_isSolverSetup            = false;
-  m_hasDeeperMultigridLevels = false;
   m_className                = "FieldSolverMultigrid";
 }
 
@@ -75,7 +61,6 @@ void FieldSolverMultigrid::parseMultigridSettings(){
   ParmParse pp(m_className.c_str());
 
   std::string str;
-  pp.get("gmg_coarsen",      m_numCoarseningsBeforeAggregation);
   pp.get("gmg_verbosity",    m_multigridVerbosity);
   pp.get("gmg_pre_smooth",   m_multigridPreSmooth);
   pp.get("gmg_post_smooth",  m_multigridPostSmooth);
@@ -193,9 +178,6 @@ bool FieldSolverMultigrid::solve(MFAMRCellData&       a_phi,
   // Do the scaled space charge density
   DataOps::copy (m_kappaRhoByEps0, a_rho);
   DataOps::scale(m_kappaRhoByEps0, 1./(Units::eps0));
-#if !(USE_NEW_HELMHOLTZ_FACTORY)
-  DataOps::scale(m_kappaRhoByEps0, 1./(m_lengthScale*m_lengthScale));
-#endif
 
   // Special flag for when a_rho is on the centroid but was not scaled by kappa on input. The multigrid operator solves
   // kappa*L(phi) = kappa*rho so the right-hand side must be kappa-weighted. 
@@ -206,12 +188,9 @@ bool FieldSolverMultigrid::solve(MFAMRCellData&       a_phi,
   // Do the scaled surface charge
   DataOps::copy (m_sigmaByEps0, a_sigma);
   DataOps::scale(m_sigmaByEps0, 1./(Units::eps0));
-#if !(USE_NEW_HELMHOLTZ_FACTORY)
-  DataOps::scale(m_sigmaByEps0, 1./(m_lengthScale*m_lengthScale));
-  m_operatorFactory->setJump(m_sigmaByEps0, 1.0);  
-#else
+
+  // Factory needs knowledge of the new surface charge. 
   m_helmholtzOpFactory->setJump(m_sigmaByEps0, 1.0);
-#endif
 
   // Aliasing
   Vector<LevelData<MFCellFAB>* > phi, cpy, rhs, res, zero;
@@ -311,108 +290,13 @@ void FieldSolverMultigrid::registerOperators(){
   }
 }
 
-void FieldSolverMultigrid::defineDeeperMultigridLevels(){
-  CH_TIME("FieldSolverMultigrid::define_mg_level");
-  if(m_verbosity > 5){
-    pout() << "FieldSolverMultigrid::defineDeeperMultigridLevels" << endl;
-  }
-
-  const RefCountedPtr<EBIndexSpace>& ebis_gas = m_multifluidIndexSpace->getEBIndexSpace(phase::gas);
-  const RefCountedPtr<EBIndexSpace>& ebis_sol = m_multifluidIndexSpace->getEBIndexSpace(phase::solid);
-
-  const int coar_ref = 2;
-  
-  m_deeperMultigridDomains.resize(0);
-  m_deeperMultigridGrids.resize(0);
-  m_mg_mflg.resize(0);
-  
-  m_mg_eblg.resize(phase::numPhases);
-
-  m_mg_eblg[phase::gas].resize(0);
-  m_mg_eblg[phase::solid].resize(0);
-
-  // Get some stuff from AmrMesh on how to decompose the levels
-  const Vector<ProblemDomain>& domains = m_amr->getDomains();
-  const int max_box_size               = m_amr->getMaxBoxSize();
-  const int blocking_factor            = m_amr->getBlockingFactor();
-  const int num_numEbGhostsCells                = m_amr->getNumberOfEbGhostCells();
-
-  int num_coar       = 0;
-  bool has_coar      = true;
-  ProblemDomain fine = domains[0];
-
-  // Coarsen problem domains and create grids
-  while(num_coar < m_numCoarseningsBeforeAggregation || !has_coar){
-
-    // Check if we can coarsen
-    const ProblemDomain coar = fine.coarsen(coar_ref);
-    const Box coar_box       = coar.domainBox();
-    for (int dir = 0; dir < SpaceDim; dir++){
-      if(coar_box.size()[dir] < max_box_size || coar_box.size()[dir]%max_box_size != 0){
-	has_coar = false;
-      }
-    }
-
-    if(has_coar){
-      // Split the domain into pieces, then order and load balance them
-      Vector<Box> boxes;
-      Vector<int> proc_assign;
-      domainSplit(coar, boxes, max_box_size, blocking_factor);
-      mortonOrdering(boxes);
-      LoadBalancing::makeBalance(proc_assign, boxes);
-
-      // Add problem domain and grid
-      m_deeperMultigridDomains.push_back(coar);
-      m_deeperMultigridGrids.push_back(DisjointBoxLayout(boxes, proc_assign, coar));
-
-      // Define the EBLevelGrids
-      const int idx = m_deeperMultigridGrids.size() - 1; // Last element added
-      if(!ebis_gas.isNull()){
-	m_mg_eblg[phase::gas].push_back(RefCountedPtr<EBLevelGrid> (new EBLevelGrid(m_deeperMultigridGrids[idx],
-										    m_deeperMultigridDomains[idx],
-										    num_numEbGhostsCells,
-										    ebis_gas)));
-      }
-      if(!ebis_sol.isNull()){
-	m_mg_eblg[phase::solid].push_back(RefCountedPtr<EBLevelGrid> (new EBLevelGrid(m_deeperMultigridGrids[idx],
-										      m_deeperMultigridDomains[idx],
-										      num_numEbGhostsCells,
-										      ebis_sol)));
-      }
-
-      // Define the MFLevelGrid object
-      Vector<EBLevelGrid> eblgs;
-      if(!ebis_gas.isNull()) eblgs.push_back(*m_mg_eblg[phase::gas][idx]);
-      if(!ebis_sol.isNull()) eblgs.push_back(*m_mg_eblg[phase::solid][idx]);
-      
-      m_mg_mflg.push_back(RefCountedPtr<MFLevelGrid> (new MFLevelGrid(m_multifluidIndexSpace, eblgs)));
-
-      // Next iterate
-      fine = coar;
-      num_coar++;
-    }
-    else{
-      break;
-    }
-  }
-
-  m_hasDeeperMultigridLevels = true;
-}
-
 void FieldSolverMultigrid::setupSolver(){
   CH_TIME("FieldSolverMultigrid::setupSolver()");
   if(m_verbosity > 5){
     pout() << "FieldSolverMultigrid::setupSolver()" << endl;
   }
 
-#if !(USE_NEW_HELMHOLTZ_FACTORY)
-  if(!m_hasDeeperMultigridLevels){
-    this->defineDeeperMultigridLevels();     // Define MG levels. These don't change during regrids so we only need to set them once. 
-  }
-#endif
-
   this->setupHelmholtzFactory(); // Set up the operator factory
-  this->setupOperatorFactory();  // Set the operator factory
   this->setupMultigrid();        // Set up the AMR multigrid solver
 
   m_isSolverSetup = true;
@@ -472,9 +356,7 @@ void FieldSolverMultigrid::setupHelmholtzFactory(){
   const IntVect ghostRhs = m_amr->getNumberOfGhostCells()*IntVect::Unit;
 
   // Set up Dirichlet boundary conditions for now
-  // auto ebbcFactory     = RefCountedPtr<MFHelmholtzEBBCFactory>     (new MFHelmholtzDirichletEBBCFactory(m_multigridBcOrder, m_multigridBcWeight, 1.0));
   auto ebbcFactory     = RefCountedPtr<MFHelmholtzEBBCFactory>     (new MFHelmholtzElectrostaticEBBCFactory(m_multigridBcOrder, m_multigridBcWeight, m_ebBc));
-  //  auto domainBcFactory = RefCountedPtr<MFHelmholtzDomainBCFactory> (new MFHelmholtzDirichletDomainBCFactory(-1.0));
   auto domainBcFactory = RefCountedPtr<MFHelmholtzDomainBCFactory> (new MFHelmholtzElectrostaticDomainBCFactory(m_domainBc));
   
   m_helmholtzOpFactory = RefCountedPtr<MFHelmholtzOpFactory>(new MFHelmholtzOpFactory(m_multifluidIndexSpace,
@@ -501,91 +383,6 @@ void FieldSolverMultigrid::setupHelmholtzFactory(){
 										      m_multigridJumpWeight,
 										      m_amr->getMaxBoxSize()));
 
-}
-
-void FieldSolverMultigrid::setupOperatorFactory(){
-  CH_TIME("FieldSolverMultigrid::setupOperatorFactory");
-  if(m_verbosity > 5){
-    pout() << "FieldSolverMultigrid::setupOperatorFactory" << endl;
-  }
-
-  const int nphases                      = m_multifluidIndexSpace->numPhases();
-  const int finestLevel                 = m_amr->getFinestLevel();
-  const Vector<DisjointBoxLayout>& grids = m_amr->getGrids(m_realm);
-  const Vector<int>& refinement_ratios   = m_amr->getRefinementRatios();
-  const Vector<ProblemDomain>& domains   = m_amr->getDomains();
-  const Vector<Real>& dx                 = m_amr->getDx();
-  const RealVect& origin                 = m_amr->getProbLo();
-
-  const RefCountedPtr<EBIndexSpace>& ebis_gas = m_multifluidIndexSpace->getEBIndexSpace(phase::gas);
-  const RefCountedPtr<EBIndexSpace>& ebis_sol = m_multifluidIndexSpace->getEBIndexSpace(phase::solid);
-
-  // This stuff is needed for the operator factory
-  Vector<MFLevelGrid>    mflg(1 + finestLevel);
-  Vector<MFQuadCFInterp> mfquadcfi(1 + finestLevel);
-  Vector<MFFluxReg>  mffluxreg(1 + finestLevel);
-  for (int lvl = 0; lvl <= finestLevel; lvl++){
-    Vector<EBLevelGrid>                    eblg_phases   (nphases);
-    Vector<RefCountedPtr<EBQuadCFInterp> > quadcfi_phases(nphases);
-    Vector<RefCountedPtr<EBFluxRegister> > fluxreg_phases(nphases);
-
-    if(!ebis_gas.isNull()) eblg_phases[phase::gas]      = *(m_amr->getEBLevelGrid(m_realm, phase::gas)[lvl]);
-    if(!ebis_sol.isNull()) eblg_phases[phase::solid]    = *(m_amr->getEBLevelGrid(m_realm, phase::solid)[lvl]);
-
-    if(!ebis_gas.isNull()) quadcfi_phases[phase::gas]   = (m_amr->getEBQuadCFInterp(m_realm, phase::gas)[lvl]);
-    if(!ebis_sol.isNull()) quadcfi_phases[phase::solid] = (m_amr->getEBQuadCFInterp(m_realm, phase::solid)[lvl]);
-
-    if(!ebis_gas.isNull()) fluxreg_phases[phase::gas]   = (m_amr->getFluxRegister(m_realm, phase::gas)[lvl]);
-    if(!ebis_sol.isNull()) fluxreg_phases[phase::solid] = (m_amr->getFluxRegister(m_realm, phase::solid)[lvl]);
-    
-    mflg[lvl].define(m_multifluidIndexSpace, eblg_phases);
-    mfquadcfi[lvl].define(quadcfi_phases);
-    mffluxreg[lvl].define(fluxreg_phases);
-  }
-
-  // Appropriate coefficients for poisson equation
-  const IntVect ghost_phi = m_amr->getNumberOfGhostCells()*IntVect::Unit;
-  const IntVect ghost_rhs = m_amr->getNumberOfGhostCells()*IntVect::Unit;
-
-  Vector<MFLevelGrid> mg_levelgrids;
-  for (int lvl = 0; lvl < m_mg_mflg.size(); lvl++){
-    mg_levelgrids.push_back(*m_mg_mflg[lvl]);
-  }
-
-  // Set the length scale for the Poisson equation. This is equivalent to solving the Poisson equation
-  // on the domain [-1,1] in the x-direction
-  m_lengthScale = 2./(domains[0].size(0)*dx[0]);
-
-
-  auto domfact = RefCountedPtr<ConductivityElectrostaticDomainBcFactory> (new ConductivityElectrostaticDomainBcFactory(m_domainBc, m_amr->getProbLo()));
-
-  // Create factory and set potential
-  m_operatorFactory = RefCountedPtr<MfHelmholtzOpFactory> (new MfHelmholtzOpFactory(m_multifluidIndexSpace,
-										 mflg,
-										 mfquadcfi,
-										 mffluxreg,
-										 refinement_ratios,
-										 grids,
-										 m_permittivityCell,
-										 m_permittivityFace,
-										 m_permittivityEB,
-										 m_alpha,
-										 m_beta,
-										 m_lengthScale,
-										 dx[0]*m_lengthScale,
-										 domains[0],
-										 domfact,
-										 origin,
-										 ghost_phi,
-										 ghost_rhs,
-										 m_multigridBcOrder,
-										    1,
-										 m_minCellsBottom,
-										 1 + finestLevel,
-										 mg_levelgrids));
-
-  // Parse Dirichlet boundary conditions on electrodes to the operator factory. 
-  m_operatorFactory->setDirichletEbBc(m_ebBc);
 }
 
 void FieldSolverMultigrid::setupMultigrid(){
@@ -624,36 +421,11 @@ void FieldSolverMultigrid::setupMultigrid(){
     MayDay::Error("FieldSolverMultigrid::setupMultigrid - logic bust in multigrid type selection");
   }
 
-  // This code should be removed later when the new operator is functional
-#if !(USE_NEW_HELMHOLTZ_FACTORY)
-  if(m_bottomSolverType == BottomSolverType::Simple){
-    m_mfsolver.setNumSmooths(m_numSmoothingsForSimpleSolver);
-    bottomSolver = &m_mfsolver;
-  }
-  else if(m_bottomSolverType == BottomSolverType::BiCGStab){
-    bottomSolver = &m_bicgstab;
-    if(m_multifluidIndexSpace->numPhases() == 2){ // BiCGStab doesn't work with multifluid (yet)
-      bottomSolver = &m_mfsolver;
-      
-      if(m_verbosity > 0){
-	pout() << "FieldSolverMultigrid::FieldSolverMultigrid - BiCGStab not supported for multifluid" << endl;
-      }
-    }
-  }
-  else if(m_bottomSolverType == BottomSolverType::GMRES){
-    bottomSolver = &m_gmres;
-  }
-#endif
-
   const int finestLevel              = m_amr->getFinestLevel();
   const ProblemDomain coarsestDomain = m_amr->getDomains()[0];
 
   // Define the Chombo multigrid solver
-#if USE_NEW_HELMHOLTZ_FACTORY
   m_multigridSolver.define(coarsestDomain, *m_helmholtzOpFactory, bottomSolver, 1 + finestLevel);
-#else
-  m_multigridSolver.define(coarsestDomain, *m_operatorFactory, bottomSolver, 1 + finestLevel);
-#endif
   m_multigridSolver.setSolverParameters(m_multigridPreSmooth,
 					m_multigridPostSmooth,
 					m_multigridBottomSmooth,
@@ -700,23 +472,19 @@ Vector<long long> FieldSolverMultigrid::computeLoads(const DisjointBoxLayout& a_
     this->setupSolver();
   }
 
-  // Dummy storage where the multigrid operator will do relaxations. 
-  MFAMRCellData dummy1, dummy2;
-
-  m_amr->allocate(dummy1, m_realm, 1);
-  m_amr->allocate(dummy2, m_realm, 1);
-
-  // Make an operator
-  auto oper = (MfHelmholtzOp*) m_operatorFactory->MGnewOp(m_amr->getDomains()[a_level], 0, false);
-
   // Reset time
   TimedDataIterator dit = a_dbl.timedDataIterator();
   dit.clearTime();
   dit.enableTime();
 
   // Do some relaxations on each level. This includes BCs.
+  MFAMRCellData dummy;
+
+  m_amr->allocate(dummy, m_realm, m_nComp);
+
+  auto oper = (MFHelmholtzOp*) m_helmholtzOpFactory->MGnewOp(m_amr->getDomains()[a_level], 0, false);
   for (int k = 0; k < numApply; k++){
-    oper->applyOp(*dummy1[a_level], *dummy2[a_level], dit, true);
+    oper->computeOperatorLoads(*dummy[a_level], dit);
   }
 
   // Merge times
