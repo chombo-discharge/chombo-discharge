8bdd77f00cfa1acf257bd1090b4da801dc98efed
Robert Marskar <robert.marskar@sintef.no>
2021-08-05 11:23:09 +0200

    WIP

--- Source/Electrostatics/CD_EBHelmholtzElectrostaticDomainBC.cpp
+++ Source/Electrostatics/CD_EBHelmholtzElectrostaticDomainBC.cpp
@@ -22,13 +22,29 @@ EBHelmholtzElectrostaticDomainBC::EBHelmholtzElectrostaticDomainBC(const Electro
       const ElectrostaticDomainBc::BcType&     bcType  = a_electrostaticBCs.getBc(curWall).first;
       const ElectrostaticDomainBc::BcFunction& bcFunc  = a_electrostaticBCs.getBc(curWall).second;
 
-      // Generate the required BC type
+      // Make a lambda which allows us to pass in the function using EBHelmholtzDomainBC API, which takes a std::function<Real(const RealVect a_position)>
+      // type of function.
+      //
+      // This is the function type that the EBHelmholtzOp API requires, and it is a design choice mandated by our choice to make that operator
+      // time-independent. Although this might seem weird, the time dependence is nonetheless passed in because a_electrostaticBCs are passed in
+      // from FieldSolver, and in that solver we capture FieldSolver::m_time by reference.
+      // 
+      // This might seem clunky, but I can't see any other way of doing it properly without changing EBHelmholtzOp to a time-dependent operator (which
+      // I really don't want to do).
+      //
+      // Here is the function
+      auto func = [&bcFunc](const RealVect& a_position) -> Real {
+	constexpr Real dummyDt = 0.0;
+	return bcFunc(a_position, dummyDt);
+      };
+
+      // Create either a Dirichlet or Neumann boundary condition object for the current domain edge (face in 3D).
       switch(bcType) {
       case ElectrostaticDomainBc::BcType::Dirichlet:
-	m_bcObjects.emplace(curWall, std::make_shared<EBHelmholtzDirichletDomainBC>(1.0));
+	m_bcObjects.emplace(curWall, std::make_shared<EBHelmholtzDirichletDomainBC>(func));
 	break;
       case ElectrostaticDomainBc::BcType::Neumann:
-	m_bcObjects.emplace(curWall, std::make_shared<EBHelmholtzNeumannDomainBC>(0.0));
+	m_bcObjects.emplace(curWall, std::make_shared<EBHelmholtzNeumannDomainBC>(func));
 	break;
       default:
 	MayDay::Error("EBHelmholtzElectrostaticDomainBC::EBHelmholtzElectrostaticDomainBC - unsupported boundary condition passed into constructor!");
