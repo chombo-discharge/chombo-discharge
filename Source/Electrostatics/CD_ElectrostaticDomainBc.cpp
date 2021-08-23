fcca62731a4fb9f00cb84e6585cf4320ce07a8f2
Robert Marskar <robert.marskar@sintef.no>
2021-08-23 08:50:59 +0200

    Added timers and some more docs

--- Source/Electrostatics/CD_ElectrostaticDomainBc.cpp
+++ Source/Electrostatics/CD_ElectrostaticDomainBc.cpp
@@ -9,18 +9,24 @@
   @author Robert Marskar
 */
 
+// Chombo includes
+#include <CH_Timer.H>
+
 // Our includes
 #include <CD_ElectrostaticDomainBc.H>
 #include <CD_NamespaceHeader.H>
 
 ElectrostaticDomainBc::ElectrostaticDomainBc() {
+  CH_TIME("ElectrostaticDomainBc::ElectrostaticDomainBc()");
+  
   m_bcFunctions.clear();
 
-  // 
+  // Make a lambda which returns zero everywhere. 
   auto zero = [](const RealVect a_pos, const Real a_time){
     return 0.0;
   };
 
+  // Set the boundary condition functions on each side to be homogeneous Neumann boundary conditions. 
   for (int dir = 0; dir < SpaceDim; dir++){
     for (SideIterator sit; sit.ok(); ++sit){
       const DomainSide domainSide = std::make_pair(dir, sit());
@@ -31,14 +37,20 @@ ElectrostaticDomainBc::ElectrostaticDomainBc() {
 }
 
 ElectrostaticDomainBc::~ElectrostaticDomainBc() {
+  CH_TIME("ElectrostaticDomainBc::~ElectrostaticDomainBc()");
+  
   m_bcFunctions.clear();
 }
 
 void ElectrostaticDomainBc::setBc(const DomainSide a_domainSide, const Bc a_bc){
+  CH_TIME("ElectrostaticDomainBc::setBc(DomainSide, Bc)");
+  
   m_bcFunctions.at(a_domainSide) = a_bc;
 }
 
 ElectrostaticDomainBc::Bc ElectrostaticDomainBc::getBc(const DomainSide a_domainSide) const{
+  CH_TIME("ElectrostaticDomainBc::getBc(DomainSide)");
+  
   return m_bcFunctions.at(a_domainSide);
 }
 
