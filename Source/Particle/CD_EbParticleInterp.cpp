bccda426e763eabe3ef9b8e30187ee06f1c54c00
Robert Marskar <robert.marskar@sintef.no>
2021-09-27 10:38:40 +0200

    WIP on EbParticleInterp docs

--- Source/Particle/CD_EbParticleInterp.cpp
+++ Source/Particle/CD_EbParticleInterp.cpp
@@ -9,32 +9,40 @@
   @author Robert Marskar
 */
 
+// Chombo includes
+#include <CH_Timer.H>
+
 // Our includes
 #include <CD_EbParticleInterp.H>
 #include <CD_EbParticleInterpF_F.H>
 #include <CD_NamespaceHeader.H>
 
 EbParticleInterp::EbParticleInterp(){
+  CH_TIME("EbParticleInterp::EbParticleInterp");
 }
 
-EbParticleInterp::EbParticleInterp(const Box&      a_domain,
+EbParticleInterp::EbParticleInterp(const Box&      a_region,
 				   const EBISBox&  a_ebisbox,
 				   const RealVect& a_dx,
 				   const RealVect& a_probLo,
-				   const bool      a_force_irreg_ngp){
-  this->define(a_domain, a_ebisbox, a_dx, a_probLo, a_force_irreg_ngp);
+				   const bool      a_forceIrregNGP){
+  CH_TIME("EbParticleInterp::EbParticleInterp");
+  
+  this->define(a_region, a_ebisbox, a_dx, a_probLo, a_forceIrregNGP);
 }
 
-void EbParticleInterp::define(const Box&      a_domain,
+void EbParticleInterp::define(const Box&      a_region,
 			      const EBISBox&  a_ebisbox,
 			      const RealVect& a_dx,
 			      const RealVect& a_probLo,
-			      const bool      a_force_irreg_ngp){
-  m_domain  = a_domain;
-  m_ebisbox = &a_ebisbox;
-  m_probLo = a_probLo;
-  m_dx      = a_dx;
-  m_irr_ngp = a_force_irreg_ngp;
+			      const bool      a_forceIrregNGP){
+  CH_TIME("EbParticleInterp::define");
+  
+  m_region        = a_region;
+  m_ebisbox       = a_ebisbox;
+  m_probLo        = a_probLo;
+  m_dx            = a_dx;
+  m_forceIrregNGP = a_forceIrregNGP;
 }
 
 void EbParticleInterp::depositParticle(FArrayBox&           a_rho,
@@ -42,19 +50,18 @@ void EbParticleInterp::depositParticle(FArrayBox&           a_rho,
 				       const RealVect&      a_dx,
 				       const RealVect&      a_position,
 				       const Real&          a_mass,
-				       const DepositionType a_interpType){
+				       const DepositionType a_depositionType){
 
+  // TLDR: This performs regular deposition as if the particle lives on regular mesh data. If the cell is irregular we can use a class option
+  //       to enforce NGP deposition in those cells. If the particle lives in a multi-valued cell I have no idea how to handle deposition. 
+
+  // Check if the cell is irregular. 
   const RealVect rv = (a_position - a_probLo)/a_dx;
-  const IntVect iv = IntVect(D_DECL(floor(rv[0]), floor(rv[1]), floor(rv[2])));
+  const IntVect  iv = IntVect(D_DECL(floor(rv[0]), floor(rv[1]), floor(rv[2])));
+  const bool isIrregular = m_ebisbox.isIrregular(iv);
 
-  const bool multi_valued = m_ebisbox->isMultiValued(iv);
-  const bool irregular    = m_ebisbox->isIrregular(iv);
-  const bool regular      = !multi_valued && !irregular;
-
-  if(multi_valued) MayDay::Abort("EbParticleInterp::depositParticle - multivalued cells not supported (yet)");
-
-  // Irregular cells always do an NGP deposit to prevent clouds leaking into the other side. 
-  if(m_irr_ngp && !regular){
+  // We can force irregular cells to always do an NGP deposit to prevent clouds leaking into the other side. 
+  if(m_forceIrregNGP && isIrregular){
     FORT_NGP_DEPOSIT_SCALAR(CHF_FRA1(a_rho, 0),
 			    CHF_CONST_REALVECT(a_probLo),
 			    CHF_CONST_REALVECT(a_dx),
@@ -62,37 +69,48 @@ void EbParticleInterp::depositParticle(FArrayBox&           a_rho,
 			    CHF_CONST_REAL(a_mass));
   }
   else{
-    switch (a_interpType){
+    switch (a_depositionType){
     case DepositionType::NGP:
-      FORT_NGP_DEPOSIT_SCALAR(CHF_FRA1(a_rho, 0),
-			      CHF_CONST_REALVECT(a_probLo),
-			      CHF_CONST_REALVECT(a_dx),
-			      CHF_CONST_REALVECT(a_position),
-			      CHF_CONST_REAL(a_mass));
-      break;
+      {
+	FORT_NGP_DEPOSIT_SCALAR(CHF_FRA1(a_rho, 0),
+				CHF_CONST_REALVECT(a_probLo),
+				CHF_CONST_REALVECT(a_dx),
+				CHF_CONST_REALVECT(a_position),
+				CHF_CONST_REAL(a_mass));
+	break;
+      }
     case DepositionType::CIC:
-      FORT_CIC_DEPOSIT_SCALAR(CHF_FRA1(a_rho, 0),
-			      CHF_CONST_REALVECT(a_probLo),
-			      CHF_CONST_REALVECT(a_dx),
-			      CHF_CONST_REALVECT(a_position),
-			      CHF_CONST_REAL(a_mass));
-      break;
+      {
+	FORT_CIC_DEPOSIT_SCALAR(CHF_FRA1(a_rho, 0),
+				CHF_CONST_REALVECT(a_probLo),
+				CHF_CONST_REALVECT(a_dx),
+				CHF_CONST_REALVECT(a_position),
+				CHF_CONST_REAL(a_mass));
+	break;
+      }
     case DepositionType::TSC:
-      FORT_TSC_DEPOSIT_SCALAR(CHF_FRA1(a_rho, 0),
-			      CHF_CONST_REALVECT(a_probLo),
-			      CHF_CONST_REALVECT(a_dx),
-			      CHF_CONST_REALVECT(a_position),
-			      CHF_CONST_REAL(a_mass));
-      break;
+      {
+	FORT_TSC_DEPOSIT_SCALAR(CHF_FRA1(a_rho, 0),
+				CHF_CONST_REALVECT(a_probLo),
+				CHF_CONST_REALVECT(a_dx),
+				CHF_CONST_REALVECT(a_position),
+				CHF_CONST_REAL(a_mass));
+	break;
+      }
     case DepositionType::W4:
-      FORT_W4_DEPOSIT_SCALAR(CHF_FRA1(a_rho, 0),
-			     CHF_CONST_REALVECT(a_probLo),
-			     CHF_CONST_REALVECT(a_dx),
-			     CHF_CONST_REALVECT(a_position),
-			     CHF_CONST_REAL(a_mass));
-      break;
+      {
+	FORT_W4_DEPOSIT_SCALAR(CHF_FRA1(a_rho, 0),
+			       CHF_CONST_REALVECT(a_probLo),
+			       CHF_CONST_REALVECT(a_dx),
+			       CHF_CONST_REALVECT(a_position),
+			       CHF_CONST_REAL(a_mass));
+	break;
+      }
     default:
-      MayDay::Error("Invalid interpolation type in MeshInterp::depositParticle");
+      {
+	MayDay::Error("EbParticleInterp::depositParticle - logic bust, unknown particle deposition.");
+	break;
+      }
     }
   }
 }
@@ -102,17 +120,17 @@ void EbParticleInterp::depositParticle2(FArrayBox&           a_rho,
 					const RealVect&      a_dx,
 					const RealVect&      a_position,
 					const Real&          a_mass,
-					const DepositionType a_interpType){
+					const DepositionType a_depositionType){
 
   const RealVect rv = (a_position - a_probLo)/a_dx;
   const IntVect iv = IntVect(D_DECL(floor(rv[0]), floor(rv[1]), floor(rv[2])));
 
-  if(m_ebisbox->isMultiValued(iv)){
+  if(m_ebisbox.isMultiValued(iv)){
     MayDay::Abort("EbParticleInterp::depositParticle2 - multivalued cells not supported (yet)");
   }
 
   // Irregular cells always do an NGP deposit to prevent clouds leaking into the other side. 
-  if(m_ebisbox->isIrregular(iv)){
+  if(m_ebisbox.isIrregular(iv)){
     FORT_NGP_DEPOSIT_SCALAR(CHF_FRA1(a_rho, 0),
 			    CHF_CONST_REALVECT(a_probLo),
 			    CHF_CONST_REALVECT(a_dx),
@@ -120,7 +138,7 @@ void EbParticleInterp::depositParticle2(FArrayBox&           a_rho,
 			    CHF_CONST_REAL(a_mass));
   }
   else{
-    switch (a_interpType){
+    switch (a_depositionType){
     case DepositionType::NGP:
       FORT_NGP_DEPOSIT_SCALAR(CHF_FRA1(a_rho, 0),
 			      CHF_CONST_REALVECT(a_probLo),
@@ -146,17 +164,17 @@ void EbParticleInterp::depositParticle4(FArrayBox&           a_rho,
 					const RealVect&      a_dx,
 					const RealVect&      a_position,
 					const Real&          a_mass,
-					const DepositionType a_interpType){
+					const DepositionType a_depositionType){
 
   const RealVect rv = (a_position - a_probLo)/a_dx;
   const IntVect iv = IntVect(D_DECL(floor(rv[0]), floor(rv[1]), floor(rv[2])));
 
-  if(m_ebisbox->isMultiValued(iv)){
+  if(m_ebisbox.isMultiValued(iv)){
     MayDay::Abort("EbParticleInterp::depositParticle2 - multivalued cells not supported (yet)");
   }
 
   // Irregular cells always do an NGP deposit to prevent clouds leaking into the other side. 
-  if(m_ebisbox->isIrregular(iv)){
+  if(m_ebisbox.isIrregular(iv)){
     FORT_NGP_DEPOSIT_SCALAR(CHF_FRA1(a_rho, 0),
 			    CHF_CONST_REALVECT(a_probLo),
 			    CHF_CONST_REALVECT(a_dx),
@@ -164,7 +182,7 @@ void EbParticleInterp::depositParticle4(FArrayBox&           a_rho,
 			    CHF_CONST_REAL(a_mass));
   }
   else{
-    switch (a_interpType){
+    switch (a_depositionType){
     case DepositionType::NGP:
       FORT_NGP_DEPOSIT_SCALAR(CHF_FRA1(a_rho, 0),
 			      CHF_CONST_REALVECT(a_probLo),
@@ -194,12 +212,12 @@ void EbParticleInterp::interpolateParticle(Real&                a_particleField,
   const RealVect rv = (a_position - a_probLo)/a_dx;
   const IntVect iv = IntVect(D_DECL(floor(rv[0]), floor(rv[1]), floor(rv[2])));
 
-  if(m_ebisbox->isMultiValued(iv)){
+  if(m_ebisbox.isMultiValued(iv)){
     MayDay::Abort("EbParticleInterp::depositParticle - multivalued cells not supported (yet)");
   }
 
   // Irregular cells always do an NGP deposit to prevent clouds leaking into the other side. 
-  if(m_ebisbox->isIrregular(iv)){
+  if(m_ebisbox.isIrregular(iv)){
     FORT_NGP_INTERPOLATE_SCALAR(CHF_REAL(a_particleField),
 				CHF_CONST_FRA1(a_field,0),
 				CHF_CONST_REALVECT(a_probLo),
@@ -252,12 +270,12 @@ void EbParticleInterp::interpolateParticle(RealVect&            a_particleField,
   const RealVect rv = (a_position - a_probLo)/a_dx;
   const IntVect iv = IntVect(D_DECL(floor(rv[0]), floor(rv[1]), floor(rv[2])));
 
-  if(m_ebisbox->isMultiValued(iv)){
+  if(m_ebisbox.isMultiValued(iv)){
     MayDay::Abort("EbParticleInterp::depositParticle - multivalued cells not supported (yet)");
   }
 
   // Irregular cells always do an NGP deposit to prevent clouds leaking into the other side.
-  if(m_ebisbox->isIrregular(iv)){
+  if(m_ebisbox.isIrregular(iv)){
     FORT_NGP_INTERPOLATE_VECTOR(CHF_REALVECT(a_particleField),
 				CHF_CONST_FRA(a_field),
 				CHF_CONST_REALVECT(a_probLo),
