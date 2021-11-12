db7d9d1ff132292de34d8710c17a489a68a3575c
Robert Marskar <robert.marskar@sintef.no>
2021-11-12 09:51:29 +0100

    Code-walk of sigma solver

--- Source/SigmaSolver/CD_SigmaSolver.cpp
+++ Source/SigmaSolver/CD_SigmaSolver.cpp
@@ -10,28 +10,44 @@
 */
 
 // Chombo includes
-#include <EBArith.H>
+#include <CH_Timer.H>
 
 // Our includes
 #include <CD_SigmaSolver.H>
 #include <CD_DataOps.H>
 #include <CD_NamespaceHeader.H>
 
+constexpr int SigmaSolver::m_comp;
+constexpr int SigmaSolver::m_nComp;
+
 SigmaSolver::SigmaSolver(){
+  CH_TIME("SigmaSolver::SigmaSolver");
+
+  // Default settings. 
   this->setVerbosity(-1);
   this->setPhase(phase::gas);
   this->setRealm(Realm::Primal);
 }
 
 SigmaSolver::~SigmaSolver(){
-
+  CH_TIME("SigmaSolver::~SigmaSolver");
 }
 
 const std::string SigmaSolver::getRealm() const {
+  CH_TIME("SigmaSolver::getRealm");
+  if(m_verbosity > 5){
+    pout() << "SigmaSolver::getRealm" << endl;
+  }
+  
   return m_realm;
 }
 
 void SigmaSolver::setRealm(const std::string a_realm){
+  CH_TIME("SigmaSolver::setRealm");
+  if(m_verbosity > 5){
+    pout() << "SigmaSolver::setRealm" << endl;
+  }
+  
   m_realm = a_realm;
 }
 
@@ -40,12 +56,9 @@ void SigmaSolver::allocateInternals(){
   if(m_verbosity > 5){
     pout() << "SigmaSolver::allocateInternals" << endl;
   }
-
-  const int comp  = 0;
-  const int ncomp = 1;
   
-  m_amr->allocate(m_phi, m_realm, m_phase, ncomp);
-  m_amr->allocate(m_flux,  m_realm, m_phase, ncomp);
+  m_amr->allocate(m_phi,  m_realm, m_phase, m_nComp);
+  m_amr->allocate(m_flux, m_realm, m_phase, m_nComp);
 }
 
 void SigmaSolver::preRegrid(const int a_lbase, const int a_oldFinestLevel){
@@ -53,14 +66,10 @@ void SigmaSolver::preRegrid(const int a_lbase, const int a_oldFinestLevel){
   if(m_verbosity > 5){
     pout() << "SigmaSolver::preRegrid" << endl;
   }
-
-  const int ncomp = 1;
-  const int finest_level = m_amr->getFinestLevel();
   
-  m_amr->allocate(m_cache, m_realm, m_phase, ncomp);
-  DataOps::setValue(m_cache, 0.0);
+  m_amr->allocate(m_cache, m_realm, m_phase, m_nComp);
   
-  for (int lvl = 0; lvl <= a_oldFinestLevel; lvl++){
+  for (int lvl = 0; lvl <= m_amr->getFinestLevel(); lvl++){
     m_phi[lvl]->localCopyTo(*m_cache[lvl]);
   }
 }
@@ -70,6 +79,7 @@ void SigmaSolver::computeRHS(EBAMRIVData& a_rhs){
   if(m_verbosity > 5){
     pout() << "SigmaSolver::computeRHS" << endl;
   }
+  
   for (int lvl = 0; lvl <= m_amr->getFinestLevel(); lvl ++){
     m_flux[lvl]->localCopyTo(*a_rhs[lvl]);
   }
@@ -91,86 +101,89 @@ void SigmaSolver::regrid(const int a_lmin, const int a_oldFinestLevel, const int
     pout() << "SigmaSolver::regrid" << endl;
   }
 
+  // TLDR: This is pretty old code and I'm reluctant to change it even though it has a potential performance bottleneck here (due to the EBISLayout filling). What
+  //       this code does is that we are essentially figuring out how to interpolate the data if a grid is added. Due to locality constraint, we have to coarsen
+  //       the fine grids and copy the previous m_phi data onto those grids (the coarsened fine grids). This provides locality between the coarse grid and the fine grid
+  //       and we can interpolate from that. 
+
   const RefCountedPtr<EBIndexSpace> ebis = m_multifluidIndexSpace->getEBIndexSpace(m_phase);
 
-  const int ebghost = 4; // m_amr->getNumberOfEbGhostCells();
-  const int comp  = 0;
-  const int ncomp = 1;
-  const Interval interv(comp, comp);
+  const int ebghost = 4;// m_amr->getNumberOfEbGhostCells();
+  const Interval interv(m_comp, m_comp);
 
   this->allocateInternals();
 
   DataOps::setValue(m_phi, 0.0);
 
-  // These levels have never changed
-  for (int lvl = 0; lvl <= Max(0, a_lmin-1); lvl++){
+  // These levels have never changed but the ownership might have, so we have to copy in order to ensure that data
+  // ends up in the right place. 
+  for (int lvl = 0; lvl <= std::max(0, a_lmin-1); lvl++){
     m_cache[lvl]->copyTo(*m_phi[lvl]); 
   }
 
-  // These levels have changed
-  for (int lvl = Max(1,a_lmin); lvl <= a_newFinestLevel; lvl++){
-    const DisjointBoxLayout& fine_grid = m_amr->getGrids(m_realm)[lvl];
-    const ProblemDomain& fine_domain   = m_amr->getDomains()[lvl];
-    const ProblemDomain& coar_domain   = m_amr->getDomains()[lvl-1];
-    const EBISLayout& fine_ebisl       = m_amr->getEBISLayout(m_realm, m_phase)[lvl];
-    const int nref                     = m_amr->getRefinementRatios()[lvl-1];
+  // These levels have changed and so we need to do the interpolation here. 
+  for (int lvl = std::max(1,a_lmin); lvl <= a_newFinestLevel; lvl++){
+    const DisjointBoxLayout& fineGrid = m_amr->getGrids(m_realm)              [lvl  ];
+    const ProblemDomain& fineDomain   = m_amr->getDomains()                   [lvl  ];
+    const ProblemDomain& coarDomain   = m_amr->getDomains()                   [lvl-1];
+    const EBISLayout& fineEBISL       = m_amr->getEBISLayout(m_realm, m_phase)[lvl  ];    
+    const int nref                    = m_amr->getRefinementRatios()          [lvl-1];
     
     // Fill a coarsened grid and a coarsened ebisl
-    EBISLayout coar_ebisl;
-    DisjointBoxLayout coar_grid = DisjointBoxLayout();
-    coarsen(coar_grid, fine_grid, nref);
-    ebis->fillEBISLayout(coar_ebisl, coar_grid, coar_domain, ebghost);
+    EBISLayout        coarEBISL;
+    DisjointBoxLayout coarGrid;
+    coarsen(coarGrid, fineGrid, nref);
+    ebis->fillEBISLayout(coarEBISL, coarGrid, coarDomain, ebghost);
 
     // Need extra storage for coarse data
-    LayoutData<IntVectSet> sets(coar_grid);
-    for (DataIterator dit = coar_grid.dataIterator(); dit.ok(); ++dit){
-      Box box          = coar_grid.get(dit());
+    LayoutData<IntVectSet> sets(coarGrid);
+    for (DataIterator dit = coarGrid.dataIterator(); dit.ok(); ++dit){
+      Box box          = coarGrid.get(dit());
       box.grow(3);
-      box &= coar_domain;
+      box &= coarDomain;
       
-      const EBISBox& ebisbox = coar_ebisl[dit()];
+      const EBISBox& ebisbox = coarEBISL[dit()];
       const IntVectSet ivs   = ebisbox.getIrregIVS(box);
 
       sets[dit()] = ivs;
     }
 
     // Allocate storage for coarsened data
-    BaseIVFactory<Real> ivfact(coar_ebisl, sets);
-    LevelData<BaseIVFAB<Real> > coarsened_fine_data(coar_grid, ncomp, m_phi[0]->ghostVect(), ivfact);
+    BaseIVFactory<Real> ivfact(coarEBISL, sets);
+    LevelData<BaseIVFAB<Real> > coFiData(coarGrid, m_nComp, m_phi[0]->ghostVect(), ivfact);
 
-    //
-    EBLevelDataOps::setVal(coarsened_fine_data, 0.0);
-    m_phi[lvl-1]->copyTo(coarsened_fine_data);
-
+    // Copy the coarse data to the local view of the current fine data (we have already done the coarse level). 
+    EBLevelDataOps::setVal(coFiData, 0.0);
+    m_phi[lvl-1]->copyTo(coFiData);
 
     // Loop through coarse grid and interpolate to fine grid
-    for (DataIterator dit = coar_grid.dataIterator(); dit.ok(); ++dit){
+    for (DataIterator dit = coarGrid.dataIterator(); dit.ok(); ++dit){
       BaseIVFAB<Real>& fine_state       = (*m_phi[lvl])[dit()];
-      const BaseIVFAB<Real>& coar_state = coarsened_fine_data[dit()];
-      const EBISBox& coar_ebisbox       = coar_ebisl[dit()];
-      const EBISBox& fine_ebisbox       = fine_ebisl[dit()];
+      const BaseIVFAB<Real>& coar_state = coFiData     [dit()];
+      const EBISBox& coar_ebisbox       = coarEBISL    [dit()];
+      const EBISBox& fine_ebisbox       = fineEBISL    [dit()];
 
       const IntVectSet ivs   = fine_state.getIVS();
       const EBGraph& ebgraph = fine_state.getEBGraph();
 
       for (VoFIterator vofit(ivs, ebgraph); vofit.ok(); ++vofit){
 	const VolIndex& fine_vof = vofit();
-	const VolIndex  coar_vof = fine_ebisl.coarsen(fine_vof, nref, dit());
+	const VolIndex  coar_vof = fineEBISL.coarsen(fine_vof, nref, dit());
 	const Real coarArea      = coar_ebisbox.bndryArea(coar_vof);
 	
 	// Same sigma for all refined cells such that charge is conserved. 
-	const Vector<VolIndex> refined_vofs = coar_ebisl.refine(coar_vof, nref, dit());
+	const Vector<VolIndex> refined_vofs = coarEBISL.refine(coar_vof, nref, dit());
 	Real fineArea = 0.0;
 	for (int i = 0; i < refined_vofs.size(); i++){
 	  fineArea += fine_ebisbox.bndryArea(refined_vofs[i]);
 	}
 
-	// Initialize conserved charge
+	// Initialize conserved charge. If there is no real area just set the state to zero. 
 	if(fineArea > 0.0 && coarArea > 0.0){
-	  fine_state(fine_vof, comp) = coar_state(coar_vof, comp)*nref*coarArea/fineArea;
+	  fine_state(fine_vof, m_comp) = coar_state(coar_vof, m_comp)*nref*coarArea/fineArea;
 	}
 	else{
-	  fine_state(fine_vof, comp) = 0.0;
+	  fine_state(fine_vof, m_comp) = 0.0;
 	}
       }
     }
@@ -191,7 +204,7 @@ void SigmaSolver::registerOperators(){
   }
 
   if(m_amr.isNull()){
-    MayDay::Abort("SigmaSolver::registerOperators - need to set AmrMesh!");
+    MayDay::Error("SigmaSolver::registerOperators - need to set AmrMesh!");
   }
   else{
     m_amr->registerOperator(s_eb_coar_ave, m_realm, m_phase);
@@ -204,25 +217,23 @@ void SigmaSolver::resetCells(EBAMRIVData& a_data){
     pout() << "SigmaSolver::resetCells" << endl;
   }
 
-  const int finest_level = m_amr->getFinestLevel();
-
-  for (int lvl = 0; lvl <= finest_level; lvl++){
-    const DisjointBoxLayout& dbl = m_amr->getGrids(m_realm)[lvl];
-    const Real dx                = m_amr->getDx()[lvl];
+  for (int lvl = 0; lvl <= m_amr->getFinestLevel(); lvl++){
+    const DisjointBoxLayout& dbl =  m_amr->getGrids(m_realm)      [lvl];
+    const Real dx                =  m_amr->getDx()                [lvl];
     const MFLevelGrid& mflg      = *m_amr->getMFLevelGrid(m_realm)[lvl];
 
     for (DataIterator dit = dbl.dataIterator(); dit.ok(); ++dit){
-      const Box box          = dbl.get(dit());
+      const Box box          = dbl           [dit()];
       BaseIVFAB<Real>& data  = (*a_data[lvl])[dit()];
+      
       IntVectSet ivs         = data.getIVS();
       const EBGraph& ebgraph = data.getEBGraph();
       
       ivs -= mflg.interfaceRegion(box, dit());
 
       for (VoFIterator vofit(ivs, ebgraph); vofit.ok(); ++vofit){
-	const VolIndex& vof = vofit();
 	for (int comp = 0; comp < data.nComp(); comp++){
-	  data(vof, comp) = 0.0;
+	  data(vofit(), comp) = 0.0;
 	}
       }
     }
@@ -245,7 +256,7 @@ void SigmaSolver::setComputationalGeometry(const RefCountedPtr<ComputationalGeom
   }
 
   m_computationalGeometry = a_computationalGeometry;
-  m_multifluidIndexSpace     = m_computationalGeometry->getMfIndexSpace();
+  m_multifluidIndexSpace  = m_computationalGeometry->getMfIndexSpace();
 }
 
 void SigmaSolver::setPhase(phase::which_phase a_phase){
@@ -263,9 +274,7 @@ void SigmaSolver::setSigma(const EBAMRIVData& a_sigma){
     pout() << "SigmaSolver::setSigma(ebamrivdata)" << endl;
   }
 
-  const int finest_level = m_amr->getFinestLevel();
-
-  for (int lvl = 0; lvl <= finest_level; lvl++){
+  for (int lvl = 0; lvl <= m_amr->getFinestLevel(); lvl++){
     a_sigma[lvl]->localCopyTo(*m_phi[lvl]);
   }
 
@@ -273,14 +282,12 @@ void SigmaSolver::setSigma(const EBAMRIVData& a_sigma){
 }
 
 void SigmaSolver::setSigma(const Real a_sigma){
-  CH_TIME("SigmaSolver::setSigma(constant)");
+  CH_TIME("SigmaSolver::setSigma(Real)");
   if(m_verbosity > 5){
-    pout() << "SigmaSolver::setSigma(constant)" << endl;
+    pout() << "SigmaSolver::setSigma(Real)" << endl;
   }
 
-  const int finest_level = m_amr->getFinestLevel();
-
-  for (int lvl = 0; lvl <= finest_level; lvl++){
+  for (int lvl = 0; lvl <= m_amr->getFinestLevel(); lvl++){
     DataOps::setValue(*m_phi[lvl], a_sigma);
   }
 
@@ -289,13 +296,12 @@ void SigmaSolver::setSigma(const Real a_sigma){
 
 void SigmaSolver::setVerbosity(const int a_verbosity){
   CH_TIME("SigmaSolver::setVerbosity");
+  
   m_verbosity = a_verbosity;
   
   if(m_verbosity > 5){
     pout() << "SigmaSolver::setVerbosity" << endl;
   }
-  
-
 }
 
 void SigmaSolver::setTime(const int a_step, const Real a_time, const Real a_dt){
@@ -305,8 +311,8 @@ void SigmaSolver::setTime(const int a_step, const Real a_time, const Real a_dt){
   }
   
   m_timeStep = a_step;
-  m_time = a_time;
-  m_dt   = a_dt;
+  m_time     = a_time;
+  m_dt       = a_dt;
 }
 
 #ifdef CH_USE_HDF5
@@ -316,8 +322,11 @@ void SigmaSolver::writeCheckpointLevel(HDF5Handle& a_handle, const int a_level) 
     pout() << "SigmaSolver::writeCheckpointLevel" << endl;
   }
 
+  // Allocate some transient storage.
   EBCellFactory fact(m_amr->getEBISLayout(m_realm, phase::gas)[a_level]);
   LevelData<EBCellFAB> scratch(m_amr->getGrids(m_realm)[a_level], 1, 3*IntVect::Unit, fact);
+
+  // Copy data into transient storage.
   DataOps::setValue(scratch, 0.0);
   DataOps::incr(scratch, *m_phi[a_level], 1.0);
 
@@ -333,15 +342,19 @@ void SigmaSolver::readCheckpointLevel(HDF5Handle& a_handle, const int a_level){
     pout() << "SigmaSolver::readCheckpointLevel" << endl;
   }
 
-  const EBISLayout& ebisl = m_amr->getEBISLayout(m_realm, phase::gas)[a_level];
-  const DisjointBoxLayout& dbl = m_amr->getGrids(m_realm)[a_level];
-  
+  // TLDR: Chombo wants us to read into EBCellFABs so we allocate LevelData<EBCellFAB> and read into those. After that, we copy
+  //       the result to our internal EBAMRIVFAB
+
+  const EBISLayout&        ebisl = m_amr->getEBISLayout(m_realm, phase::gas)[a_level];
+  const DisjointBoxLayout& dbl   = m_amr->getGrids     (m_realm            )[a_level];
+
+  // Transient that we read into.
   EBCellFactory fact(ebisl);
   LevelData<EBCellFAB> scratch(dbl, 1, 3*IntVect::Unit, fact);
   DataOps::setValue(scratch, 0.0);
   read<EBCellFAB>(a_handle, scratch, "sigma", dbl, Interval(0,0), false);
 
-		     
+  // Copy data to our EBAMRIVData
   DataOps::setValue(*m_phi[a_level], 0.0);
   DataOps::incr(*m_phi[a_level], scratch, 1.0);
 }
@@ -353,12 +366,14 @@ void SigmaSolver::writePlotData(EBAMRCellData& a_output, int& a_comp){
     pout() << "SigmaSolver::writePlotData" << endl;
   }
 
+  // TLDR: We can't plot or write an EBAMRIVData data structure, so we copy our results to an EBAMRCellData so we can copy into a_output. 
 
+  // Allocate scratch storage that we can work with when we copy over to an EBAMRCellData.
   EBAMRCellData scratch;
   m_amr->allocate(scratch, m_realm, m_phase, 1);
 
 
-  // Write sigma
+  // Write sigma. Just copy over to scratch, and then copy over to a_output after that. 
   DataOps::setValue(scratch, 0.0);
   DataOps::incr(scratch, m_phi, 1.0);
   for (int lvl = 0; lvl <= m_amr->getFinestLevel(); lvl++){
@@ -371,7 +386,7 @@ void SigmaSolver::writePlotData(EBAMRCellData& a_output, int& a_comp){
   }
   a_comp++;
 
-  // Write flux
+  // Write flux. Just the same thing as the code above. 
   DataOps::setValue(scratch, 0.0);
   DataOps::incr(scratch, m_flux, 1.0);
   for (int lvl = 0; lvl <= m_amr->getFinestLevel(); lvl++){
@@ -394,12 +409,12 @@ int SigmaSolver::getNumberOfPlotVariables(){
   return 2;
 }
   
-  
 Vector<std::string> SigmaSolver::getPlotVariableNames() const{
   CH_TIME("SigmaSolver::getNumberOfPlotVariables");
   if(m_verbosity > 5){
     pout() << "SigmaSolver::getNumberOfPlotVariables" << endl;
   }
+  
   Vector<std::string> ret(2);
 
   ret[0] = "surface charge density";
@@ -408,29 +423,32 @@ Vector<std::string> SigmaSolver::getPlotVariableNames() const{
   return ret;
 }
 
-
 Real SigmaSolver::computeCharge(){
   CH_TIME("SigmaSolver::computeCharge");
   if(m_verbosity > 5){
     pout() << "SigmaSolver::computeCharge" << endl;
   }
 
+  // TLDR: Since we can coarsen conservatively, we compute coarsen the solution
+  //       onto the coarsest level and do the integration there. 
+
   m_amr->averageDown(m_phi, m_realm, m_phase);
 
   Real charge = 0.0;
 
   const int comp               = 0;
-  const DisjointBoxLayout& dbl = m_amr->getGrids(m_realm)[0];
+  const DisjointBoxLayout& dbl = m_amr->getGrids     (m_realm         )[0];
   const EBISLayout& ebisl      = m_amr->getEBISLayout(m_realm, m_phase)[0];
-  const Real dx                = m_amr->getDx()[0];
+  const Real dx                = m_amr->getDx()                        [0];
   
-  for (DataIterator dit = dbl.dataIterator(); dit.ok(); ++dit){
+  for (DataIterator dit(dbl); dit.ok(); ++dit){
+    const Box box          = dbl  [dit()];    
     const EBISBox& ebisbox = ebisl[dit()];
-    const EBGraph& ebgraph = ebisbox.getEBGraph();
-    const Box box          = dbl.get(dit());
-    const IntVectSet irreg = ebisbox.getIrregIVS(box);
+    
+    const EBGraph&   ebgraph  = ebisbox.getEBGraph();
+    const IntVectSet irregIVS = ebisbox.getIrregIVS(box);
 
-    for (VoFIterator vofit(irreg, ebgraph); vofit.ok(); ++vofit){
+    for (VoFIterator vofit(irregIVS, ebgraph); vofit.ok(); ++vofit){
       const VolIndex& vof = vofit();
       const Real& area    = ebisbox.bndryArea(vof);
 
@@ -438,9 +456,10 @@ Real SigmaSolver::computeCharge(){
     }
   }
 
-  DataOps::sum(charge); // Parallell sum
-  
-  return charge*dx;
+  DataOps::sum(charge); // Parallell sum.
+
+  // Return charge 
+  return charge*std::pow(dx, SpaceDim-1);
 }
 
 EBAMRIVData& SigmaSolver::getPhi(){
