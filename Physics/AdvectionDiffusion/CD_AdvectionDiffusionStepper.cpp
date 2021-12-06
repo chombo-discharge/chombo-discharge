2b4ef07a34ca013ca435c99f0a51b612644d4a53
Robert Marskar <robert.marskar@sintef.no>
2021-12-06 14:57:27 +0100

    Docs to AdvectionDiffusion

--- Physics/AdvectionDiffusion/CD_AdvectionDiffusionStepper.cpp
+++ Physics/AdvectionDiffusion/CD_AdvectionDiffusionStepper.cpp
@@ -22,6 +22,8 @@
 using namespace Physics::AdvectionDiffusion;
 
 AdvectionDiffusionStepper::AdvectionDiffusionStepper(){
+  CH_TIME("AdvectionDiffusionStepper::AdvectionDiffusionStepper");
+  
   ParmParse pp("AdvectionDiffusion");
 
   m_phase = phase::gas;
@@ -29,33 +31,65 @@ AdvectionDiffusionStepper::AdvectionDiffusionStepper(){
   pp.get("realm",      m_realm);
   pp.get("verbosity",  m_verbosity); 
   pp.get("fhd",        m_fhd); 
-  pp.get("diffco",     m_faceCenteredDiffusionCoefficient);
+  pp.get("diffco",     m_diffCo);
   pp.get("omega",      m_omega);
   pp.get("cfl",        m_cfl);
-  pp.get("integrator", m_integrator);
+
+  this->parseIntegrator();  
 }
 
 AdvectionDiffusionStepper::AdvectionDiffusionStepper(RefCountedPtr<CdrSolver>& a_solver) : AdvectionDiffusionStepper() {
+  CH_TIME("AdvectionDiffusionStepper::AdvectionDiffusionStepper(full)");
+  
   m_solver = a_solver;
 }
 
 AdvectionDiffusionStepper::~AdvectionDiffusionStepper(){
+  CH_TIME("AdvectionDiffusionStepper::~AdvectionDiffusionStepper");  
 }
 
 void AdvectionDiffusionStepper::parseRuntimeOptions() {
+  CH_TIME("AdvectionDiffusionStepper::parseRuntimeOptions");
+  
   ParmParse pp("AdvectionDiffusion");
-
+  
   pp.get("verbosity",  m_verbosity);
   pp.get("cfl",        m_cfl);
-  pp.get("integrator", m_integrator);
 
+  this->parseIntegrator();
+  
   m_solver->parseRuntimeOptions();
 }
 
+void AdvectionDiffusionStepper::parseIntegrator(){
+  CH_TIME("AdvectionDiffusionStepper::parseIntegrator");
+  
+  ParmParse pp("AdvectionDiffusion");
+
+  std::string str;  
+
+  pp.get("integrator", str);
+
+  if(str == "heun"){
+    m_integrator = Integrator::Heun;
+  }
+  else if(str == "imex"){
+    m_integrator = Integrator::EulerIMEX;
+  }
+  else{
+    MayDay::Error("AdvectionDiffusionStepper::parseIntegrator -- logic bust");
+  }  
+}
+
 void AdvectionDiffusionStepper::setupSolvers(){
+  CH_TIME("AdvectionDiffusionStepper::setupSolvers");
+
+  CH_assert(!m_solver.isNull());
+
+  // Instantiate the species. 
   m_species = RefCountedPtr<CdrSpecies> (new AdvectionDiffusionSpecies());
 
-  // Solver setup
+  // Set up the solver. 
   m_solver->setVerbosity(m_verbosity);
   m_solver->setSpecies(m_species);
   m_solver->parseOptions();
@@ -65,22 +99,32 @@ void AdvectionDiffusionStepper::setupSolvers(){
   m_solver->setRealm(m_realm);
   
   if(!m_solver->isMobile() && !m_solver->isDiffusive()){
-    MayDay::Abort("AdvectionDiffusionStepper::setupSolvers - can't turn off both advection AND diffusion");
+    MayDay::Error("AdvectionDiffusionStepper::setupSolvers - can't turn off both advection AND diffusion");
   }
 }
 
 void AdvectionDiffusionStepper::postInitialize() {
+  CH_TIME("AdvectionDiffusionStepper::postInitialize");  
 }
 
 void AdvectionDiffusionStepper::registerRealms() {
+  CH_TIME("AdvectionDiffusionStepper::registerRealms");
+  
   m_amr->registerRealm(m_realm);
 }
 
 void AdvectionDiffusionStepper::registerOperators(){
+  CH_TIME("AdvectionDiffusionStepper::registerOperators");
+
+  // Let the solver do this -- it knows what it needs. 
   m_solver->registerOperators();
 }
 
 void AdvectionDiffusionStepper::allocate() {
+  CH_TIME("AdvectionDiffusionStepper::allocate");
+
+  // Allocate storage for running the TimeStepper.
+  
   m_solver->allocateInternals();
 
   m_amr->allocate(m_tmp, m_realm, m_phase, 1);
@@ -89,12 +133,16 @@ void AdvectionDiffusionStepper::allocate() {
 }
 
 void AdvectionDiffusionStepper::initialData(){
-  m_solver->initialData();       // Fill initial through the cdr species
+  CH_TIME("AdvectionDiffusionStepper::initialData");
 
+  // Fill the solver with initial data from the species.
+  m_solver->initialData();
+
+  // Set velocity, diffusion coefficient, and boundary conditions.
   m_solver->setSource(0.0);
   m_solver->setEbFlux(0.0);
   if(m_solver->isDiffusive()){
-    m_solver->setDiffusionCoefficient(m_faceCenteredDiffusionCoefficient);
+    m_solver->setDiffusionCoefficient(m_diffCo);
   }
   if(m_solver->isMobile()){
     this->setVelocity();
@@ -109,160 +157,210 @@ void AdvectionDiffusionStepper::initialData(){
 }
 
 void AdvectionDiffusionStepper::setVelocity(){
-  const int finest_level = m_amr->getFinestLevel();
-  for (int lvl = 0; lvl <= finest_level; lvl++){
-    this->setVelocity(lvl);
-  }
+  CH_TIME("AdvectionDiffusionStepper::setVelocity");
+  
+  // Set the velocity.
+  auto veloFunc = [omega=this->m_omega](const RealVect pos) -> RealVect {
+    const Real r     = sqrt(pos[0]*pos[0] + pos[1]*pos[1]);
+    const Real theta = atan2(pos[1], pos[0]);
 
+    return RealVect(D_DECL(-r*omega*sin(theta), r*omega*cos(theta), 0.));
+  };
+
+  m_solver->setVelocity(veloFunc);  
+
+  // Make sure we have updated ghost cells (they are needed for the flux computation)
   EBAMRCellData& vel = m_solver->getCellCenteredVelocity();
   m_amr->averageDown(vel, m_realm, m_phase);
   m_amr->interpGhost(vel, m_realm, m_phase);
 }
 
-void AdvectionDiffusionStepper::setVelocity(const int a_level){
-  // TLDR: This code uses a lambda for setting the velocity field to omega*r
-
-  const Real omega = m_omega;
-  
-  auto veloFunc = [&](const RealVect pos) -> RealVect {
-    const Real r     = sqrt(pos[0]*pos[0] + pos[1]*pos[1]);
-    const Real theta = atan2(pos[1], pos[0]);
-
-    return RealVect(D_DECL(-r*m_omega*sin(theta), r*m_omega*cos(theta), 0.));
-  };
-
-  m_solver->setVelocity(veloFunc);
-
-}
-
 #ifdef CH_USE_HDF5
 void AdvectionDiffusionStepper::writeCheckpointData(HDF5Handle& a_handle, const int a_lvl) const {
+  CH_TIME("AdvectionDiffusionStepper::writeCheckpointData");
+  
   m_solver->writeCheckpointLevel(a_handle, a_lvl);
 }
 #endif
 
 #ifdef CH_USE_HDF5
 void AdvectionDiffusionStepper::readCheckpointData(HDF5Handle& a_handle, const int a_lvl){
+  CH_TIME("AdvectionDiffusionStepper::readCheckpointData");
+  
   m_solver->readCheckpointLevel(a_handle, a_lvl);
 }
 #endif
 
 void AdvectionDiffusionStepper::postCheckpointSetup(){
+  CH_TIME("AdvectionDiffusionStepper::postCheckpointSetup");
+
+  // Set velocity, diffusion coefficient, and boundary conditions. 
   m_solver->setSource(0.0);
   m_solver->setEbFlux(0.0);
   if(m_solver->isDiffusive()){
-    m_solver->setDiffusionCoefficient(m_faceCenteredDiffusionCoefficient);
+    m_solver->setDiffusionCoefficient(m_diffCo);
   }
   if(m_solver->isMobile()){
     this->setVelocity();
   }
+
+  // Set flux functions
+  auto fluxFunc = [](const RealVect a_pos, const Real a_time){
+    return 0.0;
+  };
+
+  //m_solver->setDomainFlux(fluxFunc);  
 }
 
 int AdvectionDiffusionStepper::getNumberOfPlotVariables() const{
+  CH_TIME("AdvectionDiffusionStepper::getNumberOfPlotVariables");
+
+  // Not plotting anything of our own, so return whatever the solver wants to plot. 
   return m_solver->getNumberOfPlotVariables();
 }
 
 void AdvectionDiffusionStepper::writePlotData(EBAMRCellData&       a_output,
 					      Vector<std::string>& a_plotVariableNames,
 					      int&                 a_icomp) const {
+  CH_TIME("AdvectionDiffusionStepper::writePlotData");
+
+  // Append plot variable names
   a_plotVariableNames.append(m_solver->getPlotVariableNames());
+
+  // Solver writes data to be plotted.
   m_solver->writePlotData(a_output, a_icomp);
 }
 
 void AdvectionDiffusionStepper::computeDt(Real& a_dt, TimeCode& a_timeCode){
-  if(m_integrator == 0){      // Explicit diffusion. 
-    const Real dt = m_solver->computeAdvectionDiffusionDt();
-    
-    a_dt       = m_cfl*dt;
-    a_timeCode = TimeCode::AdvectionDiffusion;
-  }
-  else if(m_integrator == 1){ // Implicit diffusion
-    const Real dt = m_solver->computeAdvectionDt();
+  CH_TIME("AdvectionDiffusionStepper::computeDt");
 
-    a_dt       = m_cfl*dt;
-    a_timeCode = TimeCode::Advection;
-  }
-  else{
-    MayDay::Abort("AdvectionDiffusionStepper::computeDt - logic bust");
+  // TLDR: If we run explicit advection but implicit diffusion then we are only limited by the advective CFL. Otherwise,
+  //       if diffusion is also explicit we need the advection-diffusion limited time step. 
+
+  switch(m_integrator){
+  case Integrator::Heun:
+    {
+      const Real dt = m_solver->computeAdvectionDiffusionDt();
+    
+      a_dt       = m_cfl*dt;
+      a_timeCode = TimeCode::AdvectionDiffusion;
+      break;
+    }
+  case Integrator::EulerIMEX:
+    {
+      const Real dt = m_solver->computeAdvectionDt();
+
+      a_dt       = m_cfl*dt;
+      a_timeCode = TimeCode::Advection;
+      break;
+    }
+  default:
+    MayDay::Error("AdvectionDiffusionStepper::computeDt - logic bust");
+    break;
   }
 }
 
 Real AdvectionDiffusionStepper::advance(const Real a_dt){
+  CH_TIME("AdvectionDiffusionStepper::advance");
+
+
+  // State to be advanced. 
   EBAMRCellData& state = m_solver->getPhi();
-  
-  if(m_integrator == 0){ //   Use Heun's method
-    m_solver->computeDivJ(m_k1, state, 0.0);
-    DataOps::copy(m_tmp, state);
-    DataOps::incr(m_tmp, m_k1, -a_dt); // m_tmp = phi - dt*div(J)
 
-    m_solver->computeDivJ(m_k2, m_tmp, 0.0);
-    DataOps::incr(state, m_k1, -0.5*a_dt);
-    DataOps::incr(state, m_k2, -0.5*a_dt); // Done with deterministic update.
+  switch(m_integrator){
+  case Integrator::Heun:
+    {
 
-    // Add random diffusion flux. This is equivalent to a 1st order. Godunov splitting
-    if(m_fhd){
-      m_solver->gwnDiffusionSource(m_k1, state); // k1 holds random diffusion
-      DataOps::incr(state, m_k1, a_dt);
-    }
-  
-    m_amr->averageDown(state, m_realm, m_phase);
-    m_amr->interpGhost(state, m_realm, m_phase);
-  }
-  else if(m_integrator == 1){
-    const bool addEbFlux     = true;
-    const bool addDomainFlux = true;
-    
-    m_solver->computeDivF(m_k1, state, a_dt, addEbFlux, addDomainFlux);
-    DataOps::incr(state, m_k1, -a_dt);
+      // Compute k1 coefficient
+      m_solver->computeDivJ(m_k1, state, 0.0);
+      DataOps::copy(m_tmp, state);
+      DataOps::incr(m_tmp, m_k1, -a_dt);
 
-    if(m_solver->isDiffusive()){
-      DataOps::copy(m_k2, state); // Now holds phiOld - dt*div(F)
+      // Compute k2 coefficient and final state
+      m_solver->computeDivJ(m_k2, m_tmp, 0.0);
+      DataOps::incr(state, m_k1, -0.5*a_dt);
+      DataOps::incr(state, m_k2, -0.5*a_dt); 
+
+      // Add random diffusion flux. This is equivalent to a 1st order. Godunov splitting in an FHD context. 
       if(m_fhd){
 	m_solver->gwnDiffusionSource(m_k1, state); // k1 holds random diffusion
-	DataOps::incr(m_k2, m_k1, a_dt);
+	DataOps::incr(state, m_k1, a_dt);
       }
-      DataOps::setValue(m_k1, 0.0);
-      m_solver->advanceEuler(state, m_k2, m_k1, a_dt);
+  
+      m_amr->averageDown(state, m_realm, m_phase);
+      m_amr->interpGhost(state, m_realm, m_phase);
+      
+      break;
     }
+  case Integrator::EulerIMEX:
+    {
+      const bool addEbFlux     = true;
+      const bool addDomainFlux = true;
+    
+      m_solver->computeDivF(m_k1, state, a_dt, addEbFlux, addDomainFlux);
+      DataOps::incr(state, m_k1, -a_dt);
 
-    m_amr->averageDown(state, m_realm, m_phase);
-    m_amr->averageDown(state, m_realm, m_phase);
-  }
-  else{
-    MayDay::Abort("AdvectionDiffusionStepper - unknown integrator requested");
+      if(m_solver->isDiffusive()){
+	DataOps::copy(m_k2, state); // Now holds phiOld - dt*div(F)
+	if(m_fhd){
+	  m_solver->gwnDiffusionSource(m_k1, state); // k1 holds random diffusion
+	  DataOps::incr(m_k2, m_k1, a_dt);
+	}
+	DataOps::setValue(m_k1, 0.0);
+	m_solver->advanceEuler(state, m_k2, m_k1, a_dt);
+      }
+
+      m_amr->averageDown(state, m_realm, m_phase);
+      m_amr->averageDown(state, m_realm, m_phase);
+      
+      break;
+    }
+  default:
+    MayDay::Error("AdvectionDiffusionStepper - unknown integrator requested");
+    break;
   }
   
   return a_dt;
 }
 
 void AdvectionDiffusionStepper::synchronizeSolverTimes(const int a_step, const Real a_time, const Real a_dt){
+  CH_TIME("AdvectionDiffusionStepper::synchronizeSolverTimes");
+  
   m_timeStep = a_step;
-  m_time = a_time;
-  m_dt   = a_dt;
+  m_time     = a_time;
+  m_dt       = a_dt;
   
   m_solver->setTime(a_step, a_time, a_dt);
 }
 
 void AdvectionDiffusionStepper::printStepReport(){
-
+  CH_TIME("AdvectionDiffusionStepper::printStepReport");
 }
 
 bool AdvectionDiffusionStepper::needToRegrid(){
+  CH_TIME("AdvectionDiffusionStepper::needToRegrid");
+  
   return false;
 }
 
 void AdvectionDiffusionStepper::preRegrid(const int a_lbase, const int a_oldFinestLevel){
+  CH_TIME("AdvectionDiffusionStepper::preRegrid");
+
+  // TLDR: Call solver preRegrid function -- it knows what to do. 
   m_solver->preRegrid(a_lbase, a_oldFinestLevel);
 }
 
 void AdvectionDiffusionStepper::regrid(const int a_lmin, const int a_oldFinestLevel, const int a_newFinestLevel){
-
+  CH_TIME("AdvectionDiffusionStepper::regrid");
+  
   // Regrid CDR solver
   m_solver->regrid(a_lmin, a_oldFinestLevel, a_newFinestLevel);
+
+  // Set up the flow fields again. 
   m_solver->setSource(0.0);
   m_solver->setEbFlux(0.0);
   if(m_solver->isDiffusive()){
-    m_solver->setDiffusionCoefficient(m_faceCenteredDiffusionCoefficient);
+    m_solver->setDiffusionCoefficient(m_diffCo);
   }
   if(m_solver->isMobile()){
     this->setVelocity();
@@ -275,6 +373,9 @@ void AdvectionDiffusionStepper::regrid(const int a_lmin, const int a_oldFinestLe
 }
 
 void AdvectionDiffusionStepper::postRegrid() {
+  CH_TIME("AdvectionDiffusionStepper::postRegrid");
+
+  // Nothing to see here. 
 }
 
 #include <CD_NamespaceFooter.H>
--- Physics/AdvectionDiffusion/CD_AdvectionDiffusionTagger.cpp
+++ Physics/AdvectionDiffusion/CD_AdvectionDiffusionTagger.cpp
@@ -10,6 +10,7 @@
 */
 
 // Chombo includes
+#include <CH_Timer.H>
 #include <ParmParse.H>
 
 // Our includes
@@ -21,6 +22,8 @@ using namespace Physics::AdvectionDiffusion;
 
 AdvectionDiffusionTagger::AdvectionDiffusionTagger(RefCountedPtr<CdrSolver>& a_solver,
 						   RefCountedPtr<AmrMesh>&   a_amr){
+  CH_TIME("AdvectionDiffusionTagger::AdvectionDiffusionTagger");
+  
   m_solver    = a_solver;
   m_amr       = a_amr;
   m_name      = "AdvectionDiffusion";
@@ -30,94 +33,107 @@ AdvectionDiffusionTagger::AdvectionDiffusionTagger(RefCountedPtr<CdrSolver>& a_s
 
 
 AdvectionDiffusionTagger::~AdvectionDiffusionTagger(){
-
+  CH_TIME("AdvectionDiffusionTagger::~AdvectionDiffusionTagger");
 }
 
 void AdvectionDiffusionTagger::regrid(){
-
+  CH_TIME("AdvectionDiffusionTagger::regrid");
 }
 
 void AdvectionDiffusionTagger::parseOptions(){
-  ParmParse pp(m_name.c_str());
-  pp.get("refine_curv", m_refi_curv);
-  pp.get("refine_magn", m_refi_magn);
+  CH_TIME("AdvectionDiffusionTagger::parseOptions");
   
-  parseBuffer(); // Derived from cell
+  ParmParse pp(m_name.c_str());
+  
+  pp.get("refine_curv", m_refCurv);
+  pp.get("refine_magn", m_refMagn);
+  
+  this->parseBuffer(); // Derived from CellTagger -- sets buffer region for cell refinement. 
 }
 
 bool AdvectionDiffusionTagger::tagCells(EBAMRTags& a_tags){
+  CH_TIME("AdvectionDiffusionTagger::tagCells");
+
+  // TLDR: We allocate storage for computing grad(phi) and |grad(phi)|. We then run through the cells on each level and
+  //       refine cells where |grad(phi)|*dx/phi > m_refCurv or where phi > m_refMagn.
+
+  // Data allocation
   EBAMRCellData sca;
   EBAMRCellData vec;
 
   m_amr->allocate(sca, m_realm, phase::gas, 1);
   m_amr->allocate(vec, m_realm, phase::gas, SpaceDim);
 
+  // Get cell-centered state from the solver. 
   const EBAMRCellData& state = m_solver->getPhi();
 
-  // Compute the gradient, vec = grad(phi)
-  m_amr->computeGradient(vec, state, m_realm, phase::gas); // vec = grad(phi)
-  DataOps::vectorLength(sca, vec);                        // sca = |grad(phi)|
-  DataOps::setCoveredValue(sca, 0, 0.0);                 // covered cell values are set to 0.0
+  // Update ghost cells so we can safely compute gradient. 
+  DataOps::copy(sca, state);
+  m_amr->interpGhostMG(sca, m_realm, phase::gas);
 
-  bool found_tags = false;
+  // Compute the gradient
+  m_amr->computeGradient(vec, sca, m_realm, phase::gas); // vec =  grad(phi)
+  DataOps::vectorLength(sca, vec);                       // sca = |grad(phi)|
+  DataOps::setCoveredValue(sca, 0, 0.0);                 // Set covered cell values to zero.
 
-  // Never tag on max_amr_depth
-  const int finest_level     = m_amr->getFinestLevel();
-  const int max_depth        = m_amr->getMaxAmrDepth();
-  const int finest_tag_level = (finest_level == max_depth) ? max_depth - 1 : finest_level; // Never tag on max_amr_depth
+  bool foundTags = false;
 
-  for (int lvl = 0; lvl <= finest_tag_level; lvl++){
+  // Never tag on finest possible AMR level.
+  const int finestLevel    = m_amr->getFinestLevel();
+  const int maxLevel       = m_amr->getMaxAmrDepth();
+  const int finestTagLevel = (finestLevel == maxLevel) ? maxLevel - 1 : finestLevel; 
+
+  for (int lvl = 0; lvl <= finestTagLevel; lvl++){
     DataOps::scale(*sca[lvl], m_amr->getDx()[lvl]); // sca = |grad(phi)|*dx
 
-    const Real SAFETY = 1.E-6;
-    
+    constexpr Real SAFETY = 1.E-6; // To avoid division by zero
+
+    // Get this on this level
     const DisjointBoxLayout& dbl = m_amr->getGrids(m_realm)[lvl];
-    for (DataIterator dit = dbl.dataIterator(); dit.ok(); ++dit){
-      const EBCellFAB& c   = (*sca[lvl])[dit()];
-      const EBCellFAB& phi = (*state[lvl])[dit()];
 
-      const BaseFab<Real>& cReg   = c.getSingleValuedFAB();
-      const BaseFab<Real>& phiReg = phi.getSingleValuedFAB();
+    // Iterate through grids
+    for (DataIterator dit(dbl); dit.ok(); ++dit){
+      const Box        cellBox = dbl[dit()];
+      const EBCellFAB& gradPhi = (*sca  [lvl])[dit()];   // |grad(phi)|
+      const EBCellFAB& phi     = (*state[lvl])[dit()];   //  phi
+      const EBISBox&   ebisBox = phi.getEBISBox();
 
-      const Box box = dbl.get(dit());
-
-      // These are the tags
+      // Clear all previous cell tags. 
       DenseIntVectSet& tags = (*a_tags[lvl])[dit()];
-      tags.makeEmptyBits(); // Clear all previous tags
+      tags.makeEmptyBits(); 
+
+      // Go through regular grid cells. 
+      const BaseFab<Real>& gradReg = gradPhi.getSingleValuedFAB();
+      const BaseFab<Real>& phiReg  = phi.    getSingleValuedFAB();
 
       // Do regular cells
-      for (BoxIterator bit(box); bit.ok(); ++bit){
+      for (BoxIterator bit(cellBox); bit.ok(); ++bit){
 	const IntVect iv = bit();
 
-	const Real crit = Abs(cReg(iv,0))/(SAFETY + Abs(phiReg(iv,0)));
-	if(crit > m_refi_curv && Abs(phiReg(iv,0)) > m_refi_magn){
-	  tags |= iv;
-	  found_tags = true;
+	if(ebisBox.isRegular(iv)){
+
+	  const Real curv = std::abs(gradReg(iv,0))/(SAFETY + std::abs(phiReg(iv,0)));
+	  if(curv > m_refCurv && std::abs(phiReg(iv,0)) > m_refMagn){
+	    tags      |= iv;
+	    foundTags  = true;
+	  }
 	}
-      }
-
-      tags &= box;
-
-      // Do irregular cells
-      const EBISBox& ebisbox = c.getEBISBox();
-      const EBGraph& ebgraph = ebisbox.getEBGraph();
-      for (VoFIterator vofit(ebisbox.getIrregIVS(box), ebgraph); vofit.ok(); ++vofit){
-	const VolIndex& vof = vofit();
       }
     }
   }
 
   // Some ranks may have gotten new tags while others have not. This little code snippet
-  // sets got_new_tags = true for all ranks if any rank originally had got_new_tags = true
+  // makes sure they are all on the same page. 
 #ifdef CH_MPI
-  int glo = 1;
-  int loc = found_tags ? 1 : 0;
+  int globalFoundTags = 0;
+  int localFoundTags  = foundTags ? 1 : 0;
 
-  const int result = MPI_Allreduce(&loc, &glo, 1, MPI_INT, MPI_MAX, Chombo_MPI::comm);
+  const int result = MPI_Allreduce(&localFoundTags, &globalFoundTags, 1, MPI_INT, MPI_MAX, Chombo_MPI::comm);
 
-  found_tags = (glo == 1) ? true : false;
+  foundTags = (globalFoundTags == 1) ? true : false;
 #endif
-  return found_tags;
+  
+  return foundTags;
 }
 
 #include <CD_NamespaceFooter.H>
