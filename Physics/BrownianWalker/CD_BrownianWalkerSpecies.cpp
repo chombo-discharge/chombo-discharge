a860a9a03c20b33b77cd525ae6ee2c9741d3deb3
Robert Marskar <robert.marskar@sintef.no>
2021-12-07 10:22:52 +0100

    Added Random static class for simplifying (and safeguarding) RNG generation. Added initial docs to BrownianWalker physics module

--- Physics/BrownianWalker/CD_BrownianWalkerSpecies.cpp
+++ Physics/BrownianWalker/CD_BrownianWalkerSpecies.cpp
@@ -10,100 +10,95 @@
 */
 
 // Chombo includes
+#include <CH_Timer.H>
 #include <ParmParse.H>
 
 // Our includes
+#include <CD_Random.H>
 #include <CD_BrownianWalkerSpecies.H>
 #include <CD_NamespaceHeader.H>
 
 using namespace Physics::BrownianWalker;
 
-BrownianWalkerSpecies::BrownianWalkerSpecies() : m_rng(), m_gauss(0., 1.) {
-  m_name   = "scalar species";
+BrownianWalkerSpecies::BrownianWalkerSpecies() {
+  CH_TIME("BrownianWalkerSpecies::BrownianWalkerSpecies");
+  
+  m_name         = "scalar species";
   m_chargeNumber = 0;
 
+  // Parse input options.
   ParmParse pp("BrownianWalker");
   Vector<Real> v;
 
-  pp.get   ("seed",           m_seed);
-  pp.get   ("diffusion",      m_isDiffusive);
-  pp.get   ("advection",      m_isMobile);
-  pp.get   ("blob_amplitude", m_blob_amplitude);
-  pp.get   ("blob_radius",    m_blob_radius);
-  pp.getarr("blob_center",    v, 0, SpaceDim); m_blob_center = RealVect(D_DECL(v[0], v[1], v[2]));
-  pp.get   ("num_particles",  m_num_particles);
+  int seed;
 
+  pp.get   ("seed",           seed          );
+  pp.get   ("diffusion",      m_isDiffusive );
+  pp.get   ("advection",      m_isMobile    );
+  pp.get   ("num_particles",  m_numParticles);  
+  pp.get   ("blob_radius",    m_blobRadius  );
+  
+  pp.getarr("blob_center",    v, 0, SpaceDim); m_blobCenter = RealVect(D_DECL(v[0], v[1], v[2]));
 
-  draw_initial_particles();
+  // Set up the RNG with the input seed.
+  if(seed < 0){
+    Random::init();
+  }
+  else {
+    Random::setSeed(seed);
+  }
+
+  // Draw initial particles
+  this->drawInitParticles();
 }
 
 BrownianWalkerSpecies::~BrownianWalkerSpecies(){
-
+  CH_TIME("BrownianWalkerSpecies::~BrownianWalkerSpecies");
 }
 
-void BrownianWalkerSpecies::draw_initial_particles(){
+void BrownianWalkerSpecies::drawInitParticles(){
+  CH_TIME("BrownianWalkerSpecies::drawInitParticles");
 
-  // To avoid that MPI ranks draw the same particle positions, increment the seed for each rank
-  //  m_seed += procID();
+  // TLDR: We draw a bunch of particles from a Gaussian distribution center on m_blobCenter. If we use MPI then
+  //       we should ensure that the ranks collectively draw the specified number of starting particles. 
 
-  // Set up the RNG
-  m_rng = std::mt19937_64(m_seed);
-  m_gauss = std::normal_distribution<Real>(0.0, m_blob_radius);
-  m_udist11 = std::uniform_real_distribution<Real>(-1., 1.);
+  // Nifty little lambda for drawing particle positions from a Gaussian distribution
+  std::normal_distribution<Real> gaussianDistribution(0.0, m_blobRadius);
+  auto randomGaussian = [center=this->m_blobCenter, &gaussianDistribution]() -> RealVect {
+    const Real     len = Random::get(gaussianDistribution);
+    const RealVect dir = Random::getDirection();
 
-  // Each MPI process draws the desired number of particles from a distribution
-  const int quotient  = m_num_particles/numProc();
-  const int remainder = m_num_particles % numProc();
+    return center + len*dir;
+  };
+
+  // Draw initial particles. This is a little bit different in MPI-vs-serial code because if we run with MPI then the ranks should collectively initialize m_numParticles.
+  m_initialParticles.clear();  
+#if CH_MPI
+  // Compute the number of particles per MPI rank. 
+  const int quotient  = m_numParticles/numProc();
+  const int remainder = m_numParticles % numProc();
   
   Vector<int> particlesPerRank(numProc(), quotient);
-  
+
+  // Add in the remainder. 
   for (int i = 0; i < remainder; i++){ 
     particlesPerRank[i] += 1;
   }
 
   // Now make the particles
-  m_initialParticles.clear();
   for (int i = 0; i < particlesPerRank[procID()]; i++){
-    const Real weight  = 1.0;
-    const RealVect pos = m_blob_center + randomGaussian();
-    m_initialParticles.add(ItoParticle(weight, pos));
+    const Real     mass = 1.0;
+    const RealVect pos  = randomGaussian();
+    
+    m_initialParticles.add(ItoParticle(mass, pos));
   }
-}
+#else
+  for (int i = 0; i < m_numParticles; i++){
+    const Real     mass = 1.0;
+    const RealVect pos  = randomGaussian();    
 
-RealVect BrownianWalkerSpecies::randomGaussian(){
-
-  const Real rad = m_gauss(m_rng);
-  return rad*randomDirection();
-}
-
-RealVect BrownianWalkerSpecies::randomDirection(){
-  const Real EPS = 1.E-8;
-#if CH_SPACEDIM==2
-  Real x1 = 2.0;
-  Real x2 = 2.0;
-  Real r  = x1*x1 + x2*x2;
-  while(r >= 1.0 || r < EPS){
-    x1 = m_udist11(m_rng);
-    x2 = m_udist11(m_rng);
-    r  = x1*x1 + x2*x2;
-  }
-
-  return RealVect(x1,x2)/sqrt(r);
-#elif CH_SPACEDIM==3
-  Real x1 = 2.0;
-  Real x2 = 2.0;
-  Real r  = x1*x1 + x2*x2;
-  while(r >= 1.0 || r < EPS){
-    x1 = m_udist11(m_rng);
-    x2 = m_udist11(m_rng);
-    r  = x1*x1 + x2*x2;
-  }
-
-  const Real x = 2*x1*sqrt(1-r);
-  const Real y = 2*x2*sqrt(1-r);
-  const Real z = 1 - 2*r;
-
-  return RealVect(x,y,z);
+    m_initialParticles.add(ItoParticle(mass, pos));
+  }    
 #endif
 }
 
