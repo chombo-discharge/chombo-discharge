baef293f08b01613a12b9501e1dd34eedb32d643
Robert Marskar <robert.marskar@sintef.no>
2021-12-13 09:47:59 +0100

    Minor commit - ItoPlasma won't run right now because of all the changes

--- Physics/ItoPlasma/PlasmaModels/ItoPlasmaAir3LFA/CD_ItoPlasmaAir3LFA.cpp
+++ Physics/ItoPlasma/PlasmaModels/ItoPlasmaAir3LFA/CD_ItoPlasmaAir3LFA.cpp
@@ -305,7 +305,7 @@ ItoPlasmaAir3LFA::PhotonZ::~PhotonZ(){
 
 }
 
-Real ItoPlasmaAir3LFA::PhotonZ::getKappa(const RealVect a_pos) const {
+Real ItoPlasmaAir3LFA::PhotonZ::getAbsorptionCoefficient(const RealVect a_pos) const {
   const Real f = m_f1 + (*m_udist01)(*m_rng)*(m_f2 - m_f1);
   return m_K1*pow(m_K2/m_K1, (f-m_f1)/(m_f2-m_f1));
 }
