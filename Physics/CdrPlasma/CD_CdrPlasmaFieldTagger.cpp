590ecee438614eca04c743d2d65b4fba8bb922aa
Robert Marskar <robert.marskar@sintef.no>
2022-02-01 15:13:46 +0100

    Done with CdrPlasmaFieldTagger

--- Physics/CdrPlasma/CD_CdrPlasmaFieldTagger.cpp
+++ Physics/CdrPlasma/CD_CdrPlasmaFieldTagger.cpp
@@ -33,154 +33,181 @@ CdrPlasmaFieldTagger::~CdrPlasmaFieldTagger(){
 }
 
 void CdrPlasmaFieldTagger::allocateStorage() const {
-  CH_TIME("CdrPlasmaFieldTagger::allocateStorage");
+  CH_TIME("CdrPlasmaFieldTagger::allocateStorage()");
   if(m_verbosity > 5){
-    pout() << m_name + "::allocateStorage" << endl;
+    pout() << m_name + "::allocateStorage()" << endl;
   }
 
-  m_amr->allocate(m_scratch,  m_realm, m_phase, 1);
-  m_amr->allocate(m_E,        m_realm, m_phase, SpaceDim);
-  m_amr->allocate(m_grad_E,   m_realm, m_phase, SpaceDim);
+  m_amr->allocate(m_scratch,           m_realm, m_phase, 1       );
+  m_amr->allocate(m_electricField,     m_realm, m_phase, SpaceDim);
+  m_amr->allocate(m_gradElectricField, m_realm, m_phase, SpaceDim);
 }
 
 void CdrPlasmaFieldTagger::deallocateStorage() const {
-  CH_TIME("CdrPlasmaFieldTagger::deallocateStorage");
+  CH_TIME("CdrPlasmaFieldTagger::deallocateStorage()");
   if(m_verbosity > 5){
-    pout() << m_name + "::deallocateStorage" << endl;
+    pout() << m_name + "::deallocateStorage()" << endl;
   }
 
-  m_amr->deallocate(m_scratch);
-  m_amr->deallocate(m_E);
-  m_amr->deallocate(m_grad_E);
+  m_amr->deallocate(m_scratch          );
+  m_amr->deallocate(m_electricField    );
+  m_amr->deallocate(m_gradElectricField);
 }
 
-void CdrPlasmaFieldTagger::computeElectricField(EBAMRCellData& a_E, EBAMRCellData& a_grad_E) const {
+void CdrPlasmaFieldTagger::computeElectricField(EBAMRCellData& a_electricField, EBAMRCellData& a_gradElectricField) const {
   CH_TIME("CdrPlasmaFieldTagger::computeElectricField");
   if(m_verbosity > 5){
     pout() << m_name + "::computeElectricField" << endl;
   }
 
-  m_timeStepper->computeElectricField(a_E, m_phase);
-  DataOps::vectorLength(m_scratch, a_E);
-  m_amr->computeGradient(a_grad_E, m_scratch, m_realm, phase::gas);
+  // Time stepper computes the electric field for us. 
+  m_timeStepper->computeElectricField(a_electricField, m_phase);
 
-  m_amr->averageDown(a_grad_E, m_realm, m_phase);
-  m_amr->interpGhost(a_grad_E, m_realm, m_phase);
+  // Compute |E| onto scratch
+  DataOps::vectorLength(m_scratch, a_electricField);
+
+  // Now compute grad(|E|).
+  m_amr->computeGradient(a_gradElectricField, m_scratch, m_realm, phase::gas);
+
+  m_amr->averageDown(a_gradElectricField, m_realm, m_phase);
+  m_amr->interpGhost(a_gradElectricField, m_realm, m_phase);
   
-  // Interpolate to centroids
-  m_amr->interpToCentroids(a_E,      m_realm, m_phase);
-  m_amr->interpToCentroids(a_grad_E, m_realm, m_phase);
+  // Interpolate everything to centroids since that is the only place where things make sense. 
+  m_amr->interpToCentroids(a_electricField,     m_realm, m_phase);
+  m_amr->interpToCentroids(a_gradElectricField, m_realm, m_phase);
 }
 
 void CdrPlasmaFieldTagger::computeTracers() const {
-  CH_TIME("CdrPlasmaFieldTagger::computeTracers");
+  CH_TIME("CdrPlasmaFieldTagger::computeTracers()");
   if(m_verbosity > 5){
-    pout() << m_name + "::computeTracers" << endl;
+    pout() << m_name + "::computeTracers()" << endl;
   }
 
+  constexpr int comp = 0;
+
+  // Allocate necessary storage. 
   this->allocateStorage();
-  
-  const RealVect origin = m_amr->getProbLo();
-  const Real time       = m_timeStepper->getTime();
+
+  // Get the lower-left domain coordinate and current time. 
+  const RealVect probLo = m_amr->getProbLo();
+  const Real     time   = m_timeStepper->getTime();
 
   // Compute electric field on volumetric centroids
-  this->computeElectricField(m_E, m_grad_E);
+  this->computeElectricField(m_electricField, m_gradElectricField);
 
   // Get maximum and minimum of everything
-  Real E_max, E_min;
-  Real grad_E_max, grad_E_min;
+  Real maxElectricField     = -std::numeric_limits<Real>::max();
+  Real minElectricField     =  std::numeric_limits<Real>::max();
+  Real maxGradElectricField = -std::numeric_limits<Real>::max();
+  Real minGradElectricField =  std::numeric_limits<Real>::max();
 
-  DataOps::getMaxMinNorm(E_max,        E_min,        m_E);
-  DataOps::getMaxMinNorm(grad_E_max,   grad_E_min,   m_grad_E);
+  // Get the maximum and minium value of the electric field and its gradient. This is the
+  // norm so we are getting the max of |E| and the max of |grad(|E|)|.
+  DataOps::getMaxMinNorm(maxElectricField,     minElectricField,     m_electricField);
+  DataOps::getMaxMinNorm(maxGradElectricField, minGradElectricField, m_gradElectricField);
 
+  // Go through each AMR level and compute the tracer fields. 
   for (int lvl = 0; lvl <= m_amr->getFinestLevel(); lvl++){
-    const DisjointBoxLayout& dbl = m_amr->getGrids(m_realm)[lvl];
-    const EBISLayout& ebisl      = m_amr->getEBISLayout(m_realm, m_phase)[lvl];
-    const Real dx                = m_amr->getDx()[lvl];
+    const DisjointBoxLayout& dbl   = m_amr->getGrids(m_realm)[lvl];
+    const EBISLayout&        ebisl = m_amr->getEBISLayout(m_realm, m_phase)[lvl];
+    const Real               dx    = m_amr->getDx()[lvl];
 
-    for (DataIterator dit = dbl.dataIterator(); dit.ok(); ++dit){
-      const Box& box         = dbl.get(dit());
+    // Iterate through the grid patches. 
+    for (DataIterator dit(dbl); dit.ok(); ++dit){
+      const Box&     box     = dbl.get(dit());
       const EBISBox& ebisbox = ebisl[dit()];
       const EBGraph& ebgraph = ebisbox.getEBGraph();
-      const IntVectSet ivs(box);
 
-      const EBCellFAB& E_fab    = (*m_E[lvl])[dit()];
-      const EBCellFAB& gE_fab   = (*m_grad_E[lvl])[dit()];
+      // Expose the AMR data per patch, both full data and the single-valued data. 
+      const EBCellFAB& electricField     = (*m_electricField    [lvl])[dit()];
+      const EBCellFAB& gradElectricField = (*m_gradElectricField[lvl])[dit()];
 
-      const BaseFab<Real>& E_reg  = E_fab.getSingleValuedFAB();
-      const BaseFab<Real>& gE_reg = gE_fab.getSingleValuedFAB();
+      const FArrayBox& electricFieldReg     = electricField.    getFArrayBox();
+      const FArrayBox& gradElectricFieldReg = gradElectricField.getFArrayBox();
 
-      // Avoid the extra point lookups by getting these before the point loops
-      Vector<EBCellFAB*> tr;
-      Vector<BaseFab<Real>* > tr_fab;
+      // Get references to tracer fields, both the full data and the single-valued data. These are needed
+      // in the regular and irregular kernels. 
+      Vector<EBCellFAB*> tr   ;
+      Vector<FArrayBox*> trReg;
+      
       for (int i = 0; i < m_numTracers; i++){
-	tr.push_back(&((*m_tracers[i][lvl])[dit()]));
-	tr_fab.push_back(&(tr[i]->getSingleValuedFAB()));
+	tr.   push_back(&((*m_tracers[i][lvl])[dit()]));
+	trReg.push_back(&(tr[i]->getFArrayBox()));
       }
 
-      // Regular box loop
-      for (BoxIterator bit(box); bit.ok(); ++bit){
-	const IntVect iv   = bit();
-	const RealVect pos = origin + RealVect(iv)*dx;
+      // Regular kernel
+      auto regularKernel = [&] (const IntVect& iv) -> void {
+	const RealVect pos = probLo + (0.5*RealVect::Unit + RealVect(iv)) * dx;
 
-	const RealVect E        = RealVect(D_DECL(E_reg(iv, 0),  E_reg(iv, 1),  E_reg(iv, 2)));
-	const RealVect grad_E   = RealVect(D_DECL(gE_reg(iv, 0), gE_reg(iv, 1), gE_reg(iv, 2)));
+	// Reconstruct the electric field and gradient of the electric field. 
+	const RealVect E     = RealVect(D_DECL(electricFieldReg    (iv, 0), electricFieldReg    (iv, 1), electricFieldReg    (iv, 2)));
+	const RealVect gradE = RealVect(D_DECL(gradElectricFieldReg(iv, 0), gradElectricFieldReg(iv, 1), gradElectricFieldReg(iv, 2)));
 
-	Vector<Real> tracers = this->tracer(pos,
-					    time,
-					    dx,
-					    E,
-					    E_min,
-					    E_max,
-					    grad_E,
-					    grad_E_min,
-					    grad_E_max);
-	
+	// Call the per-point tracer function. This is a pure function. 
+	const Vector<Real> tracers = this->tracer(pos,
+						  time,
+						  dx,
+						  E,
+						  minElectricField,
+						  maxElectricField,
+						  gradE,
+						  minGradElectricField,
+						  maxGradElectricField);
+
+	// Put the tracer field where it belongs. 
 	for(int i = 0; i < m_numTracers; i++){
-	  (*tr_fab[i])(iv, 0) = tracers[i];
+	  (*trReg[i])(iv, comp) = tracers[i];
 	}
-      }
+      };
 
       // Irregular box loop
-      const IntVectSet& irreg = ebisbox.getIrregIVS(box);
-      for (VoFIterator vofit(irreg, ebgraph); vofit.ok(); ++vofit){
-	const VolIndex& vof = vofit();
-	const RealVect pos  = EBArith::getVofLocation(vof, dx*RealVect::Unit, origin);
+      auto irregularKernel = [&] (const VolIndex& vof) -> void {
+	const RealVect pos  = probLo + Location::position(Location::Cell::Center, vof, ebisbox, dx);
 
-	// Electric field and grad(|E|)
-	const RealVect E        = RealVect(D_DECL(E_fab(vof, 0),  E_fab(vof, 1), E_fab(vof, 2)));
-	const RealVect grad_E   = RealVect(D_DECL(gE_fab(vof, 0), gE_fab(vof, 1), gE_fab(vof, 2)));
+	// Reconstruct the electric field and gradient of the electric field. 
+	const RealVect E      = RealVect(D_DECL(electricField    (vof, 0), electricField    (vof, 1), electricField    (vof, 2)));
+	const RealVect gradE  = RealVect(D_DECL(gradElectricField(vof, 0), gradElectricField(vof, 1), gradElectricField(vof, 2)));
 
-	Vector<Real> tracers = this->tracer(pos,
-					    time,
-					    dx,
-					    E,
-					    E_min,
-					    E_max,
-					    grad_E,
-					    grad_E_min,
-					    grad_E_max);
-	
+	// Call the per-point tracer function. Again, it's a pure function. 
+	const Vector<Real> tracers = this->tracer(pos,
+						  time,
+						  dx,
+						  E,
+						  minElectricField,
+						  maxElectricField,
+						  gradE,
+						  minGradElectricField,
+						  maxGradElectricField);
+
+	// Put the tracer field where it belongs. 
 	for(int i = 0; i < m_numTracers; i++){
-	  (*tr[i])(vof, 0);
+	  (*tr[i])(vof, comp);
 	}
-      }
+      };
+
+      // Irregular kernel region
+      VoFIterator vofit = (*m_amr->getVofIterator(m_realm, m_phase)[lvl])[dit()];        
+
+      // Execute the kernels
+      BoxLoops::loop(box  ,   regularKernel);
+      BoxLoops::loop(vofit, irregularKernel);      
     }
   }
 
-
+  // Coarsen the data. 
   for (int i = 0; i < m_numTracers; i++){
     m_amr->averageDown(m_tracers[i], m_realm, m_phase);
     m_amr->interpGhost(m_tracers[i], m_realm, m_phase);
   }
 
-  // Compute gradient of tracers
+  // Compute gradients. 
   for (int i = 0; i < m_numTracers; i++){
     m_amr->computeGradient(m_gradTracers[i], m_tracers[i], m_realm, phase::gas);
     m_amr->averageDown(m_gradTracers[i], m_realm, m_phase);
   }
 
-  this->deallocateStorage(); // No reason to keep the extra storage lying around...
+  // No need to keep this transient storage lying around so we delete it. 
+  this->deallocateStorage(); 
 }
 
 #include <CD_NamespaceFooter.H>
