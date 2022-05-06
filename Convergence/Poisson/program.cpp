7300ddd88ce756823d3aef91208e1a7b42c80b03
Robert Marskar <robert.marskar@sintef.no>
2022-05-06 15:18:12 +0200

    Update convergence test

--- Convergence/Poisson/program.cpp
+++ Convergence/Poisson/program.cpp
@@ -8,43 +8,52 @@
 using namespace ChomboDischarge;
 using namespace Physics::Electrostatics;
 
-// Parameters from input script
-constexpr Real R0   = 1.0;
-constexpr Real R1   = 2.0;
-constexpr Real R2   = 3.0;
-constexpr Real eps  = 10.0;
-constexpr Real phi0 = 1.0;
-constexpr Real a    = phi0/( log(R1/R2) - log(R1/R0)/eps );
-
-
-// Exact solution for "coaxial cable" with relative permittivity eps and potential phi0 on the inner conductor.
-Real exactSolution(const RealVect& x) {
-  const Real r = sqrt(x[0]*x[0] + x[1]*x[1]);
-
-  Real phi = 0.0;
-    
-  if(r >= R0 && r <= R1) {
-    phi = phi0 + a/eps*log(r/R0);
-  }
-  else if(r >= R1 && r <= R2) {
-    phi = a * log(r/R2);
-  }
-
-  return phi;
-}
-
 int main(int argc, char* argv[]){
 
 #ifdef CH_MPI
   MPI_Init(&argc, &argv);
 #endif
 
-  std::vector<IntVect> nCells{64*IntVect::Unit, 128*IntVect::Unit, 256*IntVect::Unit, 512*IntVect::Unit};
+  int exitCode = 0;
+
+  std::vector<IntVect> nCells{32*IntVect::Unit, 64*IntVect::Unit, 128*IntVect::Unit, 256*IntVect::Unit, 512*IntVect::Unit};
   std::vector<std::array<Real, 3> > norms;
 
   // Build class options from input script and command line options
   const std::string input_file = argv[1];
   ParmParse pp(argc-2, argv+2, NULL, input_file.c_str());
+
+  // Get R0, R1 etc from the input script
+  ParmParse ppOuter("CoaxialCable.outer");
+  ParmParse ppInner("CoaxialCable.inner");
+  ParmParse ppDiele("CoaxialCable.dielectric");
+
+  // Parameters from input script
+  Real R0, R1, R2, eps, a;
+  constexpr Real phi0 = 1.0;
+
+  ppInner.get("radius", R0);
+  ppOuter.get("radius", R2);
+  ppDiele.get("radius", R1);
+  ppDiele.get("eps", eps);
+  a = phi0/( log(R1/R2) - log(R1/R0)/eps );
+
+  // Set up the exact solution.
+  auto exactSolution = [R0, R1, R2, eps, phi0, a](const RealVect& x) -> Real {
+    const Real r = sqrt(x[0]*x[0] + x[1]*x[1]);
+
+    Real phi = 0.0;
+    
+    if(r >= R0 && r <= R1) {
+      phi = phi0 + a/eps*log(r/R0);
+    }
+    else if(r >= R1 && r <= R2) {
+      phi = a * log(r/R2);
+    }
+
+    return phi;
+  };
+  
 
   // Set geometry and AMR 
   RefCountedPtr<ComputationalGeometry> compgeom = RefCountedPtr<ComputationalGeometry> (new CoaxialCable());
@@ -87,19 +96,40 @@ int main(int argc, char* argv[]){
   if(procID() == 0) {
 
     std::cout << "Convergence rates: " << std::endl;
-    std::cout << "Linf" << "\t" << "L1" << "\t" << "L2" << std::endl;    
+    std::cout << "Norm" << "\t" << "Coar err" << "\t" << "Fine err" << "\t" << "Conv. rate" << std::endl;
     for (size_t i = 1; i < norms.size(); i++) {
 
-      const Real Pinf = -log(std::get<0>(norms[i])/std::get<0>(norms[i-1]))/log(2);
-      const Real P1   = -log(std::get<1>(norms[i])/std::get<2>(norms[i-1]))/log(2);
-      const Real P2   = -log(std::get<1>(norms[i])/std::get<2>(norms[i-1]))/log(2);            
+      const Real coarInf = std::get<0>(norms[i-1]);
+      const Real fineInf = std::get<0>(norms[i]);
 
-      std::cout << Pinf << "\t" << P1 << "\t" << P2 << std::endl;
+      const Real coar1 = std::get<1>(norms[i-1]);
+      const Real fine1 = std::get<1>(norms[i]);
+
+      const Real coar2 = std::get<2>(norms[i-1]);
+      const Real fine2 = std::get<2>(norms[i]);            
+      
+      const Real Pinf = log(coarInf/fineInf)/log(2);
+      const Real P1   = log(coar1/fine1)/log(2);
+      const Real P2   = log(coar2/fine2)/log(2);
+
+      const IntVect cellsCoar = nCells[i-1];
+      const IntVect cellsFine = nCells[i];      
+
+      std::cout << "Inf" << "\t" << coarInf << "\t" << fineInf << "\t" << Pinf << std::endl;
+      std::cout << "1  " << "\t" << coar1 << "\t" << fine1 << "\t" << P1 << std::endl;
+      std::cout << "2  " << "\t" << coar2 << "\t" << fine2 << "\t" << P2 << std::endl;
+      std::cout << "\n";
+
+      const Real accept = 1.9;
+      if(std::min(P1, P2) < accept) {
+	exitCode = 1;
+      }
     }
   }
   
 #ifdef CH_MPI
-  CH_TIMER_REPORT();
   MPI_Finalize();
 #endif
+
+  return exitCode;
 }
