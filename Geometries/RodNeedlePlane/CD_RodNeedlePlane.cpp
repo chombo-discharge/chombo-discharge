4f1354c84208564d114da001af3dc6412410a52d
Fanny Skirbekk <fanny.skirbekk@ntnu.no>
2022-07-01 15:06:11 +0200

    rounded needle tip + clang-format

--- Geometries/RodNeedlePlane/CD_RodNeedlePlane.cpp
+++ Geometries/RodNeedlePlane/CD_RodNeedlePlane.cpp
@@ -22,7 +22,8 @@
 #include <CD_RoundedCylinderIF.H>
 #include <CD_NamespaceHeader.H>
 
-RodNeedlePlane::RodNeedlePlane(){
+RodNeedlePlane::RodNeedlePlane()
+{
   this->setGasPermittivity(1.0);
 
   ParmParse ppRod("RodNeedlePlane.rod");
@@ -37,27 +38,32 @@ RodNeedlePlane::RodNeedlePlane(){
   ppNeedle.get("on", useNeedle);
   ppPlane.get("on", usePlane);
 
-  if(useRod && useNeedle) this->defineRodWNeedle();
-  else if(useRod)         this->defineRod();
-  else if(useNeedle)      this->defineNeedle();
-  if(usePlane)            this->definePlane();
+  // Choose electrodes
+  if (useRod && useNeedle)
+    this->defineRodWNeedle();
+  else if (useRod)
+    this->defineRod();
+  else if (useNeedle)
+    this->defineNeedle();
+  if (usePlane)
+    this->definePlane();
 }
 
-RodNeedlePlane::~RodNeedlePlane(){
+RodNeedlePlane::~RodNeedlePlane() {}
 
-}
-
-void RodNeedlePlane::defineRodWNeedle(){
+void
+RodNeedlePlane::defineRodWNeedle()
+{
   Vector<Real> v(SpaceDim);
-  Real rodRadius;
-  RealVect rodE1, rodE2;
-  bool rodLive;
+  Real         rodRadius;
+  RealVect     rodE1, rodE2;
+  bool         rodLive;
 
-  Real needleRadius;
-  Real tipRadius;
-  Real angle;
+  Real     needleRadius;
+  Real     tipRadius;
+  Real     angle;
   RealVect needleE1, needleE2;
-  bool needleLive;
+  bool     needleLive;
 
   Real cornerCurve;
 
@@ -74,52 +80,61 @@ void RodNeedlePlane::defineRodWNeedle(){
 
   ppNeedle.get("cornerCurve", cornerCurve);
 
-  ppRod.getarr("endpoint1", v, 0, SpaceDim); rodE1 = RealVect(D_DECL(v[0], v[1], v[2]));
-  ppRod.getarr("endpoint2", v, 0, SpaceDim); rodE2 = RealVect(D_DECL(v[0], v[1], v[2]));
+  ppRod.getarr("endpoint1", v, 0, SpaceDim);
+  rodE1 = RealVect(D_DECL(v[0], v[1], v[2]));
+  ppRod.getarr("endpoint2", v, 0, SpaceDim);
+  rodE2 = RealVect(D_DECL(v[0], v[1], v[2]));
 
-  ppNeedle.getarr("endpointtip", v, 0, SpaceDim); needleE1 = RealVect(D_DECL(v[0], v[1], v[2]));
-  ppNeedle.getarr("endpointback", v, 0, SpaceDim); needleE2 = RealVect(D_DECL(v[0], v[1], v[2]));
+  ppNeedle.getarr("endpointtip", v, 0, SpaceDim);
+  needleE1 = RealVect(D_DECL(v[0], v[1], v[2]));
+  ppNeedle.getarr("endpointback", v, 0, SpaceDim);
+  needleE2 = RealVect(D_DECL(v[0], v[1], v[2]));
 
   Vector<BaseIF*> electrodeParts;
-  electrodeParts.push_back((BaseIF*) new RodIF(rodE1, rodE2, rodRadius, false));
-  electrodeParts.push_back((BaseIF*) new NeedleIF(needleE1, needleE2, needleRadius, false, tipRadius, angle));
+  electrodeParts.push_back((BaseIF*)new RodIF(rodE1, rodE2, rodRadius, false));
+  electrodeParts.push_back((BaseIF*)new NeedleIF(needleE1, needleE2, needleRadius, false, tipRadius, angle));
 
-  //RefCountedPtr<BaseIF> bif = RefCountedPtr<BaseIF>(new SmoothUnion(electrodeParts, cornerCurve));
   RefCountedPtr<BaseIF> bif = RefCountedPtr<BaseIF>(new SmoothIntersection(electrodeParts, cornerCurve));
 
-  m_electrodes.push_back(Electrode(bif, (rodLive&&needleLive)));
+  m_electrodes.push_back(Electrode(bif, (rodLive && needleLive)));
 
-  for(int i = 0; i < electrodeParts.size(); ++i){
+  for (int i = 0; i < electrodeParts.size(); ++i) {
     delete electrodeParts[i];
   }
 }
 
-void RodNeedlePlane::defineRod(){
+void
+RodNeedlePlane::defineRod()
+{
   Vector<Real> v(SpaceDim);
-  Real radius;
-  RealVect e1, e2;
-  bool live;
+  Real         radius;
+  RealVect     e1, e2;
+  bool         live;
 
   ParmParse pp("RodNeedlePlane.rod");
 
   pp.get("radius", radius);
   pp.get("live", live);
 
-  pp.getarr("endpoint1", v, 0, SpaceDim); e1 = RealVect(D_DECL(v[0], v[1], v[2]));
-  pp.getarr("endpoint2", v, 0, SpaceDim); e2 = RealVect(D_DECL(v[0], v[1], v[2]));
+  pp.getarr("endpoint1", v, 0, SpaceDim);
+  e1 = RealVect(D_DECL(v[0], v[1], v[2]));
+  pp.getarr("endpoint2", v, 0, SpaceDim);
+  e2 = RealVect(D_DECL(v[0], v[1], v[2]));
 
-  RefCountedPtr<BaseIF> bif = RefCountedPtr<BaseIF> (new RodIF(e1, e2, radius, false));
+  RefCountedPtr<BaseIF> bif = RefCountedPtr<BaseIF>(new RodIF(e1, e2, radius, false));
 
   m_electrodes.push_back(Electrode(bif, live));
 }
 
-void RodNeedlePlane::defineNeedle(){
+void
+RodNeedlePlane::defineNeedle()
+{
   Vector<Real> v(SpaceDim);
-  Real radius;
-  Real tipRadius;
-  Real angle;
-  RealVect e1, e2;
-  bool live;
+  Real         radius;
+  Real         tipRadius;
+  Real         angle;
+  RealVect     e1, e2;
+  bool         live;
 
   ParmParse pp("RodNeedlePlane.needle");
 
@@ -128,21 +143,25 @@ void RodNeedlePlane::defineNeedle(){
   pp.get("angle", angle);
   pp.get("live", live);
 
-  pp.getarr("endpointtip", v, 0, SpaceDim); e1 = RealVect(D_DECL(v[0], v[1], v[2]));
-  pp.getarr("endpointback", v, 0, SpaceDim); e2 = RealVect(D_DECL(v[0], v[1], v[2]));
+  pp.getarr("endpointtip", v, 0, SpaceDim);
+  e1 = RealVect(D_DECL(v[0], v[1], v[2]));
+  pp.getarr("endpointback", v, 0, SpaceDim);
+  e2 = RealVect(D_DECL(v[0], v[1], v[2]));
 
-  RefCountedPtr<BaseIF> bif = RefCountedPtr<BaseIF> (new NeedleIF(e1, e2, radius, false, tipRadius, angle));
+  RefCountedPtr<BaseIF> bif = RefCountedPtr<BaseIF>(new NeedleIF(e1, e2, radius, false, tipRadius, angle));
 
   m_electrodes.push_back(Electrode(bif, live));
 }
 
-void RodNeedlePlane::definePlane(){
+void
+RodNeedlePlane::definePlane()
+{
   Vector<Real> v(SpaceDim);
-  Real radius;
-  Real thickness;
-  Real curve;
-  RealVect normal, point;
-  bool live;
+  Real         radius;
+  Real         thickness;
+  Real         curve;
+  RealVect     normal, point;
+  bool         live;
 
   ParmParse pp("RodNeedlePlane.plane");
 
@@ -150,10 +169,13 @@ void RodNeedlePlane::definePlane(){
   pp.get("thickness", thickness);
   pp.get("curve", curve);
   pp.get("live", live);
-  pp.getarr("normal", v, 0, SpaceDim); normal = RealVect(D_DECL(v[0], v[1], v[2]));
-  pp.getarr("point", v, 0, SpaceDim); point = RealVect(D_DECL(v[0], v[1], v[2]));
+  pp.getarr("normal", v, 0, SpaceDim);
+  normal = RealVect(D_DECL(v[0], v[1], v[2]));
+  pp.getarr("point", v, 0, SpaceDim);
+  point = RealVect(D_DECL(v[0], v[1], v[2]));
 
-  RefCountedPtr<BaseIF> bif = RefCountedPtr<BaseIF> (new RoundedCylinderIF(point, point - thickness*normal, radius, curve, false));
+  RefCountedPtr<BaseIF> bif =
+    RefCountedPtr<BaseIF>(new RoundedCylinderIF(point, point - thickness * normal, radius, curve, false));
 
   m_electrodes.push_back(Electrode(bif, live));
 }
--- Source/ImplicitFunctions/CD_NeedleIF.cpp
+++ Source/ImplicitFunctions/CD_NeedleIF.cpp
@@ -10,7 +10,7 @@
   @author Fanny Skirbekk
 */
 
-// CHombo includes
+// Chombo includes
 #include <UnionIF.H>
 #include <SmoothIntersection.H>
 
@@ -22,59 +22,68 @@
 
 using Vec3 = EBGeometry::Vec3T<Real>;
 
-NeedleIF::NeedleIF(const RealVect& a_centerTipSide, const RealVect& a_centerBack, const Real& a_radius, const bool& a_fluidInside, const Real& a_tipRadius, const double& a_angle){
-  m_tipRadius = a_tipRadius; 
-  
-  constexpr Real pi = 3.14159265358979323846;
-  const double tipLength = a_radius/std::tan(a_angle*pi/180);
+NeedleIF::NeedleIF(const RealVect& a_centerTipSide,
+                   const RealVect& a_centerBack,
+                   const Real&     a_radius,
+                   const bool&     a_fluidInside,
+                   const Real&     a_tipRadius,
+                   const double&   a_angle)
+{
+  m_tipRadius = a_tipRadius;
+
+  constexpr Real pi        = 3.14159265358979323846;
+  const double   tipLength = (a_radius - m_tipRadius) / std::tan(a_angle * pi / 180);
 
   const RealVect axis    = (a_centerTipSide - a_centerBack);
-  const RealVect axisVec = axis/axis.vectorLength(); 
+  const RealVect axisVec = axis / axis.vectorLength();
 
   // find new center for where the cylinder and cone should meet.
-  const RealVect c = a_centerTipSide - tipLength*axisVec;
+  const RealVect c = a_centerTipSide - tipLength * axisVec;
 
-#if CH_SPACEDIM==2
-    const Vec3 centerT(a_centerTipSide[0], a_centerTipSide[1], 0.0);
+#if CH_SPACEDIM == 2
+  const Vec3 centerT(a_centerTipSide[0], a_centerTipSide[1], 0.0);
 #else
-    const Vec3 centerT(a_centerTipSide[0], a_centerTipSide[1], a_centerTipSide[2]);
+  const Vec3 centerT(a_centerTipSide[0], a_centerTipSide[1], a_centerTipSide[2]);
 #endif
 
-  // Build the needle-parts
+  // Build the needle-parts:
   Vector<BaseIF*> isects;
-  isects.push_back(static_cast<BaseIF*> (new CylinderSdf(c, a_centerBack, a_radius/2, a_fluidInside)));
+  isects.push_back(static_cast<BaseIF*>(new CylinderSdf(c, a_centerBack, (a_radius - m_tipRadius) / 2, a_fluidInside)));
+
   //flipinside=true for cone since EBGeometry and Chombo has opposing sign conventions/logic regarding the flipinside..
-  auto cone = std::make_shared<EBGeometry::ConeSDF<Real>>(centerT, tipLength, a_angle,true);
-  //for debugging..:
-  /*
-  std::cout << "axisVec:" << axisVec[0] << "," << axisVec[1] << "," << axisVec[2] << std::endl;
-  std::cout << "centerT:" << centerT[0] << "," << centerT[1] << "," << centerT[2] << std::endl;
-  std::cout << "c:" << c[0] << "," << c[1] << "," << c[2] << std::endl;
-  std::cout << "tiplength:" << tipLength << std::endl;
-  */
-  cone->rotate(90,0);
-  isects.push_back(static_cast<BaseIF*> (new EBGeometryIF(cone, false)));
+  auto cone = std::make_shared<EBGeometry::ConeSDF<Real>>(centerT, tipLength, a_angle, true);
+
+  // Cone rotation will only work as expected if the cone tip is placed in origo.
+  cone->rotate(90, 0);
+
+  isects.push_back(static_cast<BaseIF*>(new EBGeometryIF(cone, false)));
 
   // Build the needle
   m_baseif = RefCountedPtr<BaseIF>(new SmoothIntersection(isects, 0.01));
 
   // Delete everything we have allocated so far
-  for(int i = 0; i < isects.size(); ++i){
+  for (int i = 0; i < isects.size(); ++i) {
     delete isects[i];
   }
 }
 
-NeedleIF::NeedleIF(const NeedleIF& a_inputIF){
-  this->m_baseif = a_inputIF.m_baseif;
+NeedleIF::NeedleIF(const NeedleIF& a_inputIF)
+{
+  this->m_baseif    = a_inputIF.m_baseif;
+  this->m_tipRadius = a_inputIF.m_tipRadius;
 }
 
-Real NeedleIF::value(const RealVect& a_point) const{
-  // tried to "round" of the point
-  return m_baseif->value(a_point) - m_tipRadius;
+Real
+NeedleIF::value(const RealVect& a_point) const
+{
+  // round of the point of the needle with + m_tipRadius. Gives a curvature radius of m_tipRadius
+  return m_baseif->value(a_point) + m_tipRadius;
 }
 
-BaseIF* NeedleIF::newImplicitFunction() const{
-  return static_cast<BaseIF*> (new NeedleIF(*this));
+BaseIF*
+NeedleIF::newImplicitFunction() const
+{
+  return static_cast<BaseIF*>(new NeedleIF(*this));
 }
 
 #include <CD_NamespaceFooter.H>
