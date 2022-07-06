a07c83a844829f12b8cdd2ade4d7fa046c7e191a
Robert Marskar <robert.marskar@sintef.no>
2022-07-06 12:03:17 +0200

    Add alpha-coefficient for air at standard atm

--- MyApplications/MyStreamerInception/program.cpp
+++ MyApplications/MyStreamerInception/program.cpp
@@ -3,6 +3,7 @@
 #include <CD_Cylinder.H>
 #include <CD_FieldSolverMultigrid.H>
 #include <CD_StreamerInceptionStepper.H>
+#include <CD_DataParser.H>
 #include <ParmParse.H>
 
 using namespace ChomboDischarge;
@@ -18,12 +19,50 @@ int main(int argc, char* argv[]){
   const std::string input_file = argv[1];
   ParmParse pp(argc-2, argv+2, NULL, input_file.c_str());
 
+  // Read ionization and attachment coefficients and make them into functions.
+  constexpr Real N = 2.45E25;
+  
+  LookupTable<2> ionizationData = DataParser::fractionalFileReadASCII("transport_data.txt",
+								      "E/N (Td)	Townsend ioniz. coef. alpha/N (m2)",
+								      "");
+  LookupTable<2> attachmentData = DataParser::fractionalFileReadASCII("transport_data.txt",
+								      "E/N (Td)	Townsend attach. coef. eta/N (m2)",
+								      "");
+
+  ionizationData.sort(0);
+  attachmentData.sort(0);
+  
+  ionizationData.setTableSpacing(TableSpacing::Exponential);
+  attachmentData.setTableSpacing(TableSpacing::Exponential);
+
+  ionizationData.scale<0>(N * 1.E-21);
+  attachmentData.scale<0>(N * 1.E-21);
+
+  ionizationData.scale<1>(N);
+  attachmentData.scale<1>(N);
+  
+  ionizationData.makeUniform(500);
+  attachmentData.makeUniform(500);
+
+  ionizationData.dumpTable("alpha.dat");
+  attachmentData.dumpTable("eta.dat");
+  
+  auto alphaEff = [&](const Real& E) -> Real {
+    const Real alpha = ionizationData.getEntry<1>(E);
+    const Real eta = attachmentData.getEntry<1>(E);
+
+    return alpha - eta;
+  };
+
   // Set geometry and AMR 
   RefCountedPtr<ComputationalGeometry> compgeom = RefCountedPtr<ComputationalGeometry> (new Cylinder());
   RefCountedPtr<AmrMesh> amr                    = RefCountedPtr<AmrMesh> (new AmrMesh());
 
   // Set up time stepper 
   auto timestepper = RefCountedPtr<StreamerInceptionStepper<>> (new StreamerInceptionStepper<>());
+
+  timestepper->setAlpha(alphaEff);
+
 
   // Set up the Driver and run it
   RefCountedPtr<Driver> engine = RefCountedPtr<Driver> (new Driver(compgeom, timestepper, amr));
