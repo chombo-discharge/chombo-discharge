674cafb42f452aa8a67e7a3a4b13478cbe84092a
Robert Marskar <robert.marskar@sintef.no>
2022-08-15 13:34:08 +0200

    Started adding negative ions for dynamic runs

--- Exec/Examples/StreamerInception/Vessel/program.cpp
+++ Exec/Examples/StreamerInception/Vessel/program.cpp
@@ -26,10 +26,10 @@ main(int argc, char* argv[])
   // Read ionization and attachment coefficients and make them into functions.
   constexpr Real N = 2.45E25;
 
-  LookupTable<2> ionizationData =
-    DataParser::fractionalFileReadASCII("transport_data.txt", "E/N (Td)	Townsend ioniz. coef. alpha/N (m2)", "");
-  LookupTable<2> attachmentData =
-    DataParser::fractionalFileReadASCII("transport_data.txt", "E/N (Td)	Townsend attach. coef. eta/N (m2)", "");
+  // clang-format off
+  LookupTable<2> ionizationData = DataParser::fractionalFileReadASCII("transport_data.txt", "E/N (Td)	Townsend ioniz. coef. alpha/N (m2)", "");
+  LookupTable<2> attachmentData = DataParser::fractionalFileReadASCII("transport_data.txt", "E/N (Td)	Townsend attach. coef. eta/N (m2)", "");
+  // clang-format on
 
   ionizationData.setRange(10, 2000, 0);
   attachmentData.setRange(10, 2000, 0);
@@ -57,8 +57,26 @@ main(int argc, char* argv[])
     return attachmentData.getEntry<1>(E);
   };
 
-  auto detachment = [](const Real& E) -> Real {
-    return 1.E10;
+  // Define a detachment rate.
+  auto detachment = [N](const Real& E) -> Real {
+    return 2.E6 / (1.17E-4 * exp(2.91E7/E));
+  };
+
+  // Define a lightning impulse voltage curve.
+  ParmParse vessel("impulse");    
+  Real V0 = 1.0;
+  Real t1 = 1.2E-6;
+  Real t2 = 50E-6;
+
+  vessel.get("voltage", V0);
+  vessel.get("t1", t1);
+  vessel.get("t2", t2);    
+  
+  auto voltageCurve = [V0, t1, t2](const Real a_time) -> Real {
+    constexpr Real alpha = 1.0/50E-6;
+    constexpr Real beta  = 1.0/1.2E-6;
+
+    return V0 * (exp(-a_time/t1) - exp(-a_time/t2));
   };
 
   // Set geometry and AMR
@@ -67,13 +85,13 @@ main(int argc, char* argv[])
 
   // Set up time stepper
   auto timestepper = RefCountedPtr<StreamerInceptionStepper<>>(new StreamerInceptionStepper<>());
-  auto celltagger =
-    RefCountedPtr<StreamerInceptionTagger>(new StreamerInceptionTagger(amr, timestepper->getElectricField()));
+  auto celltagger  = RefCountedPtr<StreamerInceptionTagger>(new StreamerInceptionTagger(amr, timestepper->getElectricField()));
 
-  // Set ionization coefficient.
+  // Set everything. 
   timestepper->setAlpha(alpha);
   timestepper->setEta(eta);
-  timestepper->setBackgroundRate(detachment);    
+  timestepper->setBackgroundRate(detachment);
+  timestepper->setVoltageCurve(voltageCurve);
 
   // Set up the Driver and run it
   RefCountedPtr<Driver> engine = RefCountedPtr<Driver>(new Driver(compgeom, timestepper, amr, celltagger));
