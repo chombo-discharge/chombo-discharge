69fcaf79c5763a36fccd07762c2345c6d05cc4a1
Robert Marskar <robert.marskar@sintef.no>
2022-09-05 12:59:31 +0200

    Add noise plane

---
+++ Geometries/NoisePlane/CD_NoisePlane.cpp
@@ -0,0 +1,103 @@
+/* chombo-discharge
+ * Copyright © 2021 SINTEF Energy Research.
+ * Please refer to Copyright.txt and LICENSE in the chombo-discharge root directory.
+ */
+
+/*!
+  @file   CD_NoisePlane.cpp
+  @brief  Implementation of CD_NoisePlane.H
+  @author Robert Marskar
+*/
+
+// Std includes
+#include <string>
+
+// Chombo includes
+#include <ParmParse.H>
+
+// Our includes
+#include <CD_NoisePlane.H>
+#include <CD_BoundedNoisePlane.H>
+#include <CD_NamespaceHeader.H>
+
+NoisePlane::NoisePlane()
+{
+
+  this->setGasPermittivity(1.0);
+
+  ParmParse pp("NoisePlane");
+
+  bool usePlane;
+  pp.get("use_plane", usePlane);
+
+  if (usePlane) {
+
+  std::string material;    
+
+    int normal;
+    int noiseOctaves;
+
+    RealVect point;
+    RealVect clampLo;
+    RealVect clampHi;
+    RealVect noiseFreq;
+
+    Real eps;
+    Real clampK;
+    Real noiseAmp;
+    Real noisePersist;
+
+    bool live;
+    bool reseed;
+
+    auto get = [&](const std::string id) -> RealVect {
+      Vector<Real> v;
+      pp.getarr(id.c_str(), v, 0, SpaceDim);
+
+      return RealVect(D_DECL(v[0], v[1], v[2]));
+    };
+
+    pp.get("live", live);
+    pp.get("eps", eps);
+    pp.get("normal", normal);
+    pp.get("clamp_k", clampK);
+    pp.get("noise_amplitude", noiseAmp);
+    pp.get("noise_persistence", noisePersist);
+    pp.get("noise_reseed", reseed);
+    pp.get("noise_octaves", noiseOctaves);
+    pp.get("reseed", reseed);
+    pp.get("material", material);    
+
+    point = get("point");
+    clampLo   = get("clamp_lo");
+    clampHi   = get("clamp_hi");
+    noiseFreq = get("noise_freq");
+
+    RefCountedPtr<BaseIF> plane = RefCountedPtr<BaseIF> (new BoundedNoisePlane(normal,
+									       point,
+									       clampLo,
+									       clampHi,
+									       clampK,
+									       false,
+									       noiseAmp,
+									       noiseFreq,
+									       noisePersist,
+									       noiseOctaves,
+									       reseed));
+
+
+    if(material == "electrode") {
+      m_electrodes.push_back(Electrode(plane, live));
+    }
+    else if(material == "dielectric"){
+      m_dielectrics.push_back(Dielectric(plane, eps));
+    }
+    else {
+      MayDay::Error("NoisePlane::NoisePlane -- unknown mateiral requested");
+    }
+  }
+}
+
+NoisePlane::~NoisePlane() {}
+
+#include <CD_NamespaceFooter.H>
