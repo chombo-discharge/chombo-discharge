d03a1cc5f2a20edf454e5210dedff8c920280cbd
Robert Marskar <robert.marskar@sintef.no>
2022-09-06 09:57:34 +0200

    Added 3D example for SF6

--- Exec/Examples/StreamerInception/ElectrodeRoughness/program.cpp
+++ Exec/Examples/StreamerInception/ElectrodeRoughness/program.cpp
@@ -2,6 +2,8 @@
 #include <CD_NoisePlane.H>
 #include <CD_StreamerInceptionStepper.H>
 #include <CD_StreamerInceptionTagger.H>
+#include <CD_LookupTable.H>
+#include <CD_DataParser.H>
 #include <ParmParse.H>
 
 using namespace ChomboDischarge;
@@ -23,9 +25,35 @@ main(int argc, char* argv[])
   RefCountedPtr<ComputationalGeometry> compgeom = RefCountedPtr<ComputationalGeometry>(new NoisePlane());
   RefCountedPtr<AmrMesh>               amr      = RefCountedPtr<AmrMesh>(new AmrMesh());
 
+  // Read ionization and attachment coefficients and make them into functions.
+  constexpr Real N = 2.45E25;
+
+  LookupTable<2> ionizationData =
+    DataParser::fractionalFileReadASCII("sf6.dat", "E/N (Td)	Townsend ioniz. coef. alpha/N (m2)", "");
+  LookupTable<2> attachmentData =
+    DataParser::fractionalFileReadASCII("sf6.dat", "E/N (Td)	Townsend attach. coef. eta/N (m2)", "");
+
+  ionizationData.setRange(10, 4000, 0);
+  attachmentData.setRange(10, 4000, 0);
+
+  ionizationData.sort(0);
+  attachmentData.sort(0);
+
+  ionizationData.setTableSpacing(TableSpacing::Exponential);
+  attachmentData.setTableSpacing(TableSpacing::Exponential);
+
+  ionizationData.scale<0>(N * 1.E-21);
+  attachmentData.scale<0>(N * 1.E-21);
+
+  ionizationData.scale<1>(N);
+  attachmentData.scale<1>(N);
+
+  ionizationData.makeUniform(500);
+  attachmentData.makeUniform(500);
+
   // Define transport data
-  auto alpha         = [&](const Real& E) -> Real { return 0.0; };
-  auto eta           = [&](const Real& E) -> Real { return 0.0; };
+  auto alpha         = [&](const Real& E) -> Real { return ionizationData.getEntry<1>(E);};
+  auto eta           = [&](const Real& E) -> Real { return attachmentData.getEntry<1>(E);};
   auto alphaEff      = [&](const Real& E) -> Real { return alpha(E) - eta(E); };
   auto bgRate        = [&](const Real& E, const RealVect& x) -> Real { return 0.0; };
   auto detachRate    = [&](const Real& E, const RealVect& x) -> Real { return 0.0; };
---
+++ Exec/Examples/StreamerInception/Vessel/README.md
@@ -0,0 +1,28 @@
+## Examples/StreamerInception/Vessel
+
+This example solves a streamer inception problem for a high voltage vessel geometry. 
+It was set up from $DISCHARGE_HOME/Physics/StreamerInception using
+
+```./setup.py -base_dir=Exec/Examples/StreamerInception -app_name=Vessel -geometry=Vessel```
+
+The program uses input data for air, computed using BOLSIG+.
+In addition, the program defines the ion mobility and ion distribution for computing the discharge inception probability.
+
+In 2D, the program is set up to run in stationary mode (no transient evolution).
+To compile it and run it, type
+
+```make -s -j<num_proc> OPT=HIGH DIM=2 program```
+
+To run it, type
+
+```mpirun -np <num_proc> program2d.*.ex example2d.inputs```
+
+In 3D, the program is set up to run in transient mode for a lightning impulse. 
+To compile it and run it, type
+
+```make -s -j<num_proc> OPT=HIGH DIM=3 program```
+
+To run it, type
+
+```mpirun -np <num_proc> program3d.*.ex example3d.inputs```
+
