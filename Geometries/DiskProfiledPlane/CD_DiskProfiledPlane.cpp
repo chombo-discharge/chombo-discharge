afe315b526ef179244c17dd067f0db9259c03665
Robert Marskar <robert.marskar@sintef.no>
2023-03-11 09:46:48 +0100

    Faster geometry coming into place for both 2D and 3D

--- Geometries/DiskProfiledPlane/CD_DiskProfiledPlane.cpp
+++ Geometries/DiskProfiledPlane/CD_DiskProfiledPlane.cpp
@@ -23,6 +23,11 @@
 #include <CD_DiskProfiledPlane.H>
 #include <CD_NamespaceHeader.H>
 
+using namespace EBGeometry;
+
+using Vec3    = EBGeometry::Vec3T<Real>;
+using ImpFunc = EBGeometry::ImplicitFunction<Real>;
+
 DiskProfiledPlane::DiskProfiledPlane() noexcept
 {
   CH_TIME("DiskProfiledPlane::DiskProfiledPlane");
@@ -55,16 +60,12 @@ DiskProfiledPlane::defineElectrode() noexcept
 {
   CH_TIME("DiskProfiledPlane::defineElectrode");
 
-  using T       = Real;
-  using Vec3    = EBGeometry::Vec3T<T>;
-  using ImpFunc = EBGeometry::ImplicitFunction<T>;
-
   bool isLive         = false;
   Real wheelThickness = 0.0;
   Real wheelRadius    = 0.0;
   Real wheelCurvature = 0.0;
 
-  Vector<Real> v;
+  Vector<Real> v(3, 0.0);
 
   ParmParse pp("DiskProfiledPlane");
 
@@ -75,21 +76,23 @@ DiskProfiledPlane::defineElectrode() noexcept
   pp.get("wheel_curvature", wheelCurvature);
   pp.getarr("wheel_center", v, 0, SpaceDim);
 
-  // Create the disk electrode using EBGeometry.
-  RealVect lo = RealVect(D_DECL(v[0], v[1], v[2]));
-  RealVect hi = RealVect(D_DECL(v[0], v[1], v[2]));
+  // Create disk electrode using EBGeometry. This is an elongation of the union of a torus and a cylinder. Created
+  // in the xy-plane and then put into place later.
+  auto torus    = std::make_shared<TorusSDF<Real>>(Vec3::zero(), wheelRadius, wheelCurvature);
+  auto cylinder = std::make_shared<CylinderSDF<Real>>(Vec3::unit(2) * wheelCurvature,
+                                                      -Vec3::unit(2) * wheelCurvature,
+                                                      wheelRadius);
 
-  lo[0] -= 0.5 * wheelThickness;
-  hi[0] += 0.5 * wheelThickness;
+  // Make union and rotate and put into place.
+  auto wheel = Union<Real>(torus, cylinder);
+  if (wheelThickness > 0.0) {
+    wheel = Elongate<Real>(wheel, 0.5 * wheelThickness * Vec3::unit(2));
+  }
+  wheel = Rotate<Real>(wheel, 90.0, 1);
+  wheel = Translate<Real>(wheel, Vec3(v[0], v[1], v[2]));
 
-  // std::shared_ptr<ImpFunc> wheel = std::make_shared<EBGeometry::CylinderSDF<T>>(lo, hi, wheelRadius);
-
-  // if (wheelCurvature > 0.0) {
-  //   wheel = EBGeometry::Mollify<T>(wheel, wheelCurvature);
-  // }
-
-  // Turn EBGeometry into a Chombo function
-  auto wheelIF = RefCountedPtr<BaseIF>(new RoundedCylinderIF(lo, hi, wheelRadius, wheelCurvature, false));
+  // Turn EBGeometry into Chombo
+  auto wheelIF = RefCountedPtr<BaseIF>(new EBGeometryIF<Real>(wheel, true));
 
   m_electrodes.push_back(Electrode(wheelIF, isLive));
 }
@@ -98,6 +101,49 @@ void
 DiskProfiledPlane::defineDielectric() noexcept
 {
   CH_TIME("DiskProfiledPlane::defineDielectric");
+
+  Vector<Real> boxDimensions(3, std::numeric_limits<Real>::max());
+  Vector<Real> boxTranslation(3, 0.0);
+  Vector<Real> squareDimensions(3, std::numeric_limits<Real>::max());
+  Real         boxCurvature = 0.0;
+  Real         permittivity = 1.0;
+
+  // Get input parameters.
+  ParmParse pp("DiskProfiledPlane");
+
+  pp.get("box_permittivity", permittivity);
+  pp.get("box_curvature", boxCurvature);
+  pp.getarr("box_dimensions", boxDimensions, 0, SpaceDim);
+  pp.getarr("box_translate", boxTranslation, 0, SpaceDim);
+  pp.getarr("square_dimensions", squareDimensions, 0, SpaceDim);
+
+  boxCurvature = std::max(boxCurvature, std::numeric_limits<Real>::min());
+
+  // Basic rounded box.
+  std::shared_ptr<ImpFunc> roundedBox = std::make_shared<RoundedBoxSDF<Real>>(Vec3(boxDimensions[0] - 2 * boxCurvature,
+                                                                                   boxDimensions[1] - 2 * boxCurvature,
+                                                                                   boxDimensions[2] - 2 * boxCurvature),
+                                                                              boxCurvature);
+
+  roundedBox = Translate<Real>(roundedBox, -0.5 * boxDimensions[2] * Vec3::unit(2));
+
+  // Basic square profile shape.
+  const Vec3               squareDims(squareDimensions[0], squareDimensions[1], squareDimensions[2]);
+  std::shared_ptr<ImpFunc> square = std::make_shared<BoxSDF<Real>>(-0.5 * squareDims, 0.5 * squareDims);
+
+  // Subtract the square
+  roundedBox = Difference<Real>(roundedBox, square);
+
+  // Translate into place.
+  roundedBox = Translate<Real>(roundedBox, Vec3(boxTranslation[0], boxTranslation[1], boxTranslation[2]));
+
+  // Hack for 2D.
+  roundedBox = Elongate<Real>(roundedBox, std::numeric_limits<Real>::max() * Vec3::unit(1));
+
+  // Turn EBGeometry into Chombo.
+  auto profiledBoxIF = RefCountedPtr<BaseIF>(new EBGeometryIF<Real>(roundedBox, true));
+
+  m_dielectrics.push_back(Dielectric(profiledBoxIF, permittivity));
 }
 
 #include <CD_NamespaceFooter.H>
