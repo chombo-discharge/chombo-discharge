90102bb5d5451556b38e4152c37bc169e58e2854
Robert Marskar <robert.marskar@gmail.com>
2023-06-23 15:33:52 +0200

    WIP on JSON interface

--- Physics/ItoKMC/PlasmaModels/ItoKMCJSON/CD_ItoKMCJSON.cpp
+++ Physics/ItoKMC/PlasmaModels/ItoKMCJSON/CD_ItoKMCJSON.cpp
@@ -26,7 +26,13 @@ ItoKMCJSON::ItoKMCJSON() noexcept
 {
   CH_TIME("ItoKMCJSON::ItoKMCJSON");
 
+  m_verbose   = false;
   m_className = "ItoKMCJSON";
+
+  this->parseVerbose();
+  this->parsePPC();
+  this->parseDebug();
+  this->parseAlgorithm();
 }
 
 ItoKMCJSON::~ItoKMCJSON() noexcept { CH_TIME("ItoKMCJSON::~ItoKMCJSON"); }
@@ -36,6 +42,7 @@ ItoKMCJSON::parseRuntimeOptions() noexcept
 {
   CH_TIME("ItoKMCJSON::parseRuntimeOptions");
 
+  this->parseVerbose();
   this->parsePPC();
   this->parseDebug();
   this->parseAlgorithm();
@@ -54,6 +61,68 @@ ItoKMCJSON::doesFileExist(const std::string a_filename) const noexcept
   return istream.good();
 }
 
+std::string
+ItoKMCJSON::trim(const std::string& a_string) const noexcept
+{
+  CH_TIME("ItoKMCJSON::trim");
+  if (m_verbose) {
+    pout() << m_className + "::trim" << endl;
+  }
+
+  auto ltrim = [](const std::string a_s) -> std::string {
+    std::string s = a_s;
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+    return s;
+  };
+
+  auto rtrim = [](std::string a_s) -> std::string {
+    std::string s = a_s;
+    s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+    return s;
+  };
+
+  return ltrim(rtrim(a_string));
+}
+
+void
+ItoKMCJSON::throwParserError(const std::string a_error) const noexcept
+{
+  CH_TIME("ItoKMCJSON::throwParserError");
+  if (m_verbose) {
+    pout() << m_className + "::trim" << endl;
+  }
+
+  pout() << a_error << endl;
+
+  MayDay::Error(a_error.c_str());
+}
+
+void
+ItoKMCJSON::throwParserWarning(const std::string a_warning) const noexcept
+{
+  CH_TIME("ItoKMCJSON::throwParserError");
+  if (m_verbose) {
+    pout() << m_className + "::trim" << endl;
+  }
+
+  pout() << a_warning << endl;
+
+  MayDay::Warning(a_warning.c_str());
+}
+
+void
+ItoKMCJSON::parseVerbose() noexcept
+{
+  CH_TIME("ItoKMCJSON::parseVerbose");
+  if (m_verbose) {
+    pout() << m_className + "::parseVerbose" << endl;
+  }
+
+  ParmParse pp(m_className.c_str());
+
+  pp.get("verbose", m_verbose);
+}
+
 void
 ItoKMCJSON::parseJSON() noexcept
 {
@@ -62,14 +131,101 @@ ItoKMCJSON::parseJSON() noexcept
     pout() << m_className + "::parseJSON" << endl;
   }
 
+  const std::string baseError = m_className + "::parseJSON";
+
+  ParmParse pp(m_className.c_str());
+
+  pp.get("chemistry_file", m_jsonFile);
+
   if (!(this->doesFileExist(m_jsonFile))) {
-    this->throwParserError(m_className + "::parseJSON -- file '" + m_jsonFile + "' does not exist");
+    const std::string parseError = baseError + " -- file '" + m_jsonFile + "' does not exist";
+
+    this->throwParserError(parseError.c_str());
   }
 
   // Parse the JSON file.
   std::ifstream f(m_jsonFile);
-  m_json = json::parse(f, nullptr, true, true);
+
+  constexpr auto callback        = nullptr;
+  constexpr auto allowExceptions = true;
+  constexpr auto ignoreComments  = true;
+
+  m_json = json::parse(f, callback, allowExceptions, ignoreComments);
 }
+
+void
+ItoKMC::initializeGasLaw() noexcept
+{
+  CH_TIME("ItoKMCJSON::initializeGasLaw");
+  if (m_verbose) {
+    pout() << m_className + "::initializeGasLaw" << endl;
+  }
+
+  const std::string baseError = m_className + "::initializeGasLaw";
+
+  if (!m_json.contains["gas"]) {
+    this->throwParserError(baseError + " but field 'gas' is missing");
+  }
+  if (!(m_json["gas"].contains("law"))) {
+    this->throwParserError(baseError + " but field 'gas/law' is missing");
+  }
+
+  const auto gasJSON = m_json["gas"];
+  const auto gasLaw  = trim(gasJSON["law"].get<std::string>());
+
+  if (gasLaw == "ideal") {
+    if (!(gasJSON.contains("temperature"))) {
+      this->throwParserError(baseError + " and got ideal gas law but field 'temperature' is missing");
+    }
+    if (!(gasJSON.contains("pressure"))) {
+      this->throwParserError(baseError + " and got ideal gas law but field 'pressure' is missing");
+    }
+
+    const Real T0   = gasJSON["temperature"].get<Real>();
+    const Real P0   = gasJSON["temperature"].get<Real>();
+    const Real P    = P0 * Units::atm2pascal;
+    const Real Rho0 = (P * Units::Na) / (T0 * Units::R);
+
+    m_gasTemperature = [T0](const RealVect a_position) -> Real {
+      return T0;
+    };
+    m_gasPressure = [P](const RealVect a_position) -> Real {
+      return P;
+    };
+    m_gasDensity = [Rho0](const RealVect a_position) -> Real {
+      return Rho0;
+    };
+  }
+  else {
+    const std::string parseError = baseError + " but gas law '" + gasLaw + "' is not supported";
+
+    this->throwParserError(parseError);
+  }
+}
+
+void
+ItoKMC::initializeBackgroundSpecies() noexcept
+{
+  CH_TIME("ItoKMCJSON::initializeBackgroundSpecies");
+  if (m_verbose) {
+    pout() << m_className + "::initializeBackgroundSpecies" << endl;
+  }
+
+  const std::string baseError = m_className + "::initializeBackgroundSpecies";
+
+  if (!(m_json.contains["gas"])) {
+    this->throwParserError(baseError + " but field 'gas' is missing");
+  }
+  if (!(m_json["gas"].contains("background_species"))) {
+    this->throwParserError(baseError + " but field 'gas/background_species' is missing");
+  }
+
+  const auto gasJSON           = m_json["gas"];
+  const auto backgroundSpecies = gasJSON["background_species"];
+
+  for (const auto& species : backgroundSpecies) {
+    const auto speciesName = this->trim(species["id"].get<std::string>());
+  }
 }
 
 #include <CD_NamespaceFooter.H>
