3670a383f43b9815755e951cc158ed234e2fa85c
Robert Marskar <robert.marskar@sintef.no>
2023-08-22 23:47:07 +0200

    Script for performance comparison of some kernels

--- Exec/Examples/Electrostatics/MechShaft/compile_and_run.sh
+++ Exec/Examples/Electrostatics/MechShaft/compile_and_run.sh
@@ -2,13 +2,14 @@ export NCORES=12
 export CH_TIMER=1
 export OMP_NUM_THREADS=$NCORES
 export OMP_PLACES=cores
-export OMP_SCHEDULE="static,1"
+export OMP_PROC_BIND=close
+export OMP_SCHEDULE="guided"
 
 # Compile for serial, OpenMP, flat MPI, and MPI+OpenMP
-# make -s -j$NCORES OPT=HIGH DEBUG=FALSE OPENMPCC=FALSE MPI=FALSE
-# make -s -j$NCORES OPT=HIGH DEBUG=FALSE OPENMPCC=TRUE MPI=FALSE
-# make -s -j$NCORES OPT=HIGH DEBUG=FALSE OPENMPCC=FALSE MPI=TRUE
-# make -s -j$NCORES OPT=HIGH DEBUG=FALSE OPENMPCC=TRUE MPI=TRUE
+make -s -j$NCORES OPT=HIGH DEBUG=FALSE OPENMPCC=FALSE MPI=FALSE
+make -s -j$NCORES OPT=HIGH DEBUG=FALSE OPENMPCC=TRUE MPI=FALSE
+make -s -j$NCORES OPT=HIGH DEBUG=FALSE OPENMPCC=FALSE MPI=TRUE
+make -s -j$NCORES OPT=HIGH DEBUG=FALSE OPENMPCC=TRUE MPI=TRUE
 
 
 # Run serial version
@@ -17,14 +18,14 @@ cp time.table time.table.serial
 
 # Run OpenMP version
 ./program3d.Linux.64.g++.gfortran.OPTHIGH.OPENMPCC.ex example.inputs FieldSolverMultigrid.gmg_exit_tol=1.2 FieldSolverMultigrid.gmg_exit_tol=1.2
-cp time.table time.table.omp
+# cp time.table time.table.omp
 
 # # Run MPI version
 mpiexec -n $NCORES ./program3d.Linux.64.mpic++.gfortran.OPTHIGH.MPI.ex example.inputs FieldSolverMultigrid.gmg_exit_tol=1.2
 cp time.table.0 time.table.mpi
 
 # # Run MPI+OpenMP version
-mpiexec -n 1 --bind-to socket ./program3d.Linux.64.mpic++.gfortran.OPTHIGH.MPI.OPENMPCC.ex example.inputs FieldSolverMultigrid.gmg_exit_tol=1.2
+mpiexec -n 1 --bind-to none ./program3d.Linux.64.mpic++.gfortran.OPTHIGH.MPI.OPENMPCC.ex example.inputs FieldSolverMultigrid.gmg_exit_tol=1.2
 cp time.table.0 time.table.hybrid
 
 PATTERN='EBGhostCellInterpolator::define'
