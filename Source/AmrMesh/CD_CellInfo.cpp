17ec8c707b1f995a4d88fbbbadac4e9015bcadb8
Robert Marskar <robert.marskar@gmail.com>
2024-02-05 10:00:32 +0100

    Now merging through ItoSolver interface with simple way of adding new merging algorithms

--- Source/AmrMesh/CD_CellInfo.cpp
+++ Source/AmrMesh/CD_CellInfo.cpp
@@ -14,10 +14,10 @@
 #include <CD_CellInfo.H>
 #include <CD_NamespaceHeader.H>
 
-const CellInfo CellInfo::Regular = CellInfo();
-
-CellInfo::CellInfo() noexcept
+CellInfo::CellInfo(const IntVect a_gridIndex, const Real a_dx) noexcept
 {
+  m_gridIndex     = a_gridIndex;
+  m_dx            = a_dx;
   m_volFrac       = 1.0;
   m_bndryCentroid = RealVect::Zero;
   m_bndryNormal   = RealVect::Zero;
@@ -25,15 +25,50 @@ CellInfo::CellInfo() noexcept
   m_validHi       = 0.5 * RealVect::Unit;
 }
 
-CellInfo::CellInfo(const Real a_volFrac, const RealVect a_bndryCentroid, const RealVect a_bndryNormal) noexcept {
-  m_volFrac = a_volFrac;
+CellInfo::CellInfo(const IntVect  a_gridIndex,
+                   const Real     a_dx,
+                   const Real     a_volFrac,
+                   const RealVect a_bndryCentroid,
+                   const RealVect a_bndryNormal) noexcept
+{
+  m_gridIndex     = a_gridIndex;
+  m_dx            = a_dx;
+  m_volFrac       = a_volFrac;
   m_bndryCentroid = a_bndryCentroid;
-  m_bndryNormal = a_bndryNormal;
-  
-  DataOps::computeMinValidBox(m_validLo, m_validHi, m_bndryNormal, m_bndryCentroid);
+  m_bndryNormal   = a_bndryNormal;
+  m_validLo       = -0.5 * RealVect::Unit;
+  m_validHi       = 0.5 * RealVect::Unit;
+
+  if (a_volFrac < 1.0) {
+    DataOps::computeMinValidBox(m_validLo, m_validHi, m_bndryNormal, m_bndryCentroid);
+  }
 }
 
 CellInfo::~CellInfo() noexcept {}
+
+IntVect&
+CellInfo::getGridIndex() noexcept
+{
+  return m_gridIndex;
+}
+
+const IntVect&
+CellInfo::getGridIndex() const noexcept
+{
+  return m_gridIndex;
+}
+
+Real&
+CellInfo::getDx() noexcept
+{
+  return m_dx;
+}
+
+const Real&
+CellInfo::getDx() const noexcept
+{
+  return m_dx;
+}
 
 Real&
 CellInfo::getVolFrac() noexcept
