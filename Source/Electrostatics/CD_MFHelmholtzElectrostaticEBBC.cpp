7d87358ea5f8573b42b39c94e8a7f61f1fc21187
Robert Marskar <robert.marskar@sintef.no>
2024-02-16 16:40:59 +0100

    safeguard

--- Source/Electrostatics/CD_MFHelmholtzElectrostaticEBBC.cpp
+++ Source/Electrostatics/CD_MFHelmholtzElectrostaticEBBC.cpp
@@ -189,18 +189,14 @@ MFHelmholtzElectrostaticEBBC::applyEBFluxSinglePhase(VoFIterator&           a_si
   // Apply the stencil for computing the contribution to kappaDivF. Note divF is sum(faces) B*grad(Phi)/dx and that this
   // is the contribution from the EB face. B/dx is already included in the stencils and boundary weights, but beta is not.
 
-  // Do single phase cells
-  if (!a_homogeneousPhysBC) {
-    auto kernel = [&](const VolIndex& vof) -> void {
-#if 1
-      if (m_electrostaticBCs.getBcs().size() == 0) {
-        pout() << "MFHelmholtzElectrostaticEBBC::applyEBFluxSinglePhase -- Aborting (remove this error message later"
-               << endl;
-        MayDay::Abort(
-          "MFHelmholtzElectrostaticEBBC::applyEBFluxSinglePhase -- applying single-phase flux even though there is no electrode EB");
-      }
-#endif
+  // This is a safeguard against a corner case where we fail to correctly represent the interface region and also have
+  // no electrodes. In this case we simply bypass the flux calculation. Note that this normally happens when the user
+  // only has dielectrics AND some of the dielectric cells become incorrectly represented at the coarser multigrid levels.
+  const bool hasElectrode = m_electrostaticBCs.getBcs().size() > 0;
 
+  // Do single phase cells
+  if (!a_homogeneousPhysBC && hasElectrode) {
+    auto kernel = [&](const VolIndex& vof) -> void {
       const RealVect pos   = this->getBoundaryPosition(vof, a_dit);
       const Real     value = this->getElectrodePotential(pos);
       const Real     Bcoef = a_Bcoef(vof, m_comp);
