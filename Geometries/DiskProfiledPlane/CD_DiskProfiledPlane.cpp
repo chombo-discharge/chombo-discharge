6134974471c6a2f47a11a56cebe4dd0c1701bbe9
Hans Kristian Meyer <hans.meyer@sintef.no>
2024-04-02 09:35:42 +0200

    working on new dielectric profiles

--- Geometries/DiskProfiledPlane/CD_DiskProfiledPlane.cpp
+++ Geometries/DiskProfiledPlane/CD_DiskProfiledPlane.cpp
@@ -119,10 +119,12 @@ DiskProfiledPlane::defineDielectric() noexcept
   Vector<Real> boxDimensions(3, std::numeric_limits<Real>::max());
   Vector<Real> boxTranslation(3, 0.0);
   Vector<Real> profileTranslate(3, 0.0);
+  Vector<Real> profileTranslate2(3, 0.0);
   Vector<Real> profileRepetitionLo(3, 0.0);
   Vector<Real> profileRepetitionHi(3, 0.0);
   Vector<Real> profilePeriod(3, 0.0);
   Vector<Real> squareDimensions(3, std::numeric_limits<Real>::max());
+  Vector<Real> squareDimensions2(3, std::numeric_limits<Real>::max());
 
   // Get input parameters.
   ParmParse pp("DiskProfiledPlane");
@@ -136,10 +138,12 @@ DiskProfiledPlane::defineDielectric() noexcept
   pp.getarr("box_dimensions", boxDimensions, 0, 3);
   pp.getarr("box_translate", boxTranslation, 0, 3);
   pp.getarr("profile_translate", profileTranslate, 0, 3);
+  pp.getarr("profile_translate2", profileTranslate2, 0, 3);
   pp.getarr("profile_repetition_lo", profileRepetitionLo, 0, 3);
   pp.getarr("profile_repetition_hi", profileRepetitionHi, 0, 3);
   pp.getarr("profile_period", profilePeriod, 0, 3);
   pp.getarr("square_dimensions", squareDimensions, 0, 3);
+  pp.getarr("square_dimensions2", squareDimensions2, 0, 3);
 
   if (boxCurvature <= 0.0) {
     MayDay::Error("DiskProfiledPlane::defineDielectric - must have 'box_curvature' > 0.0");
@@ -149,12 +153,17 @@ DiskProfiledPlane::defineDielectric() noexcept
                      boxDimensions[1] - 2 * boxCurvature,
                      boxDimensions[2] - 2 * boxCurvature);
   const Vec3 profileTra(profileTranslate[0], profileTranslate[1], profileTranslate[2]);
+  const Vec3 profileTra2(profileTranslate2[0], profileTranslate2[1], profileTranslate2[2]);
   const Vec3 profileRepLo(profileRepetitionLo[0], profileRepetitionLo[1], profileRepetitionLo[2]);
   const Vec3 profileRepHi(profileRepetitionHi[0], profileRepetitionHi[1], profileRepetitionHi[2]);
   const Vec3 profilePer(profilePeriod[0], profilePeriod[1], profilePeriod[2]);
   const Vec3 squareDim(squareDimensions[0] - 2 * boxCurvature,
                        squareDimensions[1] - 2 * boxCurvature,
                        squareDimensions[2] - 2 * boxCurvature);
+  const Vec3 squareDim2(squareDimensions2[0] - 2 * boxCurvature,
+                       squareDimensions2[1] - 2 * boxCurvature,
+                       squareDimensions2[2] - 2 * boxCurvature);
+
 
   // Basic rounded box.
   std::shared_ptr<ImpFunc> roundedBox = std::make_shared<RoundedBoxSDF<Real>>(boxDims, boxCurvature);
@@ -162,6 +171,7 @@ DiskProfiledPlane::defineDielectric() noexcept
 
   // Determine the requested profile type.
   std::shared_ptr<ImpFunc> profile;
+  std::shared_ptr<ImpFunc> profile2; 
   if (str == "square") {
     profile = std::make_shared<RoundedBoxSDF<Real>>(squareDim, boxCurvature);
   }
@@ -180,6 +190,12 @@ DiskProfiledPlane::defineDielectric() noexcept
     profile = std::make_shared<SphereSDF<Real>>(Vec3::zero(), cylinderRadius);
     profile = Elongate<Real>(profile, std::numeric_limits<Real>::max() * Vec3::unit(2));
   }
+  else if (str == "l_shape") {
+    profile = std::make_shared<BoxSDF<Real>>(-0.5*squareDim, 0.5*squareDim);
+    profile2 = std::make_shared<BoxSDF<Real>>(-0.5*squareDim2, 0.5*squareDim2);
+	// profile2 = std::make_shared<Union<Real>>({profile, profile2});
+	profile2 = EBGeometry::Union<Real>(profile, profile2);
+  }
   else if (str == "none") {
   }
   else {
@@ -187,12 +203,16 @@ DiskProfiledPlane::defineDielectric() noexcept
   }
 
   // Translate and repeat the profiles. Then do a smooth union
-  if (str != "none") {
-    profile    = Translate<Real>(profile, profileTra);
-    profile    = FiniteRepetition<Real>(profile, profilePer, profileRepLo, profileRepHi);
-    roundedBox = SmoothDifference<Real>(roundedBox, profile, boxCurvature);
+  // if (str != "none") {
+    // profile    = FiniteRepetition<Real>(profile, profilePer, profileRepLo, profileRepHi);
+	// profile    = Translate<Real>(profile, profileTra);
+    // roundedBox = SmoothDifference<Real>(roundedBox, profile, boxCurvature);
+  // }
+  if (str == "l_shape"){
+      profile2    = FiniteRepetition<Real>(profile2, profilePer, profileRepLo, profileRepHi);
+	  profile2    = Translate<Real>(profile2, profileTra2);
+      roundedBox = Difference<Real>(roundedBox, profile2);
   }
-
   // Translate box into place.
   roundedBox = Translate<Real>(roundedBox, Vec3(boxTranslation[0], boxTranslation[1], boxTranslation[2]));
 
