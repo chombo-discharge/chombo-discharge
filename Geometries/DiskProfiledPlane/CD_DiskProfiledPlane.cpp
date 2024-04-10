e5a4c54cb29b0e5f16e733ad71ef294930cb29e1
Robert Marskar <robert.marskar@gmail.com>
2024-04-10 09:50:32 +0200

    robert updates

--- Geometries/DiskProfiledPlane/CD_DiskProfiledPlane.cpp
+++ Geometries/DiskProfiledPlane/CD_DiskProfiledPlane.cpp
@@ -196,17 +196,18 @@ DiskProfiledPlane::defineDielectric() noexcept
 	
 	// std::shared_ptr<ImpFunc> profile2 = std::make_shared<BoxSDF<Real>>(-0.5 * squareDim, 0.5 * squareDim);
     profile2 = std::make_shared<RoundedBoxSDF<Real>>(squareDim, boxCurvature);
-    profile2 = Translate<Real>(profile2, -0.5 * Vec3(squareDim[0], squareDim[1], 0.0));
+    profile2 = Translate<Real>(profile2, -0.5 * Vec3(squareDim[0]+2*boxCurvature, squareDim[1]+2*boxCurvature, 0.0));
+
     // profile2 = Translate<Real>(profile2, -0.5 * Vec3::unit(0) * squareDim2[0]);
 
-    // profile2 = SmoothDifference<Real>(profile, profile2, boxCurvature);
-    profile2 = Union<Real>(profile, profile2);
+    profile2 = SmoothDifference<Real>(profile, profile2, boxCurvature);
+    //    profile2 = Union<Real>(profile, profile2);
 
-    std::shared_ptr<ImpFunc> profile3 = std::make_shared<BoxSDF<Real>>(-0.5 * squareDim, 0.5 * squareDim);
+    std::shared_ptr<ImpFunc> profile3 = std::make_shared<BoxSDF<Real>>(-0.8 * squareDim, 0.5 * squareDim);
     profile3                          = Rotate<Real>(profile3, 79.38, 2);
-    profile3                          = Translate<Real>(profile3, Vec3(0*squareDim[0], 0.85*squareDim[0], 0.0));
+    profile3                          = Translate<Real>(profile3, Vec3(0*squareDim[0], 0.999*squareDim[1], 0.0));
 
-    // profile2 = SmoothDifference<Real>(profile2, profile3, boxCurvature);
+    profile2 = SmoothDifference<Real>(profile2, profile3, boxCurvature);
   }
   else if (str == "none") {
   }
