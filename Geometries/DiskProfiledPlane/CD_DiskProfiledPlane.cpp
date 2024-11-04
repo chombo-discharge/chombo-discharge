7b10e1ac0bcbd12b1784246dd9bea84e645f3e38
Hans Kristian Meyer <hans.meyer@sintef.no>
2024-11-04 15:18:24 +0100

    added more electrodes to DiskProfiledPlane for embedded electrode study

--- Geometries/DiskProfiledPlane/CD_DiskProfiledPlane.cpp
+++ Geometries/DiskProfiledPlane/CD_DiskProfiledPlane.cpp
@@ -62,6 +62,18 @@ DiskProfiledPlane::defineElectrode() noexcept
   Real wheelCurvature = 0.0;
   Real stemRadius     = 0.0;
   Real wheelSmooth    = 0.0;
+  bool isLive2         = false;
+  Real wheel2Thickness = 0.0;
+  Real wheel2Radius    = 0.0;
+  Real wheel2Curvature = 0.0;
+  Real stem2Radius     = 0.0;
+  Real wheel2Smooth    = 0.0;
+  bool isLive3         = false;
+  Real wheel3Thickness = 0.0;
+  Real wheel3Radius    = 0.0;
+  Real wheel3Curvature = 0.0;
+  Real stem3Radius     = 0.0;
+  Real wheel3Smooth    = 0.0;
 
   Vector<Real> v(3, 0.0);
 
@@ -93,15 +105,87 @@ DiskProfiledPlane::defineElectrode() noexcept
   if (wheelThickness > 0.0) {
     wheel = Elongate<Real>(wheel, 0.5 * wheelThickness * Vec3::unit(2));
   }
-  wheel = Rotate<Real>(wheel, 90.0, 0);
+  wheel = Rotate<Real>(wheel, 90.0, 1);
   wheel = Translate<Real>(wheel, Vec3(v[0], v[1], v[2]));
 
   if (SpaceDim == 2) {
     wheel = Elongate<Real>(wheel, std::numeric_limits<Real>::max() * Vec3::unit(2));
   }
 	
+	// SECOND ELECTRODE :::
+	
+	// Get input parameters
+  pp.get("wheel2_live", isLive2);
+  pp.get("wheel2_extra_thickness", wheel2Thickness);
+  pp.get("wheel2_radius", wheel2Radius);
+  pp.get("wheel2_curvature", wheel2Curvature);
+  pp.get("wheel2_stem_radius", stem3Radius);
+  pp.get("wheel2_smooth", wheel2Smooth);
+  pp.getarr("wheel2_center", v, 0, SpaceDim);
+
+  // Create disk electrode using EBGeometry. This is an elongation of the union of a torus and a cylinder. Created
+  // in the xy-plane and then put into place later.
+
+  auto torus2    = std::make_shared<TorusSDF<Real>>(Vec3::zero(), wheel2Radius, wheel2Curvature);
+  auto cylinder2 = std::make_shared<CylinderSDF<Real>>(Vec3::unit(2) * wheel2Curvature,
+                                                      -Vec3::unit(2) * wheel2Curvature,
+                                                      wheel2Radius);
+
+  // Make smooth union and put into place.
+  auto wheel2 = Union<Real>(torus2, cylinder2);
+  if (stemRadius > 0.0) {
+    auto holder = std::make_shared<CapsuleSDF<Real>>(Vec3::zero(), 1.E10 * Vec3::unit(1), stem2Radius);
+    wheel2       = SmoothUnion<Real>(wheel2, holder, wheel2Smooth);
+  }
+  if (wheel2Thickness > 0.0) {
+    wheel2 = Elongate<Real>(wheel2, 0.5 * wheel2Thickness * Vec3::unit(2));
+  }
+  wheel2 = Rotate<Real>(wheel2, 90.0, 0);
+  wheel2 = Translate<Real>(wheel2, Vec3(v[0], v[1], v[2]));
+
+  if (SpaceDim == 2) {
+    wheel2 = Elongate<Real>(wheel2, std::numeric_limits<Real>::max() * Vec3::unit(2));
+  }
+  
+  // THIRD ELECTRODE :::
+	
+	// Get input parameters
+  pp.get("wheel3_live", isLive3);
+  pp.get("wheel3_extra_thickness", wheel3Thickness);
+  pp.get("wheel3_radius", wheel3Radius);
+  pp.get("wheel3_curvature", wheel3Curvature);
+  pp.get("wheel3_stem_radius", stem3Radius);
+  pp.get("wheel3_smooth", wheel3Smooth);
+  pp.getarr("wheel3_center", v, 0, SpaceDim);
+
+  // Create disk electrode using EBGeometry. This is an elongation of the union of a torus and a cylinder. Created
+  // in the xy-plane and then put into place later.
+
+  auto torus3    = std::make_shared<TorusSDF<Real>>(Vec3::zero(), wheel3Radius, wheel3Curvature);
+  auto cylinder3 = std::make_shared<CylinderSDF<Real>>(Vec3::unit(2) * wheel3Curvature,
+                                                      -Vec3::unit(2) * wheel3Curvature,
+                                                      wheel3Radius);
+
+  // Make smooth union and put into place.
+  auto wheel3 = Union<Real>(torus3, cylinder3);
+  if (stemRadius > 0.0) {
+    auto holder = std::make_shared<CapsuleSDF<Real>>(Vec3::zero(), 1.E10 * Vec3::unit(1), stem3Radius);
+    wheel3       = SmoothUnion<Real>(wheel3, holder, wheel3Smooth);
+  }
+  if (wheel3Thickness > 0.0) {
+    wheel3 = Elongate<Real>(wheel3, 0.5 * wheel3Thickness * Vec3::unit(2));
+  }
+  wheel3 = Rotate<Real>(wheel3, 90.0, 0);
+  wheel3 = Translate<Real>(wheel3, Vec3(v[0], v[1], v[2]));
+
+  if (SpaceDim == 2) {
+    wheel3 = Elongate<Real>(wheel3, std::numeric_limits<Real>::max() * Vec3::unit(2));
+  }
+	
   // Turn EBGeometry into Chombo
   m_electrodes.push_back(Electrode(RefCountedPtr<BaseIF>(new EBGeometryIF<Real>(wheel, true)), isLive));
+  m_electrodes.push_back(Electrode(RefCountedPtr<BaseIF>(new EBGeometryIF<Real>(wheel2, true)), isLive2));
+  m_electrodes.push_back(Electrode(RefCountedPtr<BaseIF>(new EBGeometryIF<Real>(wheel3, true)), isLive3));
 }
 
 void
