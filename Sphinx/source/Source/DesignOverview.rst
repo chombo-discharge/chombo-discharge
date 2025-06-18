b77951d75942bd7579787a5b334b8029dd571783
rmrsk <robert.marskar@gmail.com>
2025-06-18 04:15:02 +0000

    Deploying to gh-pages from  @ 8bbbbbde8fd2c3ba08a71cb1b38df16b77614ec0 🚀

--- Sphinx/source/Source/DesignOverview.rst
+++ Sphinx/source/Source/DesignOverview.rst
@@ -44,10 +44,5 @@ The major design components in ``chombo-discharge`` are:
 #. :ref:`Chap:TimeStepper` for integrating the equations of motion.
 #. :ref:`Chap:ComputationalGeometry` for representing computational geometries (such as electrodes and dielectrics).
 #. :ref:`Chap:CellTagger` for flagging cells for refinement and coarsening.
-#. :ref:`Chap:GeoCoarsener` for manually removing refinement flags along the EB surface.
 
-.. caution::
-
-   In the current version of ``chombo-discharge``, :ref:`Chap:GeoCoarsener` has become redundant (due to improvements in the algorithms).
-   It will be removed in future versions. 
 
--- doxygen/html/search/classes_6.js
+++ doxygen/html/search/classes_6.js
@@ -5,7 +5,6 @@ var searchData=
   ['genericparticle_3c_202_2c_200_20_3e_2',['GenericParticle&lt; 2, 0 &gt;',['../classGenericParticle.html',1,'']]],
   ['genericparticle_3c_202_2c_201_20_3e_3',['GenericParticle&lt; 2, 1 &gt;',['../classGenericParticle.html',1,'']]],
   ['genericparticle_3c_205_2c_203_20_3e_4',['GenericParticle&lt; 5, 3 &gt;',['../classGenericParticle.html',1,'']]],
-  ['geocoarsener_5',['GeoCoarsener',['../classGeoCoarsener.html',1,'']]],
-  ['geometrystepper_6',['GeometryStepper',['../classPhysics_1_1Geometry_1_1GeometryStepper.html',1,'Physics::Geometry']]],
-  ['gradedperlinspheresdf_7',['GradedPerlinSphereSdf',['../classGradedPerlinSphereSdf.html',1,'']]]
+  ['geometrystepper_5',['GeometryStepper',['../classPhysics_1_1Geometry_1_1GeometryStepper.html',1,'Physics::Geometry']]],
+  ['gradedperlinspheresdf_6',['GradedPerlinSphereSdf',['../classGradedPerlinSphereSdf.html',1,'']]]
 ];
