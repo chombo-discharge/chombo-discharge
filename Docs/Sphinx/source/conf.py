c5cce2b56ff6207ffaeccb7ca4e323aa78c0327f
Robert Marskar <robert.marskar@sintef.no>
2025-09-01 12:51:04 +0200

    update

--- Docs/Sphinx/source/conf.py
+++ Docs/Sphinx/source/conf.py
@@ -216,8 +216,7 @@ latex_elements = {
 'pointsize': '10pt',
 
 # Additional stuff for the LaTeX preamble.
-'preamble': '\setcounter{secnumdepth}{3}'
-'preamble': '\setcounter{tocdepth}{2}'
+#'preamble': '\setcounter{secnumdepth}{3}\setcounter{tocdepth}{2}',
 
 # Release name
 #'releasename': ''
