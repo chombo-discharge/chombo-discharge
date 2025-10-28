73e56b5477ef66d52f8e42aeace9f55c111c3868
Robert Marskar <robert.marskar@sintef.no>
2025-10-28 09:51:22 +0100

    Format files

--- Exec/Examples/Electrostatics/GECReferenceCell/program.cpp
+++ Exec/Examples/Electrostatics/GECReferenceCell/program.cpp
@@ -7,7 +7,9 @@
 using namespace ChomboDischarge;
 using namespace Physics::Electrostatics;
 
-int main(int argc, char* argv[]){
+int
+main(int argc, char* argv[])
+{
 
 #ifdef CH_MPI
   MPI_Init(&argc, &argv);
@@ -15,19 +17,18 @@ int main(int argc, char* argv[]){
 
   // Build class options from input script and command line options
   const std::string input_file = argv[1];
-  ParmParse pp(argc-2, argv+2, NULL, input_file.c_str());
+  ParmParse         pp(argc - 2, argv + 2, NULL, input_file.c_str());
 
-  // Set geometry and AMR 
-  RefCountedPtr<ComputationalGeometry> compgeom = RefCountedPtr<ComputationalGeometry> (new GECReferenceCell());
-  RefCountedPtr<AmrMesh> amr                    = RefCountedPtr<AmrMesh> (new AmrMesh());
-  RefCountedPtr<CellTagger> tagger              = RefCountedPtr<CellTagger> (NULL);
+  // Set geometry and AMR
+  RefCountedPtr<ComputationalGeometry> compgeom = RefCountedPtr<ComputationalGeometry>(new GECReferenceCell());
+  RefCountedPtr<AmrMesh>               amr      = RefCountedPtr<AmrMesh>(new AmrMesh());
+  RefCountedPtr<CellTagger>            tagger   = RefCountedPtr<CellTagger>(NULL);
 
-  // Set up basic Poisson, potential = 1 
-  auto timestepper = RefCountedPtr<FieldStepper<FieldSolverMultigrid> >
-     (new FieldStepper<FieldSolverMultigrid>());
+  // Set up basic Poisson, potential = 1
+  auto timestepper = RefCountedPtr<FieldStepper<FieldSolverMultigrid>>(new FieldStepper<FieldSolverMultigrid>());
 
   // Set up the Driver and run it
-  RefCountedPtr<Driver> engine = RefCountedPtr<Driver> (new Driver(compgeom, timestepper, amr, tagger));
+  RefCountedPtr<Driver> engine = RefCountedPtr<Driver>(new Driver(compgeom, timestepper, amr, tagger));
   engine->setupAndRun(input_file);
 
 #ifdef CH_MPI
