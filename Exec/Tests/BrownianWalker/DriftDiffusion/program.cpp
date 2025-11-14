1e85f173b0208b7bf8683da847cd15151224f9df
Robert Marskar <robert.marskar@sintef.no>
2025-11-14 13:31:31 +0100

    update brownian test

--- Exec/Tests/BrownianWalker/DriftDiffusion/program.cpp
+++ Exec/Tests/BrownianWalker/DriftDiffusion/program.cpp
@@ -1,9 +1,8 @@
-#include "CD_Driver.H"
+#include <CD_Driver.H>
 #include <CD_ItoSolver.H>
 #include <CD_RodDielectric.H>
 #include <CD_BrownianWalkerStepper.H>
 #include <CD_BrownianWalkerTagger.H>
-#include "ParmParse.H"
 
 using namespace ChomboDischarge;
 using namespace Physics::BrownianWalker;
@@ -11,30 +10,16 @@ using namespace Physics::BrownianWalker;
 int
 main(int argc, char* argv[])
 {
+  ChomboDischarge::initialize(argc, argv);
 
-#ifdef CH_MPI
-  MPI_Init(&argc, &argv);
-#endif
+  auto compgeom    = RefCountedPtr<ComputationalGeometry>(new RodDielectric());
+  auto amr         = RefCountedPtr<AmrMesh>(new AmrMesh());
+  auto solver      = RefCountedPtr<ItoSolver>(new ItoSolver());
+  auto timestepper = RefCountedPtr<TimeStepper>(new BrownianWalkerStepper(solver));
+  auto tagger      = RefCountedPtr<CellTagger>(new BrownianWalkerTagger(solver, amr));
+  auto engine      = RefCountedPtr<Driver>(new Driver(compgeom, timestepper, amr, tagger));
 
-  // Build class options from input script and command line options
-  const std::string input_file = argv[1];
-  ParmParse         pp(argc - 2, argv + 2, NULL, input_file.c_str());
+  engine->setupAndRun();
 
-  // Set geometry and AMR
-  RefCountedPtr<ComputationalGeometry> compgeom = RefCountedPtr<ComputationalGeometry>(new RodDielectric());
-  RefCountedPtr<AmrMesh>               amr      = RefCountedPtr<AmrMesh>(new AmrMesh());
-
-  // Set up basic BrownianWalker
-  RefCountedPtr<ItoSolver>   solver      = RefCountedPtr<ItoSolver>(new ItoSolver());
-  RefCountedPtr<TimeStepper> timestepper = RefCountedPtr<TimeStepper>(new BrownianWalkerStepper(solver));
-  RefCountedPtr<CellTagger>  tagger      = RefCountedPtr<CellTagger>(new BrownianWalkerTagger(solver, amr));
-
-  // Set up the Driver and run it
-  RefCountedPtr<Driver> engine = RefCountedPtr<Driver>(new Driver(compgeom, timestepper, amr, tagger));
-  engine->setupAndRun(input_file);
-
-#ifdef CH_MPI
-  CH_TIMER_REPORT();
-  MPI_Finalize();
-#endif
+  ChomboDischarge::finalize();
 }
