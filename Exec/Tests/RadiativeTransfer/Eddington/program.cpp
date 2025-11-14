2d4858dc1c66a80076f6cc07a5fa7ab9d4fb4413
Robert Marskar <robert.marskar@sintef.no>
2025-11-14 14:04:50 +0100

    tests/radiativetransfer done

--- Exec/Tests/RadiativeTransfer/Eddington/program.cpp
+++ Exec/Tests/RadiativeTransfer/Eddington/program.cpp
@@ -2,7 +2,6 @@
 #include <CD_EddingtonSP1.H>
 #include <CD_RodDielectric.H>
 #include <CD_RadiativeTransferStepper.H>
-#include "ParmParse.H"
 
 using namespace ChomboDischarge;
 using namespace Physics::RadiativeTransfer;
@@ -10,30 +9,16 @@ using namespace Physics::RadiativeTransfer;
 int
 main(int argc, char* argv[])
 {
+  ChomboDischarge::initialize(argc, argv);
 
-#ifdef CH_MPI
-  MPI_Init(&argc, &argv);
-#endif
-
-  // Build class options from input script and command line options
-  const std::string input_file = argv[1];
-  ParmParse         pp(argc - 2, argv + 2, NULL, input_file.c_str());
-
-  // Set geometry and AMR
-  RefCountedPtr<ComputationalGeometry> compgeom = RefCountedPtr<ComputationalGeometry>(new RodDielectric());
-  RefCountedPtr<AmrMesh>               amr      = RefCountedPtr<AmrMesh>(new AmrMesh());
-  RefCountedPtr<CellTagger>            tagger   = RefCountedPtr<CellTagger>(NULL);
-
-  // Set up basic Poisson, potential = 1
+  auto compgeom    = RefCountedPtr<ComputationalGeometry>(new RodDielectric());
+  auto amr         = RefCountedPtr<AmrMesh>(new AmrMesh());
+  auto tagger      = RefCountedPtr<CellTagger>(NULL);
   auto timestepper = RefCountedPtr<RadiativeTransferStepper<EddingtonSP1>>(
     new RadiativeTransferStepper<EddingtonSP1>());
+  auto engine = RefCountedPtr<Driver>(new Driver(compgeom, timestepper, amr, tagger));
 
-  // Set up the Driver and run it
-  RefCountedPtr<Driver> engine = RefCountedPtr<Driver>(new Driver(compgeom, timestepper, amr, tagger));
-  engine->setupAndRun(input_file);
+  engine->setupAndRun();
 
-#ifdef CH_MPI
-  CH_TIMER_REPORT();
-  MPI_Finalize();
-#endif
+  ChomboDischarge::finalize();
 }
