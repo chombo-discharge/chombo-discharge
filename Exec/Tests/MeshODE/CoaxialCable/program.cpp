a83aa51a7180ac460e89cf09ddfb16311fff4a46
Robert Marskar <robert.marskar@sintef.no>
2025-11-14 14:02:58 +0100

    ode in the bag

--- Exec/Tests/MeshODE/CoaxialCable/program.cpp
+++ Exec/Tests/MeshODE/CoaxialCable/program.cpp
@@ -1,7 +1,6 @@
 #include <CD_Driver.H>
 #include <CD_CoaxialCable.H>
 #include <CD_MeshODEStepper.H>
-#include <ParmParse.H>
 
 using namespace ChomboDischarge;
 using namespace Physics::MeshODE;
@@ -9,23 +8,14 @@ using namespace Physics::MeshODE;
 int
 main(int argc, char* argv[])
 {
+  ChomboDischarge::initialize(argc, argv);
 
-#ifdef CH_MPI
-  MPI_Init(&argc, &argv);
-#endif
+  auto compgeom    = RefCountedPtr<ComputationalGeometry>(new CoaxialCable());
+  auto amr         = RefCountedPtr<AmrMesh>(new AmrMesh());
+  auto timestepper = RefCountedPtr<TimeStepper>(new MeshODEStepper<1>());
+  auto engine      = RefCountedPtr<Driver>(new Driver(compgeom, timestepper, amr));
 
-  // Build class options from input script and command line options
-  const std::string input_file = argv[1];
-  ParmParse         pp(argc - 2, argv + 2, NULL, input_file.c_str());
+  engine->setupAndRun();
 
-  RefCountedPtr<ComputationalGeometry> compgeom    = RefCountedPtr<ComputationalGeometry>(new CoaxialCable());
-  RefCountedPtr<AmrMesh>               amr         = RefCountedPtr<AmrMesh>(new AmrMesh());
-  RefCountedPtr<TimeStepper>           timestepper = RefCountedPtr<TimeStepper>(new MeshODEStepper<1>());
-  RefCountedPtr<Driver>                engine      = RefCountedPtr<Driver>(new Driver(compgeom, timestepper, amr));
-  engine->setupAndRun(input_file);
-
-#ifdef CH_MPI
-  CH_TIMER_REPORT();
-  MPI_Finalize();
-#endif
+  ChomboDischarge::finalize();
 }
