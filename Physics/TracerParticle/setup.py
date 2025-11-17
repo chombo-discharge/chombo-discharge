300c1df3e458b348e0de094bc9a0ccff46b51d5b
Robert Marskar <robert.marskar@sintef.no>
2025-11-17 10:42:52 +0100

    Updated the tracer particle setup tool. That's the last one

--- Physics/TracerParticle/setup.py
+++ Physics/TracerParticle/setup.py
@@ -8,11 +8,6 @@ import app_options
 
 # Get arguments from input script
 parser = argparse.ArgumentParser();
-parser.add_argument('-dim',             type=int,  help='Dimension', default=2)
-parser.add_argument('-procs',           type=int,  help='Processors to use when building executable', default=1)
-parser.add_argument('-use_mpi',         type=bool, help='MPI enabled (default true)', default=True)
-parser.add_argument('-build',           type=bool, help='Build executable at end', default=False)
-parser.add_argument('-silent',          type=bool, help='Silent build of executable', default=False)
 parser.add_argument('-discharge_home',  type=str,  help="Source code base directory", default=os.environ.get('DISCHARGE_HOME'))
 parser.add_argument('-base_dir',        type=str,  help="Base directory of mini-app", default="MyApplications")
 parser.add_argument('-app_name',        type=str,  help="Mini app name. An error message is issued if the name already exists")
@@ -28,22 +23,12 @@ if not args.discharge_home:
     print("       >export  DISCHARGE_HOME=<directory>")
 else:
     print("DISCHARGE_HOME is " + args.discharge_home)
-    print('Setting up problem in directory ' + args.discharge_home + "/" + args.base_dir + "/" + args.app_name)
+    print('Setting up problem in directory ' + "/" + args.base_dir + "/" + args.app_name)
 
     app_main.write_template(args)    # Write main file
     app_options.write_template(args) # Write options file
 
     # Copy the makefile
-    os.system('cp ./python/GNUmakefile ' + args.discharge_home + "/" + args.base_dir + "/" + args.app_name + "/GNUmakefile")
+    os.system('cp ./python/GNUmakefile ' + "/" + args.base_dir + "/" + args.app_name + "/GNUmakefile")
 
-    # Build executable if called for it
-    if args.build:
-        os.chdir(args.discharge_home + "/" + args.base_dir + "/" + args.app_name)
-        os.system('pwd')
-        if args.silent:
-            os.system('make -s -j ' + str(args.procs) + ' program')
-        else:
-            os.system('make -j ' + str(args.procs) + ' program')
-            print('Created and built your mini app - it resides in ' + args.discharge_home + "/" + args.base_dir + "/" + args.app_name)
-    else:
-        print('Problem setup successful')
+    print('Problem setup successful')
