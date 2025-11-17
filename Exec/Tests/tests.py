74489ff2e0ae243fa658eb5a234d5a72285a386c
Robert Marskar <robert.marskar@sintef.no>
2025-11-17 13:09:43 +0100

    WIP

--- Exec/Tests/tests.py
+++ Exec/Tests/tests.py
@@ -74,10 +74,43 @@ config = configparser.ConfigParser()
 config.read(test_files)
 
 # --------------------------------------------------
+# Parse Make.defs.local to extract DIM
+# --------------------------------------------------
+def get_dim_from_make_defs():
+    """ Read DIM from Make.defs.local file. """
+    make_defs_path = "../../Submodules/Chombo-3.3/lib/mk/Make.defs.local"
+    if not os.path.exists(make_defs_path):
+        print("Warning: Could not find " + make_defs_path)
+        return None
+
+    try:
+        with open(make_defs_path, 'r') as f:
+            for line in f:
+                # Look for DIM=<value> (ignoring comments and whitespace)
+                stripped = line.strip()
+                if stripped.startswith('DIM') and '=' in stripped:
+                    # Remove comments
+                    stripped = stripped.split('#')[0].strip()
+                    if stripped.startswith('DIM'):
+                        parts = stripped.split('=')
+                        if len(parts) == 2:
+                            dim_value = parts[1].strip()
+                            try:
+                                return int(dim_value)
+                            except ValueError:
+                                print("Warning: Could not parse DIM value: " + dim_value)
+                                return None
+    except Exception as e:
+        print("Warning: Error reading " + make_defs_path + ": " + str(e))
+        return None
+
+    return None
+
+# --------------------------------------------------
 # Moron check for running the test suite
 # --------------------------------------------------
 def pre_check(silent):
-    """ Check that chombo-discharge has been appropriately set up with an environment variable. 
+    """ Check that chombo-discharge has been appropriately set up with an environment variable.
         Print some error messages and what to do if we can't run anything. """
     print("Running " + __file__ + "...")
     discharge_home = os.environ.get("DISCHARGE_HOME")
@@ -132,6 +165,25 @@ def compile_test(silent, build_procs, dim, mpi, omp, hdf, petsc, clean, main):
 # --------------------------------------------------
 pre_check(args.silent)
 
+# --------------------------------------------------
+# Determine DIM to use: command line takes precedence
+# over Make.defs.local
+# --------------------------------------------------
+if args.dim is not None:
+    target_dim = args.dim
+    print("Using DIM=" + str(target_dim) + " from command line")
+else:
+    target_dim = get_dim_from_make_defs()
+    if target_dim is not None:
+        print("Using DIM=" + str(target_dim) + " from Make.defs.local")
+    else:
+        print("Warning: DIM not specified on command line and not found in Make.defs.local")
+        print("All tests will be run regardless of dimensionality")
+        target_dim = None
+
+if target_dim is not None:
+    print("Only running tests with dim=" + str(target_dim))
+
 
 # --------------------------------------------------
 # Run all tests
@@ -172,15 +224,17 @@ for test in config.sections():
         print(tests_file + " does not contain option [" + str(test) + "][dim]. Skipping this test")        
 
     # --------------------------------------------------
+    # Check dimensionality matches target_dim
+    # --------------------------------------------------
+    if do_test and target_dim is not None:
+        test_dim = int(config[str(test)]['dim'])
+        if test_dim != target_dim:
+            do_test = False
+
+    # --------------------------------------------------
     # If we're not running all tests, check that the test string equals args.test
     # --------------------------------------------------
-    if not args.tests:
-        do_test = True
-        dim     = int(config[str(test)]['dim'])
-
-        if args.dim is not None and dim != args.dim and (args.dim==2 or args.dim==3):
-            do_test = False
-    else:
+    if do_test and args.tests:
         do_test = False
         if str(test) in args.tests:
             do_test = True
@@ -223,39 +277,10 @@ for test in config.sections():
         os.chdir(baseDir + "/" + directory)
 
         # --------------------------------------------------
-        # Find the executable file with .ex extension
-        # --------------------------------------------------
-        exec_files = glob.glob("*.ex")
-        if len(exec_files) == 0:
-            print("\t Error: No .ex file found in directory " + directory)
-            run_suite = False
-            ret_code = 1
-        elif len(exec_files) > 1:
-            print("\t Error: Multiple .ex files found in directory " + directory + ": " + str(exec_files))
-            print("\t Please ensure only one executable is present.")
-            run_suite = False
-            ret_code = 1
-        else:
-            executable = exec_files[0]
-
-            # Extract dimensionality from filename (e.g., main2d.MPI.ex -> 2)
-            dim_match = re.search(r'(\d+)d\.', executable)
-            if dim_match:
-                dim = int(dim_match.group(1))
-            else:
-                print("\t Warning: Could not extract dimensionality from executable name '" + executable + "'. Using dim from config.")
-                dim = int(config[str(test)]['dim'])
-
-            print("\t Executable is    = " + executable)
-            print("\t Dimensionality   = " + str(dim))
-            print("\t Output files are = " + str(output) + ".stepXXXXXXX." + str(dim) + "d.hdf5")
-
-        # --------------------------------------------------
         # Check if executable exists. Recompile test if
         # user has called for it
         # --------------------------------------------------
-        if 'run_suite' not in locals():
-            run_suite = True
+        run_suite = True
         if args.compile:
             compile_code = compile_test(silent=args.silent,
                                         build_procs=args.cores,
@@ -276,12 +301,41 @@ for test in config.sections():
         # --------------------------------------------------
         if run_suite:
             if args.no_exec:
-                print(f'\t Test completed   = {time.time() - start:.2f}s')                
+                print(f'\t Test completed   = {time.time() - start:.2f}s')
             else:
+                # --------------------------------------------------
+                # Find the executable file with .ex extension
+                # --------------------------------------------------
+                exec_files = glob.glob("*.ex")
+                if len(exec_files) == 0:
+                    print("\t Error: No .ex file found in directory " + directory)
+                    run_suite = False
+                    ret_code = 1
+                elif len(exec_files) > 1:
+                    print("\t Error: Multiple .ex files found in directory " + directory + ": " + str(exec_files))
+                    print("\t Please ensure only one executable is present.")
+                    run_suite = False
+                    ret_code = 1
+                else:
+                    executable = exec_files[0]
+
+                    # Verify dimensionality in filename matches config
+                    dim_match = re.search(r'(\d+)d\.', executable)
+                    if dim_match:
+                        exec_dim = int(dim_match.group(1))
+                        if exec_dim != dim:
+                            print("\t Warning: Executable '" + executable + "' has dim=" + str(exec_dim) + " but config specifies dim=" + str(dim))
+                    else:
+                        print("\t Warning: Could not extract dimensionality from executable name '" + executable + "'")
+
+                    print("\t Executable is    = " + executable)
+                    print("\t Dimensionality   = " + str(dim))
+                    print("\t Output files are = " + str(output) + ".stepXXXXXXX." + str(dim) + "d.hdf5")
+
                 # --------------------------------------------------
                 # Configure OpenMP
                 # --------------------------------------------------
-                if args.openmp is not None and str(args.openmp).upper() == "TRUE":
+                if run_suite and args.openmp is not None and str(args.openmp).upper() == "TRUE":
                     os.environ['OMP_NUM_THREADS'] = str(args.cores)
                     os.environ['OMP_PLACES'] = "cores"
                     os.environ['OMP_SCHEDULE'] = "dynamic"
@@ -290,82 +344,82 @@ for test in config.sections():
                 # --------------------------------------------------
                 # Set up the run command
                 # --------------------------------------------------
-                if "MPI" in executable:
-                    if args.openmp is not None and str(args.openmp).upper() == "TRUE":
-                        runCommand = args.exec_mpi + " -np 1" + " ./" + executable + " " + inputFile
+                if run_suite:
+                    if "MPI" in executable:
+                        if args.openmp is not None and str(args.openmp).upper() == "TRUE":
+                            runCommand = args.exec_mpi + " -np 1" + " ./" + executable + " " + inputFile
+                        else:
+                            runCommand = args.exec_mpi + " -np " + str(args.cores) + " ./" + executable + " " + inputFile
                     else:
-                        runCommand = args.exec_mpi + " -np " + str(args.cores) + " ./" + executable + " " + inputFile
-                else:
-                    runCommand = "./" + executable + " " + inputFile                
+                        runCommand = "./" + executable + " " + inputFile
 
-                runCommand = runCommand + " Driver.output_names="  + str(output)
-                runCommand = runCommand + " Driver.plot_interval=" + str(nplot)
-                runCommand = runCommand + " Driver.checkpoint_interval=" + str(nplot)
-                runCommand = runCommand + " Driver.max_steps="     + str(nsteps)
-                    
-                if args.benchmark:
-                    runCommand = runCommand + " Driver.restart=0"
-                else:
-                    runCommand = runCommand + " Driver.restart="     + str(restart)
+                    runCommand = runCommand + " Driver.output_names="  + str(output)
+                    runCommand = runCommand + " Driver.plot_interval=" + str(nplot)
+                    runCommand = runCommand + " Driver.checkpoint_interval=" + str(nplot)
+                    runCommand = runCommand + " Driver.max_steps="     + str(nsteps)
 
-                print("\t Executing with   = '" + str(runCommand) + "'")
-
-                # --------------------------------------------------
-                # Run the executable and print the exit code
-                # --------------------------------------------------
-                #        exit_code = os.system(runCommand)
-                if args.silent:
-                    exit_code = subprocess.call(runCommand, shell=True, stdout=DEVNULL, stderr=DEVNULL)
-                else:
-                    exit_code = subprocess.call(runCommand, shell=True)
-
-
-                if exit_code != 0:
-                    print("\t Test run failed with exit code = " + str(exit_code))
-                    ret_code = 2
-                else:
-                    print(f'\t Test completed   = {time.time() - start:.2f}s')                    
-                    # --------------------------------------------------
-                    # Do file comparison if the test ran successfully
-                    # --------------------------------------------------
                     if args.benchmark:
-                        print("\t Regression test '" + str(test) + "' has generated benchmark files.")
-                    elif not args.benchmark and args.compare and args.hdf is not None and str(args.hdf).upper() == "TRUE":
-                        # --------------------------------------------------
-                        # Loop through all files that were generated and
-                        # compare them with h5diff. Print an error message
-                        # if files don't match. 
-                        # --------------------------------------------------
-                        for i in range (0, nsteps+nplot, nplot):
-                        
-                            # --------------------------------------------------
-                            # Get the two files that will be compared
-                            # --------------------------------------------------
-                            regFile =  "plt/" + str(config[str(test)]['output'])
-                            benFile =  "plt/" + str(config[str(test)]['benchmark'])
-                            
-                            regFile = regFile + (".step{0:07}.".format(i)) + str(dim) + "d.hdf5"
-                            benFile = benFile + (".step{0:07}.".format(i)) + str(dim) + "d.hdf5"
+                        runCommand = runCommand + " Driver.restart=0"
+                    else:
+                        runCommand = runCommand + " Driver.restart="     + str(restart)
 
-                            if not os.path.exists(benFile):
-                                print("\t Benchmark file(s) not found, generate them with --benchmark")
-                            else:
-                                print("\t Comparing files " + regFile +  " and " + str(benFile))
-                                
+                    print("\t Executing with   = '" + str(runCommand) + "'")
+
+                    # --------------------------------------------------
+                    # Run the executable and print the exit code
+                    # --------------------------------------------------
+                    #        exit_code = os.system(runCommand)
+                    if args.silent:
+                        exit_code = subprocess.call(runCommand, shell=True, stdout=DEVNULL, stderr=DEVNULL)
+                    else:
+                        exit_code = subprocess.call(runCommand, shell=True)
+
+                    if exit_code != 0:
+                        print("\t Test run failed with exit code = " + str(exit_code))
+                        ret_code = 2
+                    else:
+                        print(f'\t Test completed   = {time.time() - start:.2f}s')
+                        # --------------------------------------------------
+                        # Do file comparison if the test ran successfully
+                        # --------------------------------------------------
+                        if args.benchmark:
+                            print("\t Regression test '" + str(test) + "' has generated benchmark files.")
+                        elif not args.benchmark and args.compare and args.hdf is not None and str(args.hdf).upper() == "TRUE":
+                            # --------------------------------------------------
+                            # Loop through all files that were generated and
+                            # compare them with h5diff. Print an error message
+                            # if files don't match.
+                            # --------------------------------------------------
+                            for i in range (0, nsteps+nplot, nplot):
+
                                 # --------------------------------------------------
-                                # Run h5diff and compare the two files. Print a
-                                # petite message if they match, and a huge-ass 
-                                # warning if they don't. 
+                                # Get the two files that will be compared
                                 # --------------------------------------------------
-                                compare_command = "h5diff " + regFile + " " + benFile
-                                if args.silent:
-                                    compare_code = subprocess.call(compare_command, shell=True, stdout=DEVNULL, stderr=DEVNULL)
+                                regFile =  "plt/" + str(config[str(test)]['output'])
+                                benFile =  "plt/" + str(config[str(test)]['benchmark'])
+
+                                regFile = regFile + (".step{0:07}.".format(i)) + str(dim) + "d.hdf5"
+                                benFile = benFile + (".step{0:07}.".format(i)) + str(dim) + "d.hdf5"
+
+                                if not os.path.exists(benFile):
+                                    print("\t Benchmark file(s) not found, generate them with --benchmark")
                                 else:
-                                    compare_code = subprocess.call(compare_command, shell=True)
-                                    
-                                    if compare_code != 0:
-                                        print("\t FILES '" + regFile +  "' AND '" + benFile + "' DO NOT MATCH - REGRESSION TEST FAILED")
+                                    print("\t Comparing files " + regFile +  " and " + str(benFile))
+
+                                    # --------------------------------------------------
+                                    # Run h5diff and compare the two files. Print a
+                                    # petite message if they match, and a huge-ass
+                                    # warning if they don't.
+                                    # --------------------------------------------------
+                                    compare_command = "h5diff " + regFile + " " + benFile
+                                    if args.silent:
+                                        compare_code = subprocess.call(compare_command, shell=True, stdout=DEVNULL, stderr=DEVNULL)
                                     else:
-                                        print("\t Benchmark test succeded for files " + regFile +  " and " + str(benFile))
+                                        compare_code = subprocess.call(compare_command, shell=True)
+
+                                        if compare_code != 0:
+                                            print("\t FILES '" + regFile +  "' AND '" + benFile + "' DO NOT MATCH - REGRESSION TEST FAILED")
+                                        else:
+                                            print("\t Benchmark test succeded for files " + regFile +  " and " + str(benFile))
 
 exit(ret_code)
