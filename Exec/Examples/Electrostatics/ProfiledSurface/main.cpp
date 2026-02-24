787a9c5419dc3fe11e2395eb3313b3966faeefe5
Robert Marskar <robert.marskar@sintef.no>
2026-02-24 14:36:51 +0100

    Update and begin on mapped bcs

--- Exec/Examples/Electrostatics/ProfiledSurface/main.cpp
+++ Exec/Examples/Electrostatics/ProfiledSurface/main.cpp
@@ -1,16 +1,23 @@
 #include <CD_Driver.H>
 #include <CD_FieldSolverGMG.H>
 #include <CD_DiskProfiledPlane.H>
+#include <CD_TriangleLookup.H>
 #include <CD_FieldStepper.H>
 
 using namespace ChomboDischarge;
 using namespace Physics::Electrostatics;
 
+using T         = Real;
+using Meta      = std::array<Real, 3>;
+using BV        = EBGeometry::BoundingVolumes::AABBT<T>;
+constexpr int K = 4;
+
 int
 main(int argc, char* argv[])
 {
   ChomboDischarge::initialize(argc, argv);
-
+#warning "Debug code enabled. Remember to fix before completing PR"
+#if 0
   auto compgeom    = RefCountedPtr<ComputationalGeometry>(new DiskProfiledPlane());
   auto amr         = RefCountedPtr<AmrMesh>(new AmrMesh());
   auto tagger      = RefCountedPtr<CellTagger>(nullptr);
@@ -18,6 +25,8 @@ main(int argc, char* argv[])
   auto engine      = RefCountedPtr<Driver>(new Driver(compgeom, timestepper, amr, tagger));
 
   engine->setupAndRun();
+#endif
+  const auto triSDF = EBGeometry::Parser::readIntoTriangleBVH<T, Meta, BV, K>("x_low.vtk");
 
   ChomboDischarge::finalize();
 }
