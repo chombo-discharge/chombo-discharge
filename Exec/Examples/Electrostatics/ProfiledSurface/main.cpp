e80e471193b2dccff24b8bacbf38ae21d18c9eaa
Robert Marskar <robert.marskar@sintef.no>
2026-02-27 12:06:19 +0100

    Reset file

--- Exec/Examples/Electrostatics/ProfiledSurface/main.cpp
+++ Exec/Examples/Electrostatics/ProfiledSurface/main.cpp
@@ -1,8 +1,6 @@
 #include <CD_Driver.H>
 #include <CD_FieldSolverGMG.H>
 #include <CD_DiskProfiledPlane.H>
-#include <CD_TriangleCollection.H>
-#include <CD_Triangle.H>
 #include <CD_FieldStepper.H>
 
 using namespace ChomboDischarge;
@@ -12,8 +10,7 @@ int
 main(int argc, char* argv[])
 {
   ChomboDischarge::initialize(argc, argv);
-#warning "Debug code enabled. Remember to fix before completing PR"
-#if 0
+
   auto compgeom    = RefCountedPtr<ComputationalGeometry>(new DiskProfiledPlane());
   auto amr         = RefCountedPtr<AmrMesh>(new AmrMesh());
   auto tagger      = RefCountedPtr<CellTagger>(nullptr);
@@ -21,31 +18,6 @@ main(int argc, char* argv[])
   auto engine      = RefCountedPtr<Driver>(new Driver(compgeom, timestepper, amr, tagger));
 
   engine->setupAndRun();
-#endif
-  const auto x                  = EBGeometry::Vec3T<Real>(0.0, 0.0, 0.1);
-  const auto triangles          = DataParser::readTriangles("test.vtk", "potential");
-  const auto triangleCollection = std::make_shared<TriangleCollection>(triangles);
-  const auto closestTriangles   = triangleCollection->getClosestTriangles(x);
-
-  if (procID() == 0) {
-    const auto& tri = *(closestTriangles.front().first);
-    const auto  y   = tri.projectToTrianglePlane(x);
-    const auto  z   = tri.interpolate(y);
-
-    std::cout << "v0 = " << tri.getVertexPositions()[0] << "\n";
-    std::cout << "v1 = " << tri.getVertexPositions()[1] << "\n";
-    std::cout << "v2 = " << tri.getVertexPositions()[2] << "\n";
-    
-    std::cout << "num triangles = " << triangles.size() << "\n";
-    std::cout << "closest triangles size = " << closestTriangles.size() << "\n";
-    std::cout << "x = " << x << "\n";
-    std::cout << "y = " << y << "\n";    
-    std::cout << "inside(x) = " << tri.isInside(x) << "\n";
-    std::cout << "inside(y) = " << tri.isInside(y) << "\n";
-    std::cout << "dist(x) = " << tri.signedDistance(x) << "\n";
-    std::cout << "dist(y) = " << tri.signedDistance(y) << "\n";
-    std::cout << "interp = " << z << "\n";
-  }
 
   ChomboDischarge::finalize();
 }
