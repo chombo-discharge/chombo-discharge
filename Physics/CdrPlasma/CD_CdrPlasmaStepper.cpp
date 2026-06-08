89f080668c5eeb93b670c77c4bfe5228d5cb6082
Robert Marskar <robert.marskar@sintef.no>
2026-06-08 10:12:58 +0200

    Added Physics/CdrPlasma, but not the subfolders

--- Physics/CdrPlasma/CD_CdrPlasmaStepper.cpp
+++ Physics/CdrPlasma/CD_CdrPlasmaStepper.cpp
@@ -1,12 +1,13 @@
-/* chombo-discharge
- * Copyright © 2021 SINTEF Energy Research.
- * Please refer to Copyright.txt and LICENSE in the chombo-discharge root directory.
+/*
+ * SPDX-FileCopyrightText: 2021-2026 SINTEF Energy Research
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-/*!
-  @file   CD_CdrPlasmaStepper.cpp
-  @brief  Implementation of CD_CdrPlasmaStepper.H
-  @author Robert Marskar
+/**
+   @file   CD_CdrPlasmaStepper.cpp
+   @brief  Implementation of CD_CdrPlasmaStepper.H
+   @author Robert Marskar
 */
 
 // Chombo includes
--- Physics/CdrPlasma/CD_CdrPlasmaTagger.cpp
+++ Physics/CdrPlasma/CD_CdrPlasmaTagger.cpp
@@ -1,12 +1,13 @@
-/* chombo-discharge
- * Copyright © 2021 SINTEF Energy Research.
- * Please refer to Copyright.txt and LICENSE in the chombo-discharge root directory.
+/*
+ * SPDX-FileCopyrightText: 2021-2026 SINTEF Energy Research
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-/*!
-  @file   CD_CdrPlasmaTagger.cpp
-  @brief  Implementation of CD_CdrPlasmaTagger.H
-  @author Robert marskar
+/**
+   @file   CD_CdrPlasmaTagger.cpp
+   @brief  Implementation of CD_CdrPlasmaTagger.H
+   @author Robert Marskar
 */
 
 // Chombo includes
