17d97dd363bce23c12900c52f59cc3bfe56165d2
Robert Marskar <robert.marskar@sintef.no>
2026-06-08 10:14:26 +0200

    Two more files done

--- Physics/CdrPlasma/CellTaggers/CdrPlasmaStreamerTagger/CD_CdrPlasmaStreamerTagger.cpp
+++ Physics/CdrPlasma/CellTaggers/CdrPlasmaStreamerTagger/CD_CdrPlasmaStreamerTagger.cpp
@@ -1,12 +1,13 @@
-/* chombo-discharge
- * Copyright © 2021 SINTEF Energy Research.
- * Please refer to Copyright.txt and LICENSE in the chombo-discharge root directory.
+/*
+ * SPDX-FileCopyrightText: 2021-2026 SINTEF Energy Research
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-/*!
-  @file   CD_CdrPlasmaStreamerTagger.cpp
-  @brief  Implementation CD_CdrPlasmaStreamerTagger.H
-  @author Robert Marskar
+/**
+   @file   CD_CdrPlasmaStreamerTagger.cpp
+   @brief  Implementation CD_CdrPlasmaStreamerTagger.H
+   @author Robert Marskar
 */
 
 // Chombo includes
